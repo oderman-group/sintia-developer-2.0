9c7754798527421d7464cef1b93fe523855b5b59
Enuar Lara <enuar2110@gmail.com>
2022-12-07 18:13:07 -0500

    Se modifica consulta

--- instituciones/developer_2.0/compartido/modal-contrato-guardar.php
+++ instituciones/developer_2.0/compartido/modal-contrato-guardar.php
@@ -2,7 +2,16 @@
 include("../directivo/session.php");
 include("../modelo/conexion.php");
 
-    mysql_query("UPDATE mobiliar_sintia_admin.contratos_usuarios SET cxu_fecha_aceptacion=now() WHERE cxu_id='".$_POST["id"]."'",$conexion);
-    if(mysql_errno()!=0){echo mysql_error(); exit();}
+    $consultaExistencia= mysql_query("SELECT * FROM mobiliar_sintia_admin.contratos_usuarios WHERE cxu_id_contrato='".$_POST["id"]."'  AND cxu_id_institucion='".$config['conf_id_institucion']."'",$conexion);
+    $numDatos = mysql_num_rows($consultaExistencia);
+
+    if($numDatos==0){
+        mysql_query("INSERT INTO mobiliar_sintia_admin.contratos_usuarios(cxu_id_usuario, cxu_id_contrato, cxu_fecha_aceptacion, cxu_id_institucion)VALUES('".$_POST["idUsuario"]."', '".$_POST["id"]."', now(), '".$config['conf_id_institucion']."')", $conexion);
+        if(mysql_errno()!=0){echo mysql_error(); exit();}
+    }else{
+        mysql_query("UPDATE mobiliar_sintia_admin.contratos_usuarios SET cxu_fecha_aceptacion=now() WHERE cxu_id_contrato='".$_POST["id"]."'  AND cxu_id_institucion='".$config['conf_id_institucion']."'",$conexion);
+        if(mysql_errno()!=0){echo mysql_error(); exit();}
+    }
+
     echo '<script type="text/javascript">window.location.href="'.$_SERVER['HTTP_REFERER'].'";</script>';
     exit();
