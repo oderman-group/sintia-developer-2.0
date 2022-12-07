d7c9a16fbbfd8bda2597e229ea9ad6fecb9605d0
Enuar Lara <enuar2110@gmail.com>
2022-12-07 13:13:02 -0500

    Se llama al archivo del modal para aceptar el contrato

--- instituciones/developer_2.0/compartido/body.php
+++ instituciones/developer_2.0/compartido/body.php
@@ -17,3 +17,5 @@ $saldoEC = ($resumenEC[0] - $resumenEC[1]) * -1;
 <?php include("../compartido/modal-acciones.php");?>
 
 <?php include("../compartido/modal-terminos.php");?>
+
+<?php include("../compartido/modal-contrato.php");?>
