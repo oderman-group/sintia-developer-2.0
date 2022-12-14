c6f855e20c3361e6c47ada1d4a2daf377567bd8b
Enuar Lara <enuar2110@gmail.com>
2022-12-13 19:18:52 -0500

    SN006 - Se modifica que muestre lo que se esta buscando

--- instituciones/developer_2.0/compartido/encabezado.php
+++ instituciones/developer_2.0/compartido/encabezado.php
@@ -31,7 +31,7 @@ $institucionNombre = $institucion['ins_siglas'];
 				
                  <form class="search-form-opened" action="paginas-buscador.php" method="GET">
                     <div class="input-group">
-                        <input type="text" class="form-control" placeholder="<?=$frases[8][$datosUsuarioActual[8]];?>..." name="query">
+                        <input type="text" class="form-control" placeholder="<?=$frases[8][$datosUsuarioActual[8]];?>..." value="<?php if(isset($_GET["query"])){ echo $_GET["query"];}?>" name="query">
                         <span class="input-group-btn">
                           <a href="javascript:;" class="btn submit">
                              <i class="icon-magnifier"></i>
