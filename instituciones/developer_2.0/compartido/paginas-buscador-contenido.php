25ba4a56680c6b18d80a935c9289b6163cbd6b06
Enuar Lara <enuar2110@gmail.com>
2022-12-13 19:20:13 -0500

    SN006 - Se cambia la forma de ver los resultados y valida el ingreso a algunos archivos

--- instituciones/developer_2.0/compartido/paginas-buscador-contenido.php
+++ instituciones/developer_2.0/compartido/paginas-buscador-contenido.php
@@ -17,7 +17,7 @@
                         
                         <div class="card card-topline-purple">
                             <div class="card-head">
-                                <header>Paginas</header>
+                                <header>Busqueda</header>
                                 <div class="tools">
                                     <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
                                     <a class="t-collapse btn-color fa fa-chevron-down" href="javascript:;"></a>
@@ -25,72 +25,30 @@
                                 </div>
                             </div>
                             <div class="card-body">
-                                <!--
-                                <div class="row" style="margin-bottom: 10px;">
-                                    <div class="col-sm-12">
-                                        <div class="btn-group">
-                                            <a href="paginas-agregar.php" id="addRow" class="btn deepPink-bgcolor">
-                                                Agregar nuevo <i class="fa fa-plus"></i>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                -->
-                            <span id="respuestaGuardar"></span>	
-                            
-                                <div class="table-scrollable">
-                                <table id="example1" class="display" style="width:100%;">
-                                    <thead>
-                                        <tr>
-                                            <th>#</th>
-                                            <th>ID</th>
-                                            <th>Nombre</th>
-                                            <th>Ruta</th>
-                                            <th><?=$frases[54][$datosUsuarioActual[8]];?></th>
-                                        </tr>
-                                    </thead>
+                                <?php
+                                    $filtro = '';
+                                    if(isset($_GET["query"])){$filtro .= " AND (pagp_pagina LIKE '%".$_GET["query"]."%' OR pagp_ruta LIKE '%".$_GET["query"]."%' OR pagp_palabras_claves LIKE '%".$_GET["query"]."%')";}
                                     
-                                    <?php
-                                        $filtro = '';
-                                        if(isset($_GET["query"])){$filtro .= " AND (pagp_pagina LIKE '%".$_GET["query"]."%' OR pagp_ruta LIKE '%".$_GET["query"]."%' OR pagp_palabras_claves LIKE '%".$_GET["query"]."%')";}
+                                    $tipoUsuario=$datosUsuarioActual['uss_tipo'];
+                                    if($datosUsuarioActual['uss_tipo']==1){
+                                        $tipoUsuario=5;
+                                    }
 
-                                        $tipoUsuario=$datosUsuarioActual['uss_tipo'];
-                                        if($datosUsuarioActual['uss_tipo']==1){
-                                            $tipoUsuario=5;
+                                    $consulta = mysql_query("SELECT * FROM ".$baseDatosServicios.".paginas_publicidad WHERE pagp_tipo_usuario='".$tipoUsuario."' $filtro ORDER BY pagp_id",$conexion);
+                                    while($resultado = mysql_fetch_array($consulta)){
+
+                                        $ruta=$resultado['pagp_ruta'];
+                                        if($resultado['pagp_parametro']!=1){
+                                            $ruta="page-info.php?idmsg=303&idPagina='".$resultado['pagp_id']."'";
                                         }
-                                        $consulta = mysql_query("SELECT * FROM ".$baseDatosServicios.".paginas_publicidad WHERE pagp_tipo_usuario='".$tipoUsuario."' $filtro ORDER BY pagp_id",$conexion);
-                                        $contReg = 1;
-                                        while($resultado = mysql_fetch_array($consulta)){
-                                            ?>
-                                        <tr>
-                                            <td><?=$contReg;?></td>
-                                            <td><?=$resultado['pagp_id'];?></td>
-                                            <td><?=$resultado['pagp_pagina'];?></td>
-                                            <td>
-                                                <a href="<?=$resultado['pagp_ruta'];?>">
-                                                    <?=$resultado['pagp_ruta'];?>
-                                                </a>
-                                            </td>
-                                            <td>
-                                                <div class="btn-group">
-                                                        <button type="button" class="btn btn-primary">Acciones <?php //echo $frases[54][$datosUsuarioActual[8]];?></button>
-                                                        <button type="button" class="btn btn-primary dropdown-toggle m-r-20" data-toggle="dropdown">
-                                                            <i class="fa fa-angle-down"></i>
-                                                        </button>
-                                                        <ul class="dropdown-menu" role="menu">
-                                                            <!--<li><a href="paginas-editar.php?id=<?=$resultado['pagp_id'];?>">Editar</a></li>-->
-                                                        </ul>
-                                                    </div>
-                                            </td>
-                                        </tr>
-                                        <?php 
-                                                $contReg++;
-                                            }
-                                            ?>
-                                    
-                                </table>
-                                    
-                                </div>
+                                ?>
+                                <p>
+                                    <h3><a href="<?=$ruta;?>"><?=$resultado['pagp_pagina'];?></a></h3>
+                                    <p><?=$resultado['pagp_descripcion'];?></P>
+                                </p>
+                                <?php
+                                    }
+                                ?>
                                 
                             </div>
                         </div>
