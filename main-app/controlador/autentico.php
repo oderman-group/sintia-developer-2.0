057220127042026e58a90f82a1d20893b8bc2a29
Jhon Oderman <jhooderman@gmail.com>
2022-12-23 07:19:19 -0500

    SN00(MAIN) - Quitar mysql_errno

--- main-app/controlador/autentico.php
+++ main-app/controlador/autentico.php
@@ -3,7 +3,7 @@ session_start();
 include("../../conexion-datos.php");
 $conexionBaseDatosServicios = mysqli_connect($servidorConexion, $usuarioConexion, $claveConexion, $baseDatosServicios);
 $institucionConsulta = mysqli_query($conexionBaseDatosServicios, "SELECT * FROM ".$baseDatosServicios.".instituciones WHERE ins_id='".$_POST["bd"]."'");
-if(mysql_errno()!=0){echo mysql_error(); exit();}
+
 $institucion = mysqli_fetch_array($institucionConsulta, MYSQLI_BOTH);
 
 $_SESSION["inst"] = $institucion['ins_bd'];
@@ -19,7 +19,7 @@ include("../modelo/conexion.php");
 
 $rst_usrE = mysqli_query($conexion, "SELECT uss_usuario, uss_id, uss_intentos_fallidos FROM usuarios 
 WHERE uss_usuario='".trim($_POST["Usuario"])."' AND TRIM(uss_usuario)!='' AND uss_usuario IS NOT NULL");
-if(mysql_errno()!=0){echo "44". mysql_error(); exit();}
+
 $numE = mysqli_num_rows($rst_usrE);
 if($numE==0){
 	header("Location:".$REDIRECT_ROUTE."/index.php?error=1&msg=usuario-no-fue-encontrado");
@@ -34,7 +34,7 @@ if($usrE['uss_intentos_fallidos']>3 and md5($_POST["suma"])<>$_POST["sumaReal"])
 
 $rst_usr = mysqli_query($conexion, "SELECT * FROM usuarios 
 WHERE uss_usuario='".trim($_POST["Usuario"])."' AND uss_clave='".$_POST["Clave"]."' AND TRIM(uss_usuario)!='' AND uss_usuario IS NOT NULL AND TRIM(uss_clave)!='' AND uss_clave IS NOT NULL");
-if(mysql_errno()!=0){echo mysql_error(); exit();}
+
 $num = mysqli_num_rows($rst_usr);
 $fila = mysqli_fetch_array($rst_usr, MYSQLI_BOTH);
 if($num>0)
@@ -73,19 +73,17 @@ if($num>0)
 	include("navegador.php");
 	include("ip.php");
 	mysqli_query($conexion, "UPDATE usuarios SET uss_estado=1, uss_ultimo_ingreso=now(), uss_intentos_fallidos=0 WHERE uss_id='".$fila[0]."'");
-	if(mysql_errno()!=0){echo mysql_error();exit();}
 
 	mysqli_query($conexion, "INSERT INTO seguridad_historial_acciones(hil_usuario, hil_url, hil_titulo, hil_fecha, hil_so, hil_pagina_anterior)VALUES('".$fila[0]."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', 'Ingreso al sistema', now(),'".php_uname()."','".$_SERVER['HTTP_REFERER']."')");
-	if(mysql_errno()!=0){echo mysql_error(); exit();}
 
 	echo '<script type="text/javascript">window.location.href="'.$url.'";</script>';
 	exit();
 }else{
 	mysqli_query($conexion, "UPDATE usuarios SET uss_intentos_fallidos=uss_intentos_fallidos+1 WHERE uss_id='".$usrE['uss_id']."'");
-	if(mysql_errno()!=0){echo mysql_error();exit();}
+
 
 	mysqli_query($conexion, "INSERT INTO ".$baseDatosServicios.".usuarios_intentos_fallidos(uif_usuarios, uif_ip, uif_clave, uif_institucion)VALUES('".$usrE['uss_id']."', '".$_SERVER['REMOTE_ADDR']."', '".$_POST["Clave"]."', '".$_POST["bd"]."')");
-	if(mysql_errno()!=0){echo mysql_error();exit();}
+
 
 	header("Location:".$REDIRECT_ROUTE."/index.php?error=2&msg=clave-incorrecta");
 	exit();
