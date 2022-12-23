9727c2613af94e3fdfe2fff090f773ac3ce39efd
marvin-garcia-yepes <76126340+marvin-garcia-yepes@users.noreply.github.com>
2022-12-23 17:30:28 +0100

    TS-00 - Saber el peso del archivo.

--- main-app/compartido/peso.php
+++ main-app/compartido/peso.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
+
 <?php
-$dir = ('C:/Users/marvi/Downloads/Prueva');
+$dir = ('../files/archivos');
+
  "Total : " . Fsize($dir);
 function Fsize($dir)
 {
@@ -22,7 +16,7 @@ function Fsize($dir)
                         $cont += Fsize($dir . "/" . $archivo);
                     } else {
                         $nombreArchivo= "archivo : " . $dir . "/" . $archivo . "&nbsp;&nbsp;" . filesize($dir . "/" . $archivo) . "<br />";
-                        if (strpos($nombreArchivo, 'Documentos')){
+                        if (strpos($nombreArchivo, 'mobiliar')){
                             $cont += sprintf("%u", filesize($dir . "/" . $archivo));
                               $nombreArchivo;
                         }
@@ -34,20 +28,28 @@ function Fsize($dir)
     }
     $gb= $cont/1073741824;
     "Carpeta: ".$dir."<br>";
-    return $cont." Byte - ".round($gb, 3)." GB";
+    $porcentaje = ($gb/10)*100;
+    return "<div class='work-monitor work-progress'>
+    <div class='states'>
+        <div class='info'>
+            <div class='desc pull-left'><b>".round($gb, 3)." GB</b></div>
+            <div class='percent pull-right'>".$porcentaje."%</div>
+        </div>
+    
+        <div class='progress progress-xs'>
+            <div class='progress-bar progress-bar-".$colorGrafico." progress-bar-striped' role='progressbar' aria-valuenow='40' aria-valuemin='0' aria-valuemax='100' style='width: ".$porcentaje."%'>
+                <span class='sr-only'>90% </span>
+            </div>
+        </div>
+    </div>
+    </div>";
 }
-$archivo;
-$peso="archivo";
-$gb="Total";
 
-echo '<table border=1>
- <tr>
- <td> Uso Del Disco: </td>
-      </tr>
-      <tr>
-           <td> '.Fsize($dir).' </td>
-       </tr>
- </table>';
 ?>
-</body>
-</html>
+
+<div class="card">
+<td> Uso Del Disco: </td>
+   <?=Fsize($dir)?>
+
+</div>
+
