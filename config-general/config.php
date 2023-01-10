e0b255c525e3fda39c14e65ecd709910ddcf3140
andrea garcia <jagarcia@unac.edu.co>
2023-01-10 11:12:43 -0500

    SN-005 - actualizacion de consulta de la tabla general_informacion

--- config-general/config.php
+++ config-general/config.php
@@ -3,7 +3,7 @@ include("../modelo/conexion.php");
 $configConsulta = mysqli_query($conexion, "SELECT * FROM configuracion WHERE conf_id=1");
 $config = mysqli_fetch_array($configConsulta, MYSQLI_BOTH);
 
-$informacionInstConsulta = mysqli_query($conexion, "SELECT * FROM general_informacion");
+$informacionInstConsulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".general_informacion WHERE info_institucion='" . $config['conf_id_institucion'] . "' AND info_year='" . $_SESSION["bd"] . "'");
 $informacion_inst = mysqli_fetch_array($informacionInstConsulta, MYSQLI_BOTH);
 
 $datosUnicosInstitucionConsulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".instituciones WHERE ins_id='".$config['conf_id_institucion']."'");
