6d22bdcee8694ac6271cf166ea997f0d54719f1a
andrea garcia <jagarcia@unac.edu.co>
2023-01-10 11:12:43 -0500

    SN-005 - actualizacion de consulta de la tabla general_informacion

--- main-app/compartido/informacion.php
+++ main-app/compartido/informacion.php
@@ -1,5 +1,5 @@
 <?php
 include("../modell/conexion.php");
-$informacionInstitucion = mysqli_query($conexion, "SELECT * FROM general_informacion WHERE info_id=1");
+$informacionInstitucion = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".general_informacion WHERE info_id=1");
 $infoI = mysqli_fetch_array($informacionInstitucion, MYSQLI_BOTH);
 ?>
