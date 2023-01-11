6e0dde950f0c202cf18bf16aaa4a4fd467796312
Jhon Oderman <jhooderman@gmail.com>
2023-01-11 05:59:33 -0500

    IMP - Mejorar de la barra DEV.

--- main-app/compartido/barra-developer.php
+++ main-app/compartido/barra-developer.php
@@ -6,17 +6,17 @@ $datosUsuarioActual[3]==1
 ){?>
 <div style="
         position:relative;
-        background-color: #fbbd01; 
-        color:#000; 
-        height: 50px; 
+        background-color: #41c4c4; 
+        color:#fff; 
+        height: 40px; 
         width: 100%; 
         margin-bottom: 20px; 
-        padding: 7px;
+        padding: 6px;
         display:flex; 
         justify-content: center; 
         align-items: center;
         font-family:Arial;
-        font-size:16px;
+        font-size:14px;
 ">
 
 <?php 
@@ -30,15 +30,14 @@ if( $datosUsuarioActual[3]==1 || isset($_SESSION['admin']) ){
   $ramaActual = substr($line, 16);
 ?>
 
-    <b>Rama actual GIT:</b>&nbsp;<?php echo $ramaActual;?>&nbsp;|&nbsp;
+    <b>Rama GIT:</b>&nbsp;<?php echo $ramaActual;?>&nbsp;|&nbsp;
     <b>ID Inst:</b>&nbsp;<?php echo $config['conf_id_institucion'];?>&nbsp;|&nbsp;
     <b>Id pagina:</b>&nbsp;<?php echo $idPaginaInterna;?>&nbsp;|&nbsp;
-    <b>Archivo de ruta:</b>&nbsp;<?php echo $archivo[4];?>&nbsp;|&nbsp;
     <b>Usuario actual:</b>&nbsp;<?php echo $datosUsuarioActual[0];?>&nbsp;|&nbsp;
     <b>Tipo de Usuario:</b>&nbsp;<?php echo $datosUsuarioActual[3];?>&nbsp;|&nbsp;
-		<b>Versión PHP:&nbsp;</b> <?=phpversion(); ?>&nbsp;|&nbsp; 
+		<b>V PHP:&nbsp;</b> <?=phpversion(); ?>&nbsp;|&nbsp; 
 		<b>Host:&nbsp;</b> <?=$_SERVER['HTTP_HOST']; ?>&nbsp;|&nbsp;
-    <b>Peso página:&nbsp;</b> <?php //echo number_format(filesize($nombre_fichero)) . ' bytes'; ?>&nbsp;|&nbsp;
+    <b>Peso página:&nbsp;</b> <?php echo number_format(filesize($nombre_fichero)) . ' bytes'; ?>&nbsp;|&nbsp;
 
     <?php if( isset($_SESSION['admin']) ){?>
 			<b>User Admin:&nbsp;</b> <?=$_SESSION['admin']; ?>&nbsp;|&nbsp;
