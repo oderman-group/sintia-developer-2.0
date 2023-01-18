8b907e391ffe3eb95923afff213da752748add41
Enuar Lara <enuar2110@gmail.com>
2023-01-18 12:39:49 -0500

    Se corrige error al agragar una nota en consolidado

--- main-app/compartido/ajax-periodos-registrar.php
+++ main-app/compartido/ajax-periodos-registrar.php
@@ -1,5 +1,6 @@
-<?php include("../../config-general/config.php");?>
 <?php
+session_start();
+include("../../config-general/config.php");
 $consultaDatosCargas=mysqli_query($conexion, "SELECT * FROM academico_cargas WHERE car_id='".$_POST["carga"]."' AND car_activa=1");
 $datosCargaActual = mysqli_fetch_array($consultaDatosCargas, MYSQLI_BOTH);
 ?>
