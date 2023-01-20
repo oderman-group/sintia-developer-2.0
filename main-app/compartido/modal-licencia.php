20d6745bdcc199c66b11deb68d3bac130be06abd
Enuar Lara <enuar2110@gmail.com>
2023-01-20 10:30:17 -0500

    SN-011 - Se incluye archivo que tiene los datos de la fecha

--- main-app/compartido/modal-licencia.php
+++ main-app/compartido/modal-licencia.php
@@ -1,30 +1,5 @@
 <?php
-//DATOS DE FECHA ACTUAL
-date_default_timezone_set("America/New_York");
-$fechaActual=date("Y/m/d");  
-$valoresSegunda = explode ("/", $fechaActual); 
-$diaSegunda   = $valoresSegunda[2];  
-$mesSegunda = $valoresSegunda[1];  
-$anyoSegunda  = $valoresSegunda[0];
-$diasSegundaJuliano = gregoriantojd($mesSegunda, $diaSegunda, $anyoSegunda);
-
-//DATOS FECHA DE RENOVACION
-$fechaR=$datosUnicosInstitucion['ins_fecha_renovacion'];
-$fechaRenovacion=date("Y/m/d", strtotime($fechaR));
-$valoresPrimera = explode ("/", $fechaRenovacion);
-$diaPrimera    = $valoresPrimera[2];  
-$mesPrimera  = $valoresPrimera[1];  
-$anyoPrimera   = ($valoresPrimera[0]+1);
-$diasPrimeraJuliano = gregoriantojd($mesPrimera, $diaPrimera, $anyoPrimera);
-
-//VALIDA FECHAS
-if(!checkdate($mesPrimera, $diaPrimera, $anyoPrimera) || !checkdate($mesSegunda, $diaSegunda, $anyoSegunda)){
-  echo 'Fecha invalida.';
-  exit();
-}
-
-//OPERACION PARA SABER DIAS FALTANTES
-$dfDias=$diasPrimeraJuliano - $diasSegundaJuliano;
+include("../compartido/datos-fechas.php");
 
 //VALIDAMOS DIAS PARA NOTIFICAR POR CORREO
 if($dfDias==90 || $dfDias==30 || $dfDias==5 || $dfDias==1){
