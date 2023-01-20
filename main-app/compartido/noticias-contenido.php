c9374ab7332d46f8cbb0f4eacc4fa070372ada62
Enuar Lara <enuar2110@gmail.com>
2023-01-20 10:25:46 -0500

    SN-011 - Se añade boton de pago

--- main-app/compartido/noticias-contenido.php
+++ main-app/compartido/noticias-contenido.php
@@ -32,6 +32,21 @@
 										</div>
 									</div>
 									
+									<?php include("../compartido/datos-fechas.php");?>
+
+									<?php if((($datosUsuarioActual[3]==1) || ($datosUsuarioActual[3]==5)) && ($datosUnicosInstitucion['ins_deuda']==1 || $dfDias<=1)){?>
+										<div class="panel">
+											<header class="panel-heading panel-heading-red">Pagos</header>
+											<div class="panel-body">
+												<p><b><?=strtoupper($datosUnicosInstitucion['ins_nombre'])?></b>, le recordamos tiene un pago pendiente con la plataforma SINTIA.<br>
+												Puede hacer el pago en el siguiente boton.</p>
+												<div class="col-sm-4">
+													<a href="#" class="btn btn-danger">PAGA AQUI</a>
+												</div>
+											</div>
+										</div>
+									<?php }?>
+
 									<?php if($datosUsuarioActual[3]==2 or $datosUsuarioActual[3]==5){?>
 										<?php include("../compartido/progreso-docentes.php");?>
 									<?php }?>
