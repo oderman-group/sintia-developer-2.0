e9069fdb4f928850221430b9903626f0cafe6f55
andrea garcia <jagarcia@unac.edu.co>
2023-01-24 18:11:19 -0500

    se incluyo variable id

--- main-app/compartido/guardar-historial-acciones.php
+++ main-app/compartido/guardar-historial-acciones.php
@@ -14,4 +14,4 @@ if (isset($_SESSION["admin"])) {
 }
 
 mysqli_query($conexion, "INSERT INTO ".$baseDatosServicios.".seguridad_historial_acciones(hil_usuario, hil_url, hil_titulo, hil_pais, hil_ip, hil_so, hil_institucion, hil_pagina_anterior, hil_tiempo_carga)
-VALUES('".$_SESSION["id"]."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', '".$idPaginaInterna."', '".$paisIP."', '".$ip."', '".$_SERVER['HTTP_USER_AGENT']."', '".$config['conf_id_institucion']."', '".$_SERVER["HTTP_REFERER"]."', '".$tiempoMostrar."')");
+VALUES('".$id."', '".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']."', '".$idPaginaInterna."', '".$paisIP."', '".$ip."', '".$_SERVER['HTTP_USER_AGENT']."', '".$config['conf_id_institucion']."', '".$_SERVER["HTTP_REFERER"]."', '".$tiempoMostrar."')");
