f2e06232d1e53e82c24a8f595db1e67f74b9ec72
Enuar Lara <enuar2110@gmail.com>
2023-01-26 08:54:37 -0500

    Se configura host para envio de correos automaticos

--- config-general/mail.php
+++ config-general/mail.php
@@ -7,7 +7,7 @@ $datosEmpresa = mysql_fetch_array(mysql_query("SELECT * FROM datos_contacto WHER
 //Server settings
 $mail->SMTPDebug = 2;                                       // Enable verbose debug output
 $mail->isSMTP();                                            // Set mailer to use SMTP
-$mail->Host       = 'mail.'.$datosEmpresa['dtc_dominio'];  	// Specify main and backup SMTP servers
+$mail->Host       = 'jemima.dongee.com';  	// Specify main and backup SMTP servers
 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
 $mail->Username   = $datosEmpresa['dtc_email'];                  
 $mail->Password   = $datosEmpresa['dtc_clave_email'];                      
