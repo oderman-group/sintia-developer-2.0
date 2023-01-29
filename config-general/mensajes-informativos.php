ec49d4eefb7f6c37aa51433e704c9ff38a887dc4
Jhon Oderman <jhooderman@gmail.com>
2023-01-29 06:06:08 -0500

    Añadir mensajes informativos para directivos.

--- config-general/mensajes-informativos.php
+++ config-general/mensajes-informativos.php
@@ -1,5 +1,5 @@
 <?php
-if(isset($_GET['error']) and is_numeric($_GET['error'])){
+if(isset($_GET['error'])){
     switch($_GET['error']){
         case 1:
             $tipo = 'danger';
@@ -14,6 +14,16 @@ if(isset($_GET['error']) and is_numeric($_GET['error'])){
         case 3:
             $tipo = 'primary';
             $mensaje = 'Ha superado el número máximo de intentos fallidos. Por favor comúniquese con la administración.';
+        break;
+
+        case 'ER_DT_1':
+            $tipo = 'danger';
+            $mensaje = 'Este usuario(<b>' . $_GET["usuario"] . '</b>) ya existe para otra persona. Cambie el nombre de usuario por favor.';
+        break;
+
+        case 'ER_DT_2':
+            $tipo = 'danger';
+            $mensaje = 'No tienes permiso para editar a este usuario (<b>' . $_GET["usuario"] . '</b>)';
         break;
 
         default:
