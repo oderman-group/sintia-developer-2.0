9db584ff06cdbf30c26004f6d699508cf73d6a5c
Enuar Lara <enuar2110@gmail.com>
2023-01-31 09:56:32 -0500

    IMP - Se actualizan consultas

--- main-app/guardar.php
+++ main-app/guardar.php
@@ -14,9 +14,9 @@ require '../librerias/phpmailer/SMTP.php';
 <?php
 //RECORDAR CLAVE
 if($_POST["id"]==2){
-	$usuario = mysql_query("SELECT * FROM usuarios WHERE uss_email='".$_POST["email"]."'",$conexion);
-	$nU = mysql_num_rows($usuario);
-	$dU = mysql_fetch_array($usuario);
+	$usuario = mysqli_query($conexion,"SELECT * FROM usuarios WHERE uss_email='".$_POST["email"]."'");
+	$nU = mysqli_num_rows($usuario);
+	$dU = mysqli_fetch_array($usuario, MYSQLI_BOTH);
 	if($nU>0){
 		//INICIO ENVÍO DE MENSAJE
 		$tituloMsj = "¡".strtoupper($dU["uss_nombre"])." TUS CREDENCIALES!";
@@ -62,14 +62,10 @@ if($_POST["id"]==2){
 //======================GET=====================
 //MONITOREAR ACCESO AL DEMO
 if ($_GET["get"] == 1) {
-	mysql_query("INSERT INTO demo(demo_fecha_ingreso, demo_usuario, demo_ip)VALUES(now(), '" . $_GET["usr"] . "', '" . $_SERVER["REMOTE_ADDR"] . "')", $conexion);
-	if (mysql_errno() != 0) {
-		echo mysql_error();
-		exit();
-	}
+	mysqli_query($conexion,"INSERT INTO demo(demo_fecha_ingreso, demo_usuario, demo_ip)VALUES(now(), '" . $_GET["usr"] . "', '" . $_SERVER["REMOTE_ADDR"] . "')");
 	?>
 
-	<form name="frm_login" action="https://developer.plataformasintia.com/controlador/autentico.php" method="post">
+	<form name="frm_login" action="https://developer.plataformasintia.com/app-sintia/main-app/controlador/autentico.php" method="post">
 		<input type="hidden" name="Usuario" value="<?= $_GET["user"]; ?>">
 		<input type="hidden" name="Clave" value="<?= $_GET["pass"]; ?>">
 		<input type="hidden" name="bd" value="22">
