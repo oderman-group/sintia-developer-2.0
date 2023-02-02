e7627ce95fa83407b709f1415f4c52b157850e8d
Enuar Lara <enuar2110@gmail.com>
2023-02-02 13:37:41 -0500

    Se añade enlace para consolidado perdidos

--- main-app/directivo/informes-todos.php
+++ main-app/directivo/informes-todos.php
@@ -35,6 +35,7 @@
 										<header class="panel-heading panel-heading-blue">INFORMES ACADÉMICOS</header>
 										<div class="panel-body">
 											<p><a href="informe-estudiantes.php">Informe de Estudiantes</a></p>
+											<p><a href="consolidado-perdidos.php">Informe de consolidado perdidos</a></p>
 											<p><a href="informes-boletines.php">Boletines por curso</a></p>
 											<p><a href="estudiantes-certificados.php">Certificados</a></p>
 											<p><a href="reportes-academicos-consultas.php">Reporte general</a></p>
