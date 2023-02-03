56937687969e9a7c054cdb6683e511beaf9dceed
Enuar Lara <enuar2110@gmail.com>
2023-02-02 19:07:08 -0500

    Se añade enlace para libro de matricula

--- main-app/directivo/informes-todos.php
+++ main-app/directivo/informes-todos.php
@@ -30,13 +30,14 @@
                         <div class="col-md-12">
                             <div class="row">
 								
-								<div class="col-md-6">
+								<div class="col-md-6">informe-libro-cursos.php
 									<div class="panel">
 										<header class="panel-heading panel-heading-blue">INFORMES ACADÉMICOS</header>
 										<div class="panel-body">
 											<p><a href="informe-estudiantes.php">Informe de Estudiantes</a></p>
 											<p><a href="consolidado-perdidos.php">Informe de consolidado perdidos</a></p>
 											<p><a href="informes-boletines.php">Boletines por curso</a></p>
+											<p><a href="informe-libro-cursos.php">Informe libro por curso</a></p>
 											<p><a href="estudiantes-certificados.php">Certificados</a></p>
 											<p><a href="reportes-academicos-consultas.php">Reporte general</a></p>
                                             <p><a href="informe-parcial-grupo.php">Informe parcial por grupo</a></p>
