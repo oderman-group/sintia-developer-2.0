875bc7614beb0e0157781483bf829a2273192c2e
Enuar Lara <enuar2110@gmail.com>
2023-02-09 17:04:54 -0500

    SN00- Se modifica archivo para importar excel

--- main-app/directivo/excel-importar-estudiantes.php
+++ main-app/directivo/excel-importar-estudiantes.php
@@ -1,105 +1,249 @@
-<?php include("session.php");?>
 <?php
-if($_FILES['planilla']['name']!=""){
-	$archivo = $_FILES['planilla']['name']; $destino = "../files/excel";
-	move_uploaded_file($_FILES['planilla']['tmp_name'], $destino ."/".$archivo);
-}
-?>
-<?php
-//set_time_limit (0);
+include("session.php");
+require '../../librerias/Excel/vendor/autoload.php';
 
-// Test CVS
-require_once '../../librerias/Excel/reader.php';
+use PhpOffice\PhpSpreadsheet\IOFactory;
 
+$archivo = $_FILES['planilla']['name']; $destino = "../files/excel/";
+move_uploaded_file($_FILES['planilla']['tmp_name'], $destino.$archivo);
 
-// ExcelFile($filename, $encoding);
-$data = new Spreadsheet_Excel_Reader();
+$nombreArchivo= $destino.$archivo;
+$documento= IOFactory::load($nombreArchivo);
+$totalHojas= $documento->getSheetCount();
 
-// Set output Encoding.
-$data->setOutputEncoding('CP1251');
-
-//Read File
-$data->read('../files/excel/'.$archivo);
-
-error_reporting(E_ALL ^ E_NOTICE);
-/*
-for ($i = 1; $i <= $data->sheets[0]['numRows']; $i++) {
-	for ($j = 1; $j <= $data->sheets[0]['numCols']; $j++) {
-		echo $data->sheets[0]['cells'][$i][$j].", ";	
-	}
-	echo "<br>";
-}
-*/
-
-$datosInsert = '';
-for ($i = 2; $i <= $data->sheets[0]['numRows']; $i++) {
-
-	//Campos obligatorios para llenar el registro
-	if(trim($data->sheets[0]['cells'][$i][1])!="" and trim($data->sheets[0]['cells'][$i][2])!="" and trim($data->sheets[0]['cells'][$i][3])!="" and trim($data->sheets[0]['cells'][$i][4])!="" and trim($data->sheets[0]['cells'][$i][5])!="" and trim($data->sheets[0]['cells'][$i][9])!=""){
+$hojaActual= $documento->getSheet(0);
+$numFilas= $hojaActual->getHighestDataRow();
+$letraColumnas= $hojaActual->getHighestDataColumn();
+$f=2;
+while($f<=$numFilas){
+	$A= $hojaActual->getCell('A'.$f)->getValue();
+	$B= $hojaActual->getCell('B'.$f)->getValue();
+	$C= $hojaActual->getCell('C'.$f)->getValue();
+	$D= $hojaActual->getCell('D'.$f)->getValue();
+	$E= $hojaActual->getCell('E'.$f)->getValue();
+	$F= $hojaActual->getCell('F'.$f)->getValue();
+	$G= $hojaActual->getCell('G'.$f)->getValue();
+	$H= $hojaActual->getCell('H'.$f)->getFormattedValue();
+	$I= $hojaActual->getCell('I'.$f)->getValue();
+	$J= $hojaActual->getCell('J'.$f)->getValue();
+	$K= $hojaActual->getCell('K'.$f)->getValue();
+	$L= $hojaActual->getCell('L'.$f)->getValue();
+	$M= $hojaActual->getCell('M'.$f)->getValue();
+	$N= $hojaActual->getCell('N'.$f)->getValue();
+	$O= $hojaActual->getCell('O'.$f)->getValue();
+	$P= $hojaActual->getCell('P'.$f)->getValue();
+	$Q= $hojaActual->getCell('Q'.$f)->getValue();
+	
+	if($A!='' OR $B!='' OR $C!='' OR $D!='' OR $E!='' OR $F!='' OR $G!='' OR $H!='' OR $I!='' OR $J!='' OR $K!='' OR $L!='' OR $M!='' OR $N!='' OR $O!='' OR $P!='' OR $Q!=''){
 		
-		//Cuadramos los generos vacíos
-		if(trim($data->sheets[0]['cells'][$i][6])=="" or !is_numeric($data->sheets[0]['cells'][$i][6])){
-			$data->sheets[0]['cells'][$i][6] = 126;
-		}
+		$tDocumento = "";
+		if(isset($A) AND $A!='') {
+			switch($A){
+				case 'RC';
+					$tDocumento = 108;
+				break;
 
-		//Cuadramos las fechas de nacimientos vacías
-		if(trim($data->sheets[0]['cells'][$i][7])==""){
-			$data->sheets[0]['cells'][$i][7] = date("Y-m-d");
-		}
-
-		//Cuadramos los tipos de documentos vacíos
-		if(trim($data->sheets[0]['cells'][$i][8])=="" or !is_numeric($data->sheets[0]['cells'][$i][8])){
-			$data->sheets[0]['cells'][$i][8] = 108;
+				case 'CC';
+					$tDocumento = 105;
+				break;
+				
+				case 'CE';
+					$tDocumento = 109;
+				break;
+				
+				case 'TI';
+					$tDocumento = 107;
+				break;
+				
+				case 'PP';
+					$tDocumento = 110;
+				break;
+				
+				case 'PE';
+					$tDocumento = 139;
+				break;
+				
+				case 'NUIP';
+					$tDocumento = 106;
+				break;
+			}
+			
 		}
 		
-		$nombreCompleto = $data->sheets[0]['cells'][$i][1]." ".$data->sheets[0]['cells'][$i][2]." ".$data->sheets[0]['cells'][$i][3];
+		$documento = "";
+		if(isset($B) AND $B!='') {
+			$documento = $B;
+		}
 
-		mysqli_query($conexion, "INSERT INTO usuarios(uss_usuario, uss_clave, uss_tipo, uss_nombre, uss_idioma, uss_bloqueado, uss_fecha_registro, uss_responsable_registro)VALUES('".$data->sheets[0]['cells'][$i][9]."', '".$data->sheets[0]['cells'][$i][9]."', 4, '".$nombreCompleto."', 1, 0, now(), '".$_SESSION["id"]."')");
-		$lineaError = __LINE__;
-		include("../compartido/reporte-errores.php");
+		$nombre1 = "";
+		if(isset($C) AND $C!='') {
+			$nombre1 = $C;
+		}
+		
+		$nombre2 = "";
+		if(isset($D) AND $D!='') {
+			$nombre2 = $D;
+		}
+		
+		$apellido1 = "";
+		if(isset($E) AND $E!='') {
+			$apellido1 = $E;
+		}
+		
+		$apellido2 = "";
+		if(isset($F) AND $F!='') {
+			$apellido2 = $F;
+		}
+		
+		$genero = "";
+		if(isset($G) AND $G!='') {
+			switch($G){
+				case 'M';
+					$genero = 126;
+				break;
+
+				case 'F';
+					$genero = 127;
+				break;
+			}
+		}
+		
+		$fNacimiento = "0000-00-00";
+		if(isset($H) AND $H!='') {
+			$fecha = explode ("/", $H); 
+			$dia   = $fecha[0];  
+			$mes = $fecha[1];  
+			$year  = $fecha[2];
+			$fNacimiento = $year.'-'.$mes.'-'.$dia;
+		}
+
+		$grado = "";
+		if(isset($I) AND $I!='') {
+			
+			try{
+				$consulta= mysqli_query($conexion, "SELECT * FROM academico_grados WHERE gra_nombre='".$I."'");
+			} catch (Exception $e) {
+				echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+				exit();
+			}
+			$datos=mysqli_fetch_array($consulta, MYSQLI_BOTH);
+			$grado = $datos['gra_id'];
+		}
+		
+		$grupo = 4;
+		if(isset($J) AND $J!='') {
+			switch($J){
+				case 'A';
+					$grupo = 1;
+				break;
+
+				case 'B';
+					$grupo = 2;
+				break;
+
+				case 'C';
+					$grupo = 3;
+				break;
+			}
+		}
+		
+		$direccion = "";
+		if(isset($K) AND $K!='') {
+			$direccion = $K;
+		}
+		
+		$barrio = "";
+		if(isset($L) AND $L!='') {
+			$barrio = $L;
+		}
+		
+		$celular = "";
+		if(isset($M) AND $M!='') {
+			$celular = $M;
+		}
+		
+		$email = "notiene@notiene.com";
+		if(isset($N) AND $N!='') {
+			$email = $N;
+		}
+		
+		$estrato = 116;
+		if(isset($O) AND $O!='') {
+			switch($O){
+				case 1;
+					$estrato = 114;
+				break;
+
+				case 2;
+					$estrato = 115;
+				break;
+
+				case 3;
+					$estrato = 116;
+				break;
+				case 4;
+					$estrato = 117;
+				break;
+
+				case 5;
+					$estrato = 118;
+				break;
+
+				case 6;
+					$estrato = 119;
+				break;
+				case 7;
+					$estrato = 120;
+				break;
+
+				case 8;
+					$estrato = 121;
+				break;
+
+				case 9;
+					$estrato = 122;
+				break;
+				case 10;
+					$estrato = 123;
+				break;
+
+				case 11;
+					$estrato = 124;
+				break;
+
+				case 12;
+					$estrato = 125;
+				break;
+			}
+		}
+		
+		$tSangre = "";
+		if(isset($P) AND $P!='') {
+			$tSangre = $P;
+		}
+		
+		$eps = "";
+		if(isset($Q) AND $Q!='') {
+			$eps = $Q;
+		}
+
+		try{
+			mysqli_query($conexion, "INSERT INTO usuarios(uss_usuario,uss_clave,uss_tipo,uss_nombre,uss_estado,uss_foto,uss_portada,uss_idioma,uss_email,uss_fecha_nacimiento,uss_celular,uss_genero,uss_bloqueado,uss_fecha_registro,uss_responsable_registro,uss_direccion,uss_apellido1,uss_apellido2,uss_nombre2) VALUES ('".$documento."', '12345678', 4, '".$nombre1."',0,'default.png','default.png',1,'".$email."','".$fNacimiento."','".$celular."', '".$genero."',0, now(),3349, '".$direccion."', '".$apellido1."','".$apellido2."','".$nombre2."')");
+		} catch (Exception $e) {
+			echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+			exit();
+		}
 		$idRegistro = mysqli_insert_id($conexion);
 
-
-		$datosInsert .="(
-		now(),
-		1,
-		'".$data->sheets[0]['cells'][$i][1]."', 
-		'".$data->sheets[0]['cells'][$i][2]."', 
-		'".$data->sheets[0]['cells'][$i][3]."', 
-		'".$data->sheets[0]['cells'][$i][4]."', 
-		'".$data->sheets[0]['cells'][$i][5]."', 
-		'".$data->sheets[0]['cells'][$i][6]."', 
-		'".$data->sheets[0]['cells'][$i][7]."', 
-		'".$data->sheets[0]['cells'][$i][8]."', 
-		'".$data->sheets[0]['cells'][$i][9]."', 
-		'".$data->sheets[0]['cells'][$i][10]."',
-		'".$data->sheets[0]['cells'][$i][11]."',
-		'".$data->sheets[0]['cells'][$i][12]."',
-		'".$data->sheets[0]['cells'][$i][13]."',
-		'".$data->sheets[0]['cells'][$i][14]."',
-		'".$data->sheets[0]['cells'][$i][15]."',
-		'".$data->sheets[0]['cells'][$i][16]."',
-		'".$data->sheets[0]['cells'][$i][17]."',
-		'".$idRegistro."'
-		),";
-		
-		
+		try{
+			mysqli_query($conexion, "INSERT INTO academico_matriculas(mat_fecha,mat_primer_apellido,mat_segundo_apellido,mat_nombres,mat_grado,mat_grupo,mat_genero,mat_fecha_nacimiento,mat_tipo_documento,mat_documento,mat_direccion,mat_barrio,mat_celular,mat_estrato,mat_tipo,mat_estado_matricula,mat_id_usuario,mat_eliminado,mat_email,mat_inclusion,mat_extranjero,mat_estado_agno,mat_solicitud_inscripcion,mat_tipo_sangre,mat_eps,mat_nombre2) VALUES (now(), '".$apellido1."','".$apellido2."','".$nombre1."','".$grado."','".$grupo."', '".$genero."','".$fNacimiento."','".$tDocumento."','".$documento."', '".$direccion."','".$barrio."','".$celular."','".$estrato."',128,4,'".$idRegistro."',0,'".$email."',0,0,0,0,'".$tSangre."','".$eps."','".$nombre2."')");
+		} catch (Exception $e) {
+			echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+			exit();
+		}
 	}
+	$f++;
 }
-//exit();
 
-$datosInsert = substr($datosInsert,0,-1);
-		
-mysqli_query($conexion, "INSERT INTO academico_matriculas(mat_fecha, mat_estado_matricula, mat_primer_apellido, mat_segundo_apellido, mat_nombres, mat_grado, mat_grupo, mat_genero, mat_fecha_nacimiento, mat_tipo_documento, mat_documento, mat_lugar_expedicion, mat_direccion, mat_barrio, mat_telefono, mat_celular, mat_email, mat_inclusion, mat_extranjero, mat_id_usuario)VALUES
-".$datosInsert."");
-$lineaError = __LINE__;
-include("../compartido/reporte-errores.php");	
-
-
-echo '<script type="text/javascript">window.location.href="estudiantes.php";</script>';
+echo '<script type="text/javascript">window.location.href="estudiantes.php?cantidad=10&success=SC_DT_4";</script>';
 exit();
-
-
-//print_r($data);
-//print_r($data->formatRecords);
-?>
