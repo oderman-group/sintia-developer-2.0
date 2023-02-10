31eeac1f5b2d93f61b9209126204942101b81160
Jhon Oderman <jhooderman@gmail.com>
2023-02-10 13:32:44 -0500

    Añadir funciones para listar estudiantes a docentes y obtener sus datos

--- main-app/class/Estudiantes.php
+++ main-app/class/Estudiantes.php
@@ -2,9 +2,9 @@
 class Estudiantes {
 
     public static function listarEstudiantes(
-        int $eliminados = 0, 
+        int    $eliminados      = 0, 
         string $filtroAdicional = '', 
-        $filtroLimite = 'LIMIT 0, 2000'
+        string $filtroLimite    = 'LIMIT 0, 2000'
     )
     {
         global $conexion, $baseDatosServicios;
@@ -18,14 +18,68 @@ class Estudiantes {
             LEFT JOIN ".$baseDatosServicios.".opciones_generales ON ogen_id=mat_genero
             WHERE mat_eliminado IN (0, '".$eliminados."')
             ".$filtroAdicional."
-            ORDER BY mat_grado, mat_grupo, mat_primer_apellido
+            ORDER BY mat_grado, mat_grupo, mat_primer_apellido, mat_segundo_apellido, mat_nombres
             ".$filtroLimite."
             ");
         } catch (Exception $e) {
-            return "Excepción catpurada: ".$e->getMessage();
+            echo "Excepción catpurada: ".$e->getMessage();
+            exit();
         }
 
         return $resultado;
+    }
+
+    public static function listarEstudiantesParaDocentes(string $filtroDocentes = '')
+    {
+        global $conexion, $baseDatosServicios;
+        $resultado = [];
+
+        try {
+            $resultado = mysqli_query($conexion, "SELECT * FROM academico_matriculas
+            LEFT JOIN usuarios ON uss_id=mat_id_usuario
+            LEFT JOIN academico_grados ON gra_id=mat_grado
+            LEFT JOIN academico_grupos ON gru_id=mat_grupo
+            LEFT JOIN ".$baseDatosServicios.".opciones_generales ON ogen_id=mat_genero
+            WHERE mat_eliminado=0 
+            AND (mat_estado_matricula=1 OR mat_estado_matricula=2)
+            ".$filtroDocentes."
+            ORDER BY mat_primer_apellido, mat_segundo_apellido, mat_nombres
+            ");
+        } catch (Exception $e) {
+            echo "Excepción catpurada: ".$e->getMessage();
+            exit();
+        }
+
+        return $resultado;
+    }
+
+    public static function obtenerDatosEstudiante($estudiante = 0)
+    {
+
+        global $conexion, $baseDatosServicios;
+        $resultado = [];
+
+        try {
+            $consulta = mysqli_query($conexion, "SELECT * FROM academico_matriculas
+            LEFT JOIN usuarios ON uss_id=mat_id_usuario
+            LEFT JOIN academico_grados ON gra_id=mat_grado
+            LEFT JOIN academico_grupos ON gru_id=mat_grupo
+            LEFT JOIN ".$baseDatosServicios.".opciones_generales ON ogen_id=mat_genero
+            WHERE mat_id='".$estudiante."' AND mat_eliminado=0
+            ");
+            $num = mysqli_num_rows($consulta);
+            if($num == 0){
+                echo "Este estudiante no existe";
+                exit();
+            }
+            $resultado = mysqli_fetch_array($consulta, MYSQLI_BOTH);
+        } catch (Exception $e) {
+            echo "Excepción catpurada: ".$e->getMessage();
+            exit();
+        }
+
+        return $resultado;
+
     }
 
 }
