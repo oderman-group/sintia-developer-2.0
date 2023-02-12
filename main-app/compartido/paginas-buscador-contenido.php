150dd287cd16aa1fff22a4a23eaf0fa63ab26f54
Jhon Oderman <jhooderman@gmail.com>
2023-02-11 23:02:56 -0500

    Mejoras en el buscador general

--- main-app/compartido/paginas-buscador-contenido.php
+++ main-app/compartido/paginas-buscador-contenido.php
@@ -27,38 +27,48 @@
                             <div class="card-body">
                                 <?php
                                     $filtro = '';
-                                    if(isset($_GET["query"])){$filtro .= " AND (pagp_pagina LIKE '%".$_GET["query"]."%' OR pagp_ruta LIKE '%".$_GET["query"]."%' OR pagp_palabras_claves LIKE '%".$_GET["query"]."%')";}
+                                    if(isset($_GET["query"])){$filtro .= " AND (pagp_pagina LIKE '%".$_GET["query"]."%' 
+                                        OR pagp_ruta LIKE '%".$_GET["query"]."%' 
+                                        OR pagp_palabras_claves LIKE '%".$_GET["query"]."%')";}
                                     
                                     $tipoUsuario=$datosUsuarioActual['uss_tipo'];
                                     if($datosUsuarioActual['uss_tipo']==1){
                                         $tipoUsuario=5;
                                     }
-                                    $dato=1;
-                                    $consulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".paginas_publicidad WHERE pagp_tipo_usuario='".$tipoUsuario."' $filtro ORDER BY pagp_id");
+                                    $dato = 1;
+                                    $consulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".paginas_publicidad 
+                                    WHERE pagp_tipo_usuario='".$tipoUsuario."' $filtro ORDER BY pagp_id");
                                     $numDatos=mysqli_num_rows($consulta);
                                     //buscador usuarios
                                     if ($numDatos<=0){
-                                        $dato=2;
-                                        $consulta = mysqli_query($conexion, "SELECT * FROM usuarios WHERE uss_usuario LIKE '%".$_GET["query"]."%' OR uss_nombre LIKE '%".$_GET["query"]."%' OR uss_email LIKE '%".$_GET["query"]."%'");
+                                        $dato = 2;
+                                        $consulta = mysqli_query($conexion, "SELECT * FROM usuarios 
+                                        WHERE uss_usuario LIKE '%".$_GET["query"]."%' 
+                                        OR uss_nombre LIKE '%".$_GET["query"]."%' 
+                                        OR uss_email LIKE '%".$_GET["query"]."%'");
                                     }
                                     $numDatos=mysqli_num_rows($consulta);
                                     if ($numDatos<=0) {
-                                        $dato=3;
-                                        $consulta = mysqli_query($conexion, "SELECT * FROM academico_materias WHERE mat_nombre LIKE '%".$_GET["query"]."%' OR mat_siglas LIKE '%".$_GET["query"]."%'");
+                                        $dato = 3;
+                                        $consulta = mysqli_query($conexion, "SELECT * FROM academico_materias 
+                                        WHERE mat_nombre LIKE '%".$_GET["query"]."%' 
+                                        OR mat_siglas LIKE '%".$_GET["query"]."%'");
                                     }
                                     $numDatos=mysqli_num_rows($consulta);
                                     if ($numDatos<=0) {
-                                        $dato=4;
-                                        $consulta = mysqli_query($conexion, "SELECT * FROM academico_grados WHERE gra_nombre LIKE '%".$_GET["query"]."%' OR gra_codigo LIKE '%".$_GET["query"]."%'");
+                                        $dato = 4;
+                                        $consulta = mysqli_query($conexion, "SELECT * FROM academico_grados 
+                                         WHERE gra_nombre LIKE '%".$_GET["query"]."%' 
+                                         OR gra_codigo LIKE '%".$_GET["query"]."%'");
                                     }
                                     
                                     while($resultado = mysqli_fetch_array($consulta, MYSQLI_BOTH)){
                                         switch ($dato) {
                                             case 1:
-                                                $nombre=$resultado['pagp_pagina'];
-                                                $descripcion=$resultado['pagp_descripcion'];
-                                                $ruta=$resultado['pagp_ruta'];
-                                                $nombreRuta=$resultado['pagp_ruta'];
+                                                $nombre      = $resultado['pagp_pagina'];
+                                                $descripcion = $resultado['pagp_descripcion'];
+                                                $ruta        = $resultado['pagp_ruta'];
+                                                $nombreRuta  = $resultado['pagp_ruta'];
                                                 if ($resultado['pagp_parametro']!=1) {
                                                     $ruta="page-info.php?idmsg=303&idPagina='".$resultado['pagp_id']."'";
                                                 }
@@ -68,24 +78,28 @@
                                                 switch ($resultado['uss_tipo']){
                                                     case 1:
                                                         $usuarioTipo="DEV";
+                                                        $ruta="usuarios-editar.php?id=".$resultado['uss_id'];
                                                     break;
                                                     case 2:
                                                         $usuarioTipo="Docente";
+                                                        $ruta="usuarios-editar.php?id=".$resultado['uss_id'];
                                                     break;
                                                     case 3:
                                                         $usuarioTipo="Acudiente";
+                                                        $ruta="usuarios-editar.php?id=".$resultado['uss_id'];
                                                     break;
                                                     case 4:
                                                         $usuarioTipo="Estudiante";
+                                                        $ruta="estudiantes-editar.php?id=".$resultado['uss_usuario'];
                                                     break;
                                                     case 5:
                                                         $usuarioTipo="Directivo";
+                                                        $ruta="usuarios-editar.php?id=".$resultado['uss_id'];
                                                     break;
 
                                                 }
                                                 $nombre=$resultado['uss_nombre'];
-                                                $descripcion="";
-                                                $ruta="usuarios-editar.php?id=".$resultado['uss_id'];
+                                                $descripcion=$resultado['uss_usuario']." - ".$usuarioTipo."";
                                                 $nombreRuta="";
                                                 break;
 
@@ -101,18 +115,15 @@
                                                 $descripcion="";
                                                 $ruta="cursos-editar.php?id=".$resultado['gra_id'];
                                                 $nombreRuta="";
-                                                break;  
-
+                                                break;
                                         }
  
                                 ?>
                                 <p>
-                                    <h3 class="text-transform: uppercase"; style="margin: 0px";><a href="<?=$ruta;?>"><?=$nombre;?>( <?=$usuarioTipo;?>)</a></h3>
+                                    <h3 class="text-transform: uppercase"; style="margin: 0px";><a href="<?=$ruta;?>"><?=$nombre;?></a></h3>
                                     <h6 class="text-transform: uppercase"; style="margin: 0px";><a href="<?=$ruta;?>"><?=$nombreRuta;?></a></h6>
                                     <p><?=$descripcion;?></P>
                                 </p>
-
-                                    
                                 <?php
                                     }
                                 ?>
