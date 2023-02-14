3dbc1b78cd19a5055826e9ea03a7a200472df971
Enuar Lara <enuar2110@gmail.com>
2023-02-14 08:36:03 -0500

    SN00- Se crea metodo para validar usuarios

--- main-app/class/Usuarios.php
+++ main-app/class/Usuarios.php
@@ -2,5 +2,25 @@
 
 class Usuarios {
 
+    public static function validarExistenciaUsuario($usuario = 0)
+    {
+
+        global $conexion;
+        $num = 0;
+
+        try {
+            $consulta = mysqli_query($conexion, "SELECT * FROM usuarios
+            WHERE (uss_id='".$usuario."' || uss_usuario='".$usuario."' || uss_email='".$usuario."') AND uss_bloqueado=0
+            ");
+            $num = mysqli_num_rows($consulta);
+        } catch (Exception $e) {
+            echo "Excepción catpurada: ".$e->getMessage();
+            exit();
+        }
+
+        return $num;
+
+    }
+
 
 }
