04602a4dd5310a2460952036b4da26829fddfca9
Enuar Lara <enuar2110@gmail.com>
2023-02-14 08:58:22 -0500

    SN00- Se crea metodo para odtener datos de usuarios

--- main-app/class/Usuarios.php
+++ main-app/class/Usuarios.php
@@ -2,6 +2,31 @@
 
 class Usuarios {
 
+    public static function obtenerDatosUsuario($usuario = 0)
+    {
+
+        global $conexion;
+        $resultado = [];
+
+        try {
+            $consulta = mysqli_query($conexion, "SELECT * FROM usuarios
+            WHERE (uss_id='".$usuario."' || uss_usuario='".$usuario."') AND uss_bloqueado=0
+            ");
+            $num = mysqli_num_rows($consulta);
+            if($num == 0){
+                echo "Este usuario no existe";
+                exit();
+            }
+            $resultado = mysqli_fetch_array($consulta, MYSQLI_BOTH);
+        } catch (Exception $e) {
+            echo "Excepción catpurada: ".$e->getMessage();
+            exit();
+        }
+
+        return $resultado;
+
+    }
+
     public static function validarExistenciaUsuario($usuario = 0)
     {
 
