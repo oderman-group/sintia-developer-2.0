6216f5c7c141d565780f11a8459cb67e55cd1de3
Enuar Lara <enuar2110@gmail.com>
2023-02-16 09:50:32 -0500

    SN00- Se añaden campos para guardar documento y nombres

--- main-app/directivo/usuarios-guardar.php
+++ main-app/directivo/usuarios-guardar.php
@@ -8,16 +8,16 @@ $consultaUsuarioA = mysqli_query($conexion, "SELECT * FROM usuarios WHERE uss_us
 $numUsuarioA = mysqli_num_rows($consultaUsuarioA);
 $datosUsuarioA = mysqli_fetch_array($consultaUsuarioA, MYSQLI_BOTH);
 if ($numUsuarioA > 0) {
-    echo '<script type="text/javascript">window.location.href="usuarios-agregar.php?error=ER_DT_1&usuario='.$_POST["usuario"].'&nombre='.$_POST["nombre"].'&email='.$_POST["email"].'&celular='.$_POST["celular"].'&genero='.$_POST["genero"].'&tipoUsuario='.$_POST["tipoUsuario"].'";</script>';
+    echo '<script type="text/javascript">window.location.href="usuarios-agregar.php?error=ER_DT_1&usuario='.$_POST["usuario"].'&nombre='.$_POST["nombre"].'&nombre2='.$_POST["nombre2"].'&apellido1='.$_POST["apellido1"].'&apellido2='.$_POST["apellido2"].'&tipoD='.$_POST["tipoD"].'&documento='.$_POST["documento"].'&email='.$_POST["email"].'&celular='.$_POST["celular"].'&genero='.$_POST["genero"].'&tipoUsuario='.$_POST["tipoUsuario"].'";</script>';
     exit();
 }
 $validarClave=validarClave($_POST["clave"]);
 if($validarClave!=true){
-    echo '<script type="text/javascript">window.location.href="usuarios-agregar.php?error=5&usuario='.$_POST["usuario"].'&nombre='.$_POST["nombre"].'&email='.$_POST["email"].'&celular='.$_POST["celular"].'&genero='.$_POST["genero"].'&tipoUsuario='.$_POST["tipoUsuario"].'";</script>';
+    echo '<script type="text/javascript">window.location.href="usuarios-agregar.php?error=5&usuario='.$_POST["usuario"].'&nombre='.$_POST["nombre"].'&nombre2='.$_POST["nombre2"].'&apellido1='.$_POST["apellido1"].'&apellido2='.$_POST["apellido2"].'&tipoD='.$_POST["tipoD"].'&documento='.$_POST["documento"].'&email='.$_POST["email"].'&celular='.$_POST["celular"].'&genero='.$_POST["genero"].'&tipoUsuario='.$_POST["tipoUsuario"].'";</script>';
     exit();
 }
 mysqli_query($conexion, "INSERT INTO usuarios (uss_usuario, uss_clave, uss_tipo, uss_nombre, uss_estado, uss_email, uss_celular, uss_genero, uss_foto, uss_portada, uss_idioma, uss_tema, uss_permiso1, uss_bloqueado, uss_fecha_registro, uss_responsable_registro, uss_ocupacion, uss_intentos_fallidos, uss_tema_sidebar,
-uss_tema_header, uss_tema_logo)VALUES(
+uss_tema_header, uss_tema_logo,uss_tipo_documento,uss_apellido1,uss_apellido2,uss_nombre2,uss_documento)VALUES(
     '" . $_POST["usuario"] . "',
     '" . $_POST["clave"] . "',
     " . $_POST["tipoUsuario"] . ",
@@ -38,7 +38,12 @@ uss_tema_header, uss_tema_logo)VALUES(
     0,
     'cyan-sidebar-color',
 	'header-indigo',
-	'logo-indigo'
+	'logo-indigo', 
+    '" . $_POST["tipoD"] . "',
+    '" . $_POST["apellido1"] . "',
+    '" . $_POST["apellido2"] . "',
+    '" . $_POST["nombre2"] . "',
+    '" . $_POST["documento"] . "'
     )");
 $idRegistro = mysqli_insert_id($conexion);
 $lineaError = __LINE__;
