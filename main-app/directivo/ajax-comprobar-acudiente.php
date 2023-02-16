b39c5179be894c2cb139b6e7740d09315916d190
herazo1997 <carlosherazo1997@gmail.com>
2023-02-16 15:19:23 -0500

    ts_064 Se agrega archivo para la consulta del acudiente

---
+++ main-app/directivo/ajax-comprobar-acudiente.php
@@ -0,0 +1,22 @@
+<?php
+	include('session.php');
+		if(isset($_POST['buscar']))
+    { 
+    	$doc = $_POST['uss_usuario'];
+    	$valores = array();
+    	$valores['existe'] = "0"; 
+
+		  $resultados = mysqli_query($conexion,"SELECT * FROM usuarios WHERE uss_usuario = '$doc' or uss_documento = '$doc'");
+		  while($consulta = mysqli_fetch_array($resultados))
+		  {
+		  	$valores['existe'] = "1"; 
+		  	$valores['nombre1'] = $consulta['uss_nombre'];
+        $valores['nombre2'] = $consulta['uss_nombre2'];
+		  	$valores['apellido1'] = $consulta['uss_apellido1'];
+		  	$valores['apellido2'] = $consulta['uss_apellido2'];	
+        $valores['lugardE'] = $consulta['uss_lugar_expedicion'];		    
+		  }
+		  
+		  $valores = json_encode($valores);
+			echo $valores;
+    }
