0b0fe3d010c28156393dee03785de25fa51e7166
marvin-garcia-yepes <76126340+marvin-garcia-yepes@users.noreply.github.com>
2023-02-16 08:03:26 -0500

    TS-053 Se elimina el include, cambio de variables y se agrega div class paginación

--- main-app/directivo/cargas-paginacion-listados.php
+++ main-app/directivo/cargas-paginacion-listados.php
@@ -1,41 +1,56 @@
+<html>
+<body>
+    
+ <?php
+ $elementos_por_pagina = 10;
+ $lista = $cargaSP;
+ $total_elementos = count($lista);
+ $total_paginas = ceil($cargaSP / $elementos_por_pagina);
 
-<?php include("../directivo/cargas.php");?>
-<?php
-
-$elementos_por_pagina = 10;
-
-$lista = $resultado;
-$consulta = count($lista);
-$total_paginas = ceil($consulta / $elementos_por_pagina);
-
-if (empty($_GET['pagina'])) {
+ if (!isset($_GET['pagina'])) {
     $pagina_actual = 1;
-} else {
+ } else {
     $pagina_actual = $_GET['pagina'];
-}
+ }
 
-if ($pagina_actual < 1) {
+ if ($pagina_actual < 1) {
     $pagina_actual = 1;
-} elseif ($pagina_actual > $total_paginas) {
+ } elseif ($pagina_actual > $total_paginas) {
     $pagina_actual = $total_paginas;
-}
+ }
 
-$inicio = ($pagina_actual - 1) * $elementos_por_pagina;
-$pagina_actuale = array_slice($lista, $inicio, $elementos_por_pagina);
+ $inicio = ($pagina_actual - 1) * $elementos_por_pagina;
+ $elementos_actuales = array_slice($lista, $inicio, $elementos_por_pagina);
 
-foreach ($elementos_actuales as $elemento) {
+ foreach ($elementos_actuales as $elemento) {
     echo $elemento . "<br>";
-}
+ }
 
-echo "<br>";
-echo "Página " . $pagina_actual . " de " . $total_paginas;
-echo "<br>";
+ ?>
+ <div class="paginacion">
+    <ul>
+        <?php
+           echo "<br>";
+           echo "Total " . $pagina_actual . " de " . $total_paginas;
+           echo "<br>";
 
-if ($pagina_actual > 1) {
-    echo '<a href="?pagina=' . ($pagina_actual - 1) . '">Anterior</a> ';
-}
+          if ($pagina_actual > 1) {
+              echo '<a href="?pagina=' . ($pagina_actual - 1) . '">Anterior</a> ';
+            }
+             for ($i=1; $i <= $total_paginas; $i++){
+             if ($i == $pagina) {
+                echo '<li class="pageSelected">'.$i.'</li>';
+            } else{
+                 echo '<li><a "href=?pagina='.$i.'">'.$i.'</a></li>';
+                }   
+            }
 
-if ($pagina_actual < $total_paginas) {
-    echo '<a href="?pagina=' . ($pagina_actual + 1) . '">Siguiente</a>';
-}
-?>
+            if ($pagina_actual < $total_paginas) {
+            echo '<a href="?pagina=' . ($pagina_actual + 1) . '">Siguiente</a>';
+           }
+        ?>
+    </ul>
+ </div>
+ <script src="../../config-general/assets/css/theme/light/style.css ></script>
+</body>
+</html>
