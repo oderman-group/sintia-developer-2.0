e6856e44f4f942366aa210298d0df8ca453ac7c4
Jhon Oderman <jhooderman@gmail.com>
2023-02-19 09:16:41 -0500

    Modificar etiquetas de analytics

--- config-general/analytics/instituciones.php
+++ config-general/analytics/instituciones.php
@@ -1,9 +1,42 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-52643833-14"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+<?php
+switch($_SERVER['HTTP_HOST']){
+	case 'localhost':
+    echo "
+    <script async src='https://www.googletagmanager.com/gtag/js?id=UA-52643833-14'></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
 
-  gtag('config', 'UA-52643833-14');
-</script>
+      gtag('config', 'UA-52643833-14');
+    </script>
+    ";
+    break;
+
+	case 'developer.plataformasintia.com':
+    echo "
+    <script async src='https://www.googletagmanager.com/gtag/js?id=UA-52643833-14'></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-52643833-14');
+    </script>
+    ";
+	  break;
+
+	case 'main.plataformasintia.com':
+    echo "
+    <script async src='https://www.googletagmanager.com/gtag/js?id=G-H2QYYN10VP'></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-H2QYYN10VP');
+    </script>
+    ";
+    break;
+}
