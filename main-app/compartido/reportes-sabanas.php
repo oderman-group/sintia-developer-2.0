69721fe7b92782cc6fbe02d2065a381a3d6a4a9f
Enuar Lara <enuar2110@gmail.com>
2023-02-21 21:44:09 -0500

    TD-48- Se modifican consultas y se añade BD para los año

--- main-app/compartido/reportes-sabanas.php
+++ main-app/compartido/reportes-sabanas.php
@@ -3,9 +3,15 @@ session_start();
 include("../../config-general/config.php");
 include("../../config-general/consulta-usuario-actual.php");?>
 <?php
-$asig=mysqli_query($conexion, "SELECT * FROM academico_matriculas WHERE mat_grado='".$_GET["curso"]."' AND mat_grupo='".$_GET["grupo"]."' AND (mat_estado_matricula=1 OR mat_estado_matricula=2) AND mat_eliminado=0 ORDER BY mat_primer_apellido");			
+$year=$agnoBD;
+if(isset($_POST["year"])){
+$year=$_POST["year"];
+}
+$BD=$_SESSION["inst"]."_".$year;
+
+$asig=mysqli_query($conexion, "SELECT * FROM $BD.academico_matriculas WHERE mat_grado='".$_REQUEST["curso"]."' AND mat_grupo='".$_REQUEST["grupo"]."' AND (mat_estado_matricula=1 OR mat_estado_matricula=2) AND mat_eliminado=0 ORDER BY mat_primer_apellido");			
 $num_asg=mysqli_num_rows($asig);
-$consultaGrados=mysqli_query($conexion, "SELECT * FROM academico_grados, academico_grupos WHERE gra_id='".$_GET["curso"]."' AND gru_id='".$_GET["grupo"]."'");
+$consultaGrados=mysqli_query($conexion, "SELECT * FROM $BD.academico_grados, academico_grupos WHERE gra_id='".$_REQUEST["curso"]."' AND gru_id='".$_REQUEST["grupo"]."'");
 $grados = mysqli_fetch_array($consultaGrados, MYSQLI_BOTH);
 ?>
 <head>
@@ -17,8 +23,8 @@ $grados = mysqli_fetch_array($consultaGrados, MYSQLI_BOTH);
 <div align="center" style="margin-bottom:20px;">
    <img src="../files/images/logo/<?=$informacion_inst["info_logo"]?>" height="150" width="250"><br>
     <?=$informacion_inst["info_nombre"]?><br>
-    INFORME DE SABANAS - PERIODO: <?=$_GET["per"];?></br>
-    <b><?=strtoupper($grados["gra_nombre"]." ".$grados["gru_nombre"]);?></b><br>
+    INFORME DE SABANAS - PERIODO: <?=$_REQUEST["per"];?></br>
+    <b><?=strtoupper($grados["gra_nombre"]." ".$grados["gru_nombre"]." ".$year);?></b><br>
 </div>   
   <table bgcolor="#FFFFFF" width="80%" cellspacing="5" cellpadding="5" rules="all" border="<?php echo $config[13] ?>" style="border:solid; border-color:<?php echo $config[11] ?>;" align="center">
   <tr style="font-weight:bold; font-size:12px; height:30px; background:<?php echo $config[12] ?>;">
@@ -27,9 +33,9 @@ $grados = mysqli_fetch_array($consultaGrados, MYSQLI_BOTH);
         <td align="center">Estudiante</td>
         <!--<td align="center">Gru</td>-->
         <?php
-		$materias1=mysqli_query($conexion, "SELECT * FROM academico_cargas WHERE car_curso=".$_GET["curso"]." AND car_grupo='".$_GET["grupo"]."'");
+		$materias1=mysqli_query($conexion, "SELECT * FROM $BD.academico_cargas WHERE car_curso=".$_REQUEST["curso"]." AND car_grupo='".$_REQUEST["grupo"]."'");
 		while($mat1=mysqli_fetch_array($materias1, MYSQLI_BOTH)){
-			$nombresMat=mysqli_query($conexion, "SELECT * FROM academico_materias WHERE mat_id=".$mat1[4]);
+			$nombresMat=mysqli_query($conexion, "SELECT * FROM $BD.academico_materias WHERE mat_id=".$mat1[4]);
 			$Mat=mysqli_fetch_array($nombresMat, MYSQLI_BOTH);
 		?>
         	<td align="center"><?=strtoupper($Mat[3]);?></td>      
@@ -44,7 +50,7 @@ $grados = mysqli_fetch_array($consultaGrados, MYSQLI_BOTH);
   $nombreMayor="";
   while($fila=mysqli_fetch_array($asig, MYSQLI_BOTH))
   {
-  		$cuentaest=mysqli_query($conexion, "SELECT * FROM academico_boletin WHERE bol_estudiante=".$fila[0]." AND bol_periodo=".$_GET["per"]." GROUP BY bol_carga");
+  		$cuentaest=mysqli_query($conexion, "SELECT * FROM $BD.academico_boletin WHERE bol_estudiante=".$fila[0]." AND bol_periodo=".$_REQUEST["per"]." GROUP BY bol_carga");
 		$numero=mysqli_num_rows($cuentaest);
 		$def='0.0';
 		
@@ -56,9 +62,9 @@ $grados = mysqli_fetch_array($consultaGrados, MYSQLI_BOTH);
       <!--<td align="center"><?php if($fila[7]==1)echo "A"; else echo "B";?></td> -->
        <?php
 		$suma=0;
-		$materias1=mysqli_query($conexion, "SELECT * FROM academico_cargas WHERE car_curso=".$_GET["curso"]." AND car_grupo='".$_GET["grupo"]."'");
+		$materias1=mysqli_query($conexion, "SELECT * FROM $BD.academico_cargas WHERE car_curso=".$_REQUEST["curso"]." AND car_grupo='".$_REQUEST["grupo"]."'");
 		while($mat1=mysqli_fetch_array($materias1, MYSQLI_BOTH)){
-			$notas=mysqli_query($conexion, "SELECT * FROM academico_boletin WHERE bol_estudiante=".$fila[0]." AND bol_carga=".$mat1[0]." AND bol_periodo=".$_GET["per"]);
+			$notas=mysqli_query($conexion, "SELECT * FROM $BD.academico_boletin WHERE bol_estudiante=".$fila[0]." AND bol_carga=".$mat1[0]." AND bol_periodo=".$_REQUEST["per"]);
 			$nota=mysqli_fetch_array($notas, MYSQLI_BOTH);
 			$defini = $nota[4];
 			if($defini<$config[5]) $color='red'; else $color='blue';
@@ -139,7 +145,7 @@ $grados = mysqli_fetch_array($consultaGrados, MYSQLI_BOTH);
   
   </center>
 	<div align="center" style="font-size:10px; margin-top:10px;">
-      <img src="../files/images/sintia.png" height="50" width="100"><br>
+      <img src="../sintia-logo-2023.png" height="50" width="100"><br>
       SINTIA -  SISTEMA INTEGRAL DE GESTI&Oacute;N INSTITUCIONAL
     </div>
 </body>
