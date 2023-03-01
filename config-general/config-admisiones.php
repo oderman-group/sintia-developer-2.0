2e68999ef8b0600d256f1a77f4ef09c8df14129f
Jhon Oderman <jhooderman@gmail.com>
2023-03-01 04:08:00 -0500

    Crear configuración de admisiones

---
+++ config-general/config-admisiones.php
@@ -0,0 +1,24 @@
+<?php
+#VARIABLES PARA SUBMODULO DE ADMISIONES
+$estadosSolicitud = array(
+	1 => 'VERIFICACIÓN DE PAGO', 
+	2 => 'PAGO RECHAZADO', 
+	3 => 'PENDIENTE POR DILIGENCIAR EL FORMULARIO',
+	4 => 'EN PROCESO',
+	5 => 'EXAMEN Y ENTREVISTA', 
+	6 => 'APROBADO', 
+	7 => 'NO APROBADO',
+	8 => 'VERIFICACIÓN DE CUPO DISPONIBLE',
+	9 => 'MOVIDO AL AÑO SIGUIENTE'
+);
+$progresoSolicitud = array(
+	1 => '15%', 
+	2 => '15%', 
+	3 => '30%', 
+	4 => '60%',
+	5 => '75%', 
+	6 => '90%',
+	7 => '100%',
+	8 => '15%',
+	9 => '100%',
+);
