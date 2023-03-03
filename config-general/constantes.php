54ecd3e233602d1509df7f88cadf2dea9ff1a218
Jhon Oderman <jhooderman@gmail.com>
2023-03-03 04:40:50 -0500

    PES2023-122 - Mover el switch del host para definir constantes

--- config-general/constantes.php
+++ config-general/constantes.php
@@ -8,3 +8,19 @@ define('EMAIL_SENDER', 'info@plataformasintia.com');
 define('NAME_SENDER', 'Nombre constante');
 
 define('HEADER_EMAIL_BACKGROUND', '#6017dc');
+
+switch($_SERVER['HTTP_HOST']){
+	case 'localhost':
+        define('REDIRECT_ROUTE', 'http://localhost/app-sintia/main-app');
+        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
+        break;
+
+	case 'developer.plataformasintia.com':
+        define('REDIRECT_ROUTE', 'https://developer.plataformasintia.com/app-sintia/main-app');
+        break;
+
+	case 'main.plataformasintia.com':
+        define('REDIRECT_ROUTE', 'https://main.plataformasintia.com/app-sintia/main-app');
+        error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
+        break;
+}
