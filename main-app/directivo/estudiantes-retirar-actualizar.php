e6c5f3306c1e5de62db6e689b6de67c622a011bc
herazo1997 <carlosherazo1997@gmail.com>
2023-03-14 15:39:18 -0500

    PES2023-15 - Se hace el llamado al método de la clase estudiantes

--- main-app/directivo/estudiantes-retirar-actualizar.php
+++ main-app/directivo/estudiantes-retirar-actualizar.php
@@ -1,12 +1,10 @@
 <?php
 include("session.php");
 include("../modelo/conexion.php");
+include("../class/Estudiantes.php");
 
      if ($_POST["estadoMatricula"]==1){
-            mysqli_query($conexion, "INSERT INTO academico_matriculas_retiradas (matret_estudiante, matret_fecha, matret_motivo, matret_responsable)VALUES('".$_POST["estudiante"]."', now(), '".$_POST["motivo"]."', '".$_SESSION["id"]."')");
-
-            mysqli_query($conexion, "UPDATE academico_matriculas SET mat_estado_matricula=3 WHERE mat_id='".$_POST["estudiante"]."'");
-
+        Estudiantes::ActualizarEstadoMatricula();
      } elseif ($_POST["estadoMatricula"]==3){
             mysqli_query($conexion, "UPDATE academico_matriculas_retiradas SET matret_motivo='El Estudiante Estaba Retirado, Pero Fue Restaurado Exitosamente!' WHERE matret_estudiante='".$_POST["estudiante"]."'");
 
