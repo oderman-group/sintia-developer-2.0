ef15dc169d26309be9f0d635d9f7db44f05c8aba
Jhon Oderman <jhooderman@gmail.com>
2023-03-16 06:36:23 -0500

    PES2023-138 - Script SQL del cambio

---
+++ sql/pes2023_138.sql
@@ -0,0 +1,1 @@
+ALTER TABLE mobiliar_dev_2023.academico_matriculas ADD mat_tipo_matricula varchar(45) DEFAULT 'grupal' NOT NULL COMMENT 'Se requiere para definir si el estudiante podrá estar en varios cursos a la vez';
