4ffa42f9b72c651aed352fb482422f95b5909a50
herazo1997 <carlosherazo1997@gmail.com>
2023-03-16 19:20:51 -0500

    PES2023-100 - Se crea archivo nuevo para la petición ajax

---
+++ main-app/directivo/ajax-estudiantes-editar.php
@@ -0,0 +1,38 @@
+<?php 
+include("session.php");
+$consultaDoc=mysqli_query($conexion, "SELECT mat_documento FROM academico_matriculas
+WHERE mat_documento ='".$_POST["nDoct"]."' AND mat_eliminado=0");
+$numDotos=mysqli_num_rows($consultaDoc);
+if ($numDotos > 0) {
+    include("../class/Estudiantes.php");
+    $datosEstudianteActual = Estudiantes::obtenerDatosEstudiante($_POST["nDoct"]);
+    $nombreEstudiante = Estudiantes::NombreCompletoDelEstudiante($datosEstudianteActual);
+?>
+    <script type="application/javascript">
+        document.getElementById('apellido1').disabled = 'disabled';
+        document.getElementById('apellido2').disabled = 'disabled';
+        document.getElementById('nombres').disabled = 'disabled';
+        document.getElementById('nDoc').style.backgroundColor = "#f8d7da";
+    </script>   
+    
+    <div class="alert alert-danger">
+        <button type="button" class="close" data-dismiss="alert">&times;</button>
+
+        <p>
+            Este número de documento se encuentra registrado y asociado al estudiante <b><?=$nombreEstudiante;?></b>.<br>
+        </p>
+        
+
+    </div>
+<?php
+    exit();
+}else{
+?>
+    <script type="application/javascript">
+        document.getElementById('apellido1').disabled = '';
+        document.getElementById('apellido2').disabled = '';
+        document.getElementById('nombres').disabled = '';
+        document.getElementById('nDoc').style.backgroundColor = "";
+    </script> 
+<?php    
+}
