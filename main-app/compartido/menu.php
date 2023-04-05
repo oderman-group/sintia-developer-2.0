089118b684744adde573c52b722a4231508042f3
Jhon Oderman <jhooderman@gmail.com>
2023-04-05 18:33:44 -0500

    Quitar cantidades al acceder a paginas

--- main-app/compartido/menu.php
+++ main-app/compartido/menu.php
@@ -98,7 +98,7 @@
 	                            </a>
 	                            <ul class="sub-menu">
 	                                <li class="nav-item"><a href="inscripciones.php" class="nav-link "> <span class="title">Inscripciones</span></a></li>
-	                                <li class="nav-item"><a href="estudiantes.php?cantidad=10" class="nav-link "> <span class="title"><?=$frases[209][$datosUsuarioActual[8]];?></span></a></li>
+	                                <li class="nav-item"><a href="estudiantes.php" class="nav-link "> <span class="title"><?=$frases[209][$datosUsuarioActual[8]];?></span></a></li>
 									<li class="nav-item"><a href="cursos.php" class="nav-link "> <span class="title"><?=$frases[5][$datosUsuarioActual[8]];?></span></a></li>
 									<li class="nav-item"><a href="areas.php" class="nav-link "> <span class="title"><?=$frases[93][$datosUsuarioActual[8]];?></span></a></li>
 									<li class="nav-item"><a href="asignaturas.php" class="nav-link "> <span class="title"><?=$frases[73][$datosUsuarioActual[8]];?></span></a></li>
@@ -147,7 +147,7 @@
 	                                <span class="title"><?=$frases[87][$datosUsuarioActual['uss_idioma']];?></span> <span class="arrow"></span>
 	                            </a>
 	                            <ul class="sub-menu">
-	                                <li class="nav-item"><a href="usuarios.php?cantidad=100" class="nav-link "> <span class="title"><?=$frases[75][$datosUsuarioActual[8]];?></span></a></li>
+	                                <li class="nav-item"><a href="usuarios.php" class="nav-link "> <span class="title"><?=$frases[75][$datosUsuarioActual[8]];?></span></a></li>
 									<li class="nav-item"><a href="solicitudes.php" class="nav-link "> <span class="title">Solicitud desbloqueo</span></a></li>
 									<li class="nav-item"><a href="galeria.php" class="nav-link "> <span class="title"><?=$frases[223][$datosUsuarioActual[8]];?></span></a></li>
 
