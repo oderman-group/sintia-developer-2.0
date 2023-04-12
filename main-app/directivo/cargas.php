732688a253e1ad79385c43d3f2948a7626488539
Jhon Oderman <jhooderman@gmail.com>
2023-04-12 07:07:26 -0500

    Mostrar cargas sin asociación

--- main-app/directivo/cargas.php
+++ main-app/directivo/cargas.php
@@ -91,10 +91,10 @@ $Plataforma = new Plataforma;
 													<?php
 													include("consulta-paginacion-cargas.php");											       
 													$busqueda=mysqli_query($conexion,"SELECT * FROM academico_cargas
-													  INNER JOIN academico_grados ON gra_id=car_curso
-													  INNER JOIN academico_grupos ON gru_id=car_grupo
-													  INNER JOIN academico_materias ON mat_id=car_materia
-													  INNER JOIN usuarios ON uss_id=car_docente
+													  LEFT JOIN academico_grados ON gra_id=car_curso
+													  LEFT JOIN academico_grupos ON gru_id=car_grupo
+													  LEFT JOIN academico_materias ON mat_id=car_materia
+													  LEFT JOIN usuarios ON uss_id=car_docente
 													  WHERE car_id=car_id $filtro
 												        ORDER BY car_id
 													    LIMIT $inicio,$registros;");
