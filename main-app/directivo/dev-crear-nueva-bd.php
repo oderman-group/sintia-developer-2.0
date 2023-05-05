98bb4d969ee36d64a6bf8293be804c8ce766c0ac
Enuar Lara <enuar2110@gmail.com>
2023-05-05 16:01:35 -0500

    PES2023-136 - Se descomenta archivo Jquery

--- main-app/directivo/dev-crear-nueva-bd.php
+++ main-app/directivo/dev-crear-nueva-bd.php
@@ -153,7 +153,7 @@ $institucionesConsulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServ
         <?php include("../compartido/footer.php");?>
     </div>
     <!-- start js include path -->
-    <!-- <script src="../../config-general/assets/plugins/jquery/jquery.min.js" ></script> -->
+    <script src="../../config-general/assets/plugins/jquery/jquery.min.js" ></script>
     <script src="../../config-general/assets/plugins/popper/popper.js" ></script>
     <script src="../../config-general/assets/plugins/jquery-blockui/jquery.blockui.min.js" ></script>
 	<script src="../../config-general/assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
