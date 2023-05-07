6c40fdb04a524258c408e12349c1e05949423ca0
Jhon Oderman <jhooderman@gmail.com>
2023-05-07 12:39:48 -0500

    PES2023-196 - Crear interface gráfica

---
+++ main-app/directivo/dev-ejecutar-scripts.php
@@ -0,0 +1,120 @@
+<?php include("session.php");?>
+<?php $idPaginaInterna = 'DV0002';?>
+<?php include("../compartido/historial-acciones-guardar.php");?>
+<?php include("../compartido/head.php");?>
+<!--bootstrap -->
+    <link href="../../config-general/assets/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+    <link href="../../config-general/assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.css" rel="stylesheet" media="screen">
+	<!-- Theme Styles -->
+    <link href="../../config-general/assets/css/pages/formlayout.css" rel="stylesheet" type="text/css" />
+	<!-- dropzone -->
+    <link href="../../config-general/assets/plugins/dropzone/dropzone.css" rel="stylesheet" media="screen">
+    <!--tagsinput-->
+    <link href="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.css" rel="stylesheet">
+    <!--select2-->
+    <link href="../../config-general/assets/plugins/select2/css/select2.css" rel="stylesheet" type="text/css" />
+    <link href="../../config-general/assets/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+</head>
+<!-- END HEAD -->
+<?php include("../compartido/body.php");?>
+    <div class="page-wrapper">
+        <?php include("../compartido/encabezado.php");?>
+		
+        <?php include("../compartido/panel-color.php");?>
+        <!-- start page container -->
+        <div class="page-container">
+ 			<?php include("../compartido/menu.php");?>
+			<!-- start page content -->
+            <div class="page-content-wrapper">
+                <div class="page-content">
+                    <div class="page-bar">
+                        <div class="page-title-breadcrumb">
+                            <div class="pull-left">
+                                <div class="page-title">Execute SQL Script</div>
+								<?php include("../compartido/texto-manual-ayuda.php");?>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="card card-box">
+                                <div class="card-head">
+                                    <header>Execute SQL Script</header>
+                                </div>
+                                <div class="card-body " id="bar-parent6">
+                                    <form class="form-horizontal" action="dev-ejecutar-scripts-guardar.php" method="post">
+										
+										<div class="form-group row">
+                                            <label class="col-sm-2 control-label">Enviroment</label>
+                                            <div class="col-sm-4">
+                                                <select class="form-control  select2" name="enviroment" required>
+                                                  <option value="LOCAL">Local</option>
+                                                  <option value="DEV">Testing</option>
+                                                  <option value="PROD">Production</option>
+                                                </select>
+                                            </div>
+                                        </div>
+										
+										<div class="form-group row">
+                                            <label class="col-sm-2 control-label">Script</label>
+                                            <div class="col-sm-10">  
+                                                <textarea name="script" class="form-control" rows="5" style="margin-top: 0px; margin-bottom: 0px; height: 100px; resize: none;"></textarea>
+                                            </div>
+                                        </div>
+										
+										<input type="submit" class="btn btn-primary" value="Execute Script">
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+						
+                        <div class="col-sm-3">
+                            <?php include("../compartido/publicidad-lateral.php");?>
+                        </div>
+						
+                    </div>
+					
+                </div>
+            </div>
+            <!-- end page content -->
+             <?php // include("../compartido/panel-configuracion.php");?>
+        </div>
+        <!-- end page container -->
+        <?php include("../compartido/footer.php");?>
+    </div>
+    <!-- start js include path -->
+    <script src="../../config-general/assets/plugins/jquery/jquery.min.js" ></script>
+    <script src="../../config-general/assets/plugins/popper/popper.js" ></script>
+    <script src="../../config-general/assets/plugins/jquery-blockui/jquery.blockui.min.js" ></script>
+	<script src="../../config-general/assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <!-- bootstrap -->
+    <script src="../../config-general/assets/plugins/bootstrap/js/bootstrap.min.js" ></script>
+    <script src="../../config-general/assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js" ></script>
+    <script src="../../config-general/assets/plugins/bootstrap-inputmask/bootstrap-inputmask.min.js" ></script>
+    <script src="../../config-general/assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js"  charset="UTF-8"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker-init.js"  charset="UTF-8"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js"  charset="UTF-8"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker-init.js"  charset="UTF-8"></script>
+    <!-- Common js-->
+	<script src="../../config-general/assets/js/app.js" ></script>
+    <script src="../../config-general/assets/js/layout.js" ></script>
+	<script src="../../config-general/assets/js/theme-color.js" ></script>
+	<!-- notifications -->
+	<script src="../../config-general/assets/plugins/jquery-toast/dist/jquery.toast.min.js" ></script>
+	<script src="../../config-general/assets/plugins/jquery-toast/dist/toast.js" ></script>	
+	<!-- Material -->
+	<script src="../../config-general/assets/plugins/material/material.min.js"></script>
+	<!-- dropzone -->
+    <script src="../../config-general/assets/plugins/dropzone/dropzone.js" ></script>
+    <!--tags input-->
+    <script src="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.js" ></script>
+    <script src="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input-init.js" ></script>
+    <!--select2-->
+    <script src="../../config-general/assets/plugins/select2/js/select2.js" ></script>
+    <script src="../../config-general/assets/js/pages/select2/select2-init.js" ></script>
+    <!-- end js include path -->
+</body>
+
+</html>
