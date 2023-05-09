93d1c055c367ae9addfc2b7308bc267f03e1b0a7
Enuar Lara <enuar2110@gmail.com>
2023-05-09 13:21:09 -0500

    PES2023-200 - Se añade enlace correspondiente para paginas

--- main-app/compartido/menu.php
+++ main-app/compartido/menu.php
@@ -212,7 +212,7 @@
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Historial de acciones</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Instituciones</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Módulos</span></a></li>
-										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Páginas</span></a></li>
+										<li class="nav-item"><a href="dev-paginas.php" class="nav-link"> <span class="title">Páginas</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Opciones generales</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Claves restauradas</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Contratos</span></a></li>
