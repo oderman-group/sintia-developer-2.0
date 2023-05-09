924d0586a9cfa8465a419db9a9517d62c6aa5913
Enuar Lara <enuar2110@gmail.com>
2023-05-09 12:02:06 -0500

    PES2023-136 - Se recarga variable del usuario actual al cambiar de año

--- config-general/consulta-usuario-actual.php
+++ config-general/consulta-usuario-actual.php
@@ -1,3 +1,9 @@
 <?php
 if(!isset($idSession) || $idSession==""){$idSession = $_SESSION["id"];}
 $datosUsuarioActual = $_SESSION["datosUsuario"];
+
+//SE RECARGA VARIABLE SESSION PARA EL USUARIO ACTUAL
+if(isset($_SESSION["yearAnterior"])){
+    $consultaUss = mysqli_query($conexion, "SELECT * FROM usuarios WHERE uss_id='".$datosUsuarioActual['uss_id']."'");
+    $datosUsuarioActual = mysqli_fetch_array($consultaUss, MYSQLI_BOTH);
+}
