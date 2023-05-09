2069c8e2d8c0cc792d25d6ac76a4e9c9a001bdd3
Enuar Lara <enuar2110@gmail.com>
2023-05-09 10:35:49 -0500

    PES2023-199 - Se añade enlace correspondiente para modulos

--- main-app/compartido/menu.php
+++ main-app/compartido/menu.php
@@ -211,7 +211,7 @@
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Log de errores</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Historial de acciones</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Instituciones</span></a></li>
-										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Módulos</span></a></li>
+										<li class="nav-item"><a href="dev-modulos.php" class="nav-link"> <span class="title">Módulos</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Páginas</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Opciones generales</span></a></li>
 										<li class="nav-item"><a href="#" class="nav-link"> <span class="title">Claves restauradas</span></a></li>
