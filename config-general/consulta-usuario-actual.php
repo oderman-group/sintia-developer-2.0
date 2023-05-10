1bfbcd9f64eb45c9810771b9732ee416e81cbbfe
Enuar Lara <enuar2110@gmail.com>
2023-05-10 12:09:04 -0500

    PES2023-136 - Mejora al recargar variable session del usuario

--- config-general/consulta-usuario-actual.php
+++ config-general/consulta-usuario-actual.php
@@ -1,9 +1,9 @@
 <?php
-if(!isset($idSession) || $idSession==""){$idSession = $_SESSION["id"];}
-$datosUsuarioActual = $_SESSION["datosUsuario"];
-
+require_once("../class/UsuariosPadre.php");
 //SE RECARGA VARIABLE SESSION PARA EL USUARIO ACTUAL
 if(isset($_SESSION["yearAnterior"])){
-    $consultaUss = mysqli_query($conexion, "SELECT * FROM usuarios WHERE uss_id='".$datosUsuarioActual['uss_id']."'");
-    $datosUsuarioActual = mysqli_fetch_array($consultaUss, MYSQLI_BOTH);
+	$_SESSION["datosUsuario"] = UsuariosPadre::sesionUsuario($_SESSION["id"]);
 }
+
+if(!isset($idSession) || $idSession==""){$idSession = $_SESSION["id"];}
+$datosUsuarioActual = $_SESSION["datosUsuario"];
