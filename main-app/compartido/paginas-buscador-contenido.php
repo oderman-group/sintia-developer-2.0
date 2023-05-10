c2750c3b2e925abf6bfa9583f877b34692e31b3f
Enuar Lara <enuar2110@gmail.com>
2023-05-10 12:36:32 -0500

    PES2023-99 - Se mejora buscador para mostrar páginas navegables

--- main-app/compartido/paginas-buscador-contenido.php
+++ main-app/compartido/paginas-buscador-contenido.php
@@ -27,21 +27,31 @@
                             <div class="card-body">
                                 <?php
                                     $filtro = '';
-                                    if(isset($_GET["query"])){$filtro .= " AND (pagp_pagina LIKE '%".$_GET["query"]."%' 
+
+                                    if(isset($_GET["query"])){
+                                        $filtro .= " AND (pagp_pagina LIKE '%".$_GET["query"]."%' 
                                         OR pagp_ruta LIKE '%".$_GET["query"]."%' 
-                                        OR pagp_palabras_claves LIKE '%".$_GET["query"]."%')";}
-                                    
-                                    $dato = 1;
-                                    $consulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".paginas_publicidad 
-                                    WHERE pagp_tipo_usuario='".$datosUsuarioActual['uss_tipo']."' $filtro ORDER BY pagp_id");
-                                    $tipoUsuario=$datosUsuarioActual['uss_tipo'];
-                                    
-                                    if($datosUsuarioActual['uss_tipo']==1){
-                                        $consulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".paginas_publicidad 
-                                        WHERE (pagp_tipo_usuario=1 OR pagp_tipo_usuario=5) $filtro ORDER BY pagp_id");
+                                        OR pagp_palabras_claves LIKE '%".$_GET["query"]."%')";
                                     }
+
+                                    $dato = 1;
+
+                                    $consulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".paginas_publicidad 
+                                    WHERE pagp_navegable=1 
+                                    AND pagp_tipo_usuario='".$datosUsuarioActual['uss_tipo']."' $filtro 
+                                    ORDER BY pagp_id");
+
+                                    if($datosUsuarioActual['uss_tipo']==1){
+
+                                        $consulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".paginas_publicidad 
+                                        WHERE pagp_navegable=1 
+                                        AND (pagp_tipo_usuario=1 OR pagp_tipo_usuario=5) $filtro 
+                                        ORDER BY pagp_id");
+
+                                    }
+
                                     $numDatos=mysqli_num_rows($consulta);
-                                    if($tipoUsuario == 5){
+                                    if($datosUsuarioActual['uss_tipo'] == 5 || $datosUsuarioActual['uss_tipo'] == 1){
                                         //buscador usuarios
                                         if ($numDatos<=0){
                                             $dato = 2;
