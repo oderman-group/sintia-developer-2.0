f42d820cd09a88ee305f477466a40cfe35116d41
Enuar Lara <enuar2110@gmail.com>
2023-05-11 18:54:45 -0500

    PES2023-84 - Se añade paginación y barra superior a estudiantes de docentes

---
+++ main-app/directivo/includes/consulta-paginacion-estudiantes-todos.php
@@ -0,0 +1,13 @@
+<?php
+    $nombrePagina="estudiantes-todos.php";
+    if($_REQUEST["nume"] == "" ){$_REQUEST["nume"] = "1";}
+    $consulta = Estudiantes::listarEstudiantesParaDocentes($filtro,'');
+    $numRegistros=mysqli_num_rows($consulta);
+    $registros= $config['conf_num_registros'];
+    $pagina=$_REQUEST["nume"];
+    if (is_numeric($pagina)){
+        $inicio= (($pagina-1)*$registros);
+    }			     
+    else{
+        $inicio=1;
+    }
