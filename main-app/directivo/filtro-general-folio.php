1e8cfbb7385ca4508da9a5918e2cb1cbca1671eb
Enuar Lara <enuar2110@gmail.com>
2023-05-15 17:11:28 -0500

    PES2023-223 - Se crea interfaz para generar folios

---
+++ main-app/directivo/filtro-general-folio.php
@@ -0,0 +1,134 @@
+<?php
+include("session.php");
+
+$idPaginaInterna = 'DT0149';
+
+include("../compartido/historial-acciones-guardar.php");
+include("../compartido/head.php");
+?>
+
+<!--bootstrap -->
+<link href="../../config-general/assets/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+<link href="../../config-general/assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.css" rel="stylesheet" media="screen">
+<!-- Theme Styles -->
+<link href="../../config-general/assets/css/pages/formlayout.css" rel="stylesheet" type="text/css" />
+<!-- dropzone -->
+<link href="../../config-general/assets/plugins/dropzone/dropzone.css" rel="stylesheet" media="screen">
+<!--tagsinput-->
+<link href="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.css" rel="stylesheet">
+<!--select2-->
+<link href="../../config-general/assets/plugins/select2/css/select2.css" rel="stylesheet" type="text/css" />
+<link href="../../config-general/assets/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+</head>
+<!-- END HEAD -->
+<?php include("../compartido/body.php"); ?>
+<div class="page-wrapper">
+    <?php include("../compartido/encabezado.php"); ?>
+
+    <?php include("../compartido/panel-color.php"); ?>
+    <!-- start page container -->
+    <div class="page-container">
+        <?php include("../compartido/menu.php"); ?>
+        <!-- start page content -->
+        <div class="page-content-wrapper">
+            <div class="page-content">
+                <div class="page-bar">
+                    <div class="page-title-breadcrumb">
+                        <div class=" pull-left">
+                            <div class="page-title">Generar Folios</div>
+                            <?php include("../compartido/texto-manual-ayuda.php"); ?>
+                        </div>
+                        <ol class="breadcrumb page-breadcrumb pull-right">
+                            <li><a class="parent-item" href="#" name="estudiantes.php" onClick="deseaRegresar(this)">Estudiantes</a>&nbsp;<i class="fa fa-angle-right"></i></li>
+                            <li class="active">Generar Folios</li>
+                        </ol>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="panel">
+                            <header class="panel-heading panel-heading-purple">Filtros</header>
+                            <div class="panel-body">
+                                <form name="formularioGuardar" action="generar-folio-guardar.php" method="post">
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 control-label">Número de inicio de folio</label>
+                                        <div class="col-sm-8">
+                                            <input type="number" name="numFolio" class="form-control col-sm-2" required>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 control-label">Estudiantes a generar folio</label>
+                                        <div class="col-sm-4">
+                                            <select class="form-control  select2" name="tipoEstudiantes">
+                                                <option value="">Seleccione una opción</option>
+                                                <option value="0">Todos</option>
+                                                <option value="1">Matriculados</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 control-label">Año</label>
+                                        <div class="col-sm-4">
+                                            <select class="form-control  select2" name="year" required>
+                                                <option value="">Seleccione una opción</option>
+                                                <?php
+                                                while ($yearStart <= $yearEnd) {
+                                                    if ($_SESSION["bd"] == $yearStart)
+                                                        echo "<option value='" . $yearStart . "' selected style='color:blue;'>" . $yearStart . "</option>";
+                                                    else
+                                                        echo "<option value='" . $yearStart . "'>" . $yearStart . "</option>";
+                                                    $yearStart++;
+                                                }
+                                                ?>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <input type="submit" class="btn btn-primary" value="Generar informe">&nbsp;
+
+                                    <a href="#" name="informes-todos.php" class="btn btn-secondary" onClick="deseaRegresar(this)"><i class="fa fa-long-arrow-left"></i>Regresar</a>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- end page container -->
+            <?php include("../compartido/footer.php"); ?>
+        </div>
+        <!-- start js include path -->
+        <script src="../../config-general/assets/plugins/jquery/jquery.min.js"></script>
+        <script src="../../config-general/assets/plugins/popper/popper.js"></script>
+        <script src="../../config-general/assets/plugins/jquery-blockui/jquery.blockui.min.js"></script>
+        <script src="../../config-general/assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+        <!-- bootstrap -->
+        <script src="../../config-general/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+        <script src="../../config-general/assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+        <script src="../../config-general/assets/plugins/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
+        <script src="../../config-general/assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js" charset="UTF-8"></script>
+        <script src="../../config-general/assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker-init.js" charset="UTF-8"></script>
+        <script src="../../config-general/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+        <script src="../../config-general/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker-init.js" charset="UTF-8"></script>
+        <!-- Common js-->
+        <script src="../../config-general/assets/js/app.js"></script>
+        <script src="../../config-general/assets/js/layout.js"></script>
+        <script src="../../config-general/assets/js/theme-color.js"></script>
+        <!-- notifications -->
+        <script src="../../config-general/assets/plugins/jquery-toast/dist/jquery.toast.min.js"></script>
+        <script src="../../config-general/assets/plugins/jquery-toast/dist/toast.js"></script>
+        <!-- Material -->
+        <script src="../../config-general/assets/plugins/material/material.min.js"></script>
+        <!-- dropzone -->
+        <script src="../../config-general/assets/plugins/dropzone/dropzone.js"></script>
+        <!--tags input-->
+        <script src="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.js"></script>
+        <script src="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input-init.js"></script>
+        <!--select2-->
+        <script src="../../config-general/assets/plugins/select2/js/select2.js"></script>
+        <script src="../../config-general/assets/js/pages/select2/select2-init.js"></script>
+        <!-- end js include path -->
+    </body>
+</html>
