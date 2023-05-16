59928b1dcbe3b664f7c27a74ef23a8cf7ed8e508
Enuar Lara <enuar2110@gmail.com>
2023-05-16 12:38:44 -0500

    PES2023-86 - Se cambia forma de leer archivo para mostrar solo 20 ultimos

--- main-app/directivo/dev/console.php
+++ main-app/directivo/dev/console.php
@@ -1,7 +1,20 @@
 <?php
-$fp = fopen('../error_log', "r");
-while (!feof($fp)){
-    $linea = fgets($fp);
-    echo "<p>".$linea."</p>";
+$file = fopen($archivo, "r");
+fseek($file, 0, SEEK_END);
+$size = 4096; // Tamaño del bloque de lectura
+$buffer = '';
+$lines = [];
+
+while (count($lines) <= 20) {
+    $pos = ftell($file);
+    if ($pos <= 0) {
+        break;
+    }
+    fseek($file, max($pos - $size, 0), SEEK_SET);
+    $buffer = fread($file, $size) . $buffer;
+    fseek($file, max($pos - $size * 2, 0), SEEK_SET);
+
+    $lines = explode("\n", $buffer);
+    $lines = array_filter($lines);
+    $lines = array_slice($lines, -20);
 }
-fclose($fp);
