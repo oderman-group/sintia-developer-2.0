0774033048f08aa4df180523e294dd010785760a
Enuar Lara <enuar2110@gmail.com>
2023-05-16 12:28:31 -0500

    PES2023-197 - Mejora para error por cantidad de registros

--- main-app/directivo/includes/consulta-paginacion-dev-historial-acciones.php
+++ main-app/directivo/includes/consulta-paginacion-dev-historial-acciones.php
@@ -4,8 +4,7 @@
     $consulta=mysqli_query($conexion,"SELECT * FROM ".$baseDatosServicios.".seguridad_historial_acciones
     LEFT JOIN ".$baseDatosServicios.".instituciones ON ins_id=hil_institucion
     LEFT JOIN ".$baseDatosServicios.".paginas_publicidad ON pagp_id=hil_titulo
-    LEFT JOIN usuarios ON uss_id=hil_usuario
-    WHERE hil_id=hil_id $filtro
+    WHERE  hil_institucion='".$instID."' AND YEAR(hil_fecha) =".$year." ".$filtro."
     ORDER BY hil_id DESC;");
     $numRegistros=mysqli_num_rows($consulta);
     $registros= $config['conf_num_registros'];
