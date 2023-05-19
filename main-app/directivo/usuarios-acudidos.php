fa8d089dc584ca06cba644dfa7c377991356aaff
luigiecalo02 <114239695+luigiecalo02@users.noreply.github.com>
2023-05-19 14:42:33 -0500

    Update usuarios-acudidos.php

--- main-app/directivo/usuarios-acudidos.php
+++ main-app/directivo/usuarios-acudidos.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 include("session.php");
 $idPaginaInterna = 'DT0137';
 include("../compartido/historial-acciones-guardar.php");
@@ -8,43 +8,43 @@ require_once("../class/servicios/UsuarioServicios.php");
 require_once("../class/servicios/MatriculaServicios.php");
 ?>
 
-<!--bootstrap -->
-<link href="../../config-general/assets/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
-<link href="../../config-general/assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.css" rel="stylesheet" media="screen">
-<!-- Theme Styles -->
-<link href="../../config-general/assets/css/pages/formlayout.css" rel="stylesheet" type="text/css" />
-<!-- dropzone -->
-<link href="../../config-general/assets/plugins/dropzone/dropzone.css" rel="stylesheet" media="screen">
-<!--tagsinput-->
-<link href="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.css" rel="stylesheet">
-<!--select2-->
-<link href="../../config-general/assets/plugins/select2/css/select2.css" rel="stylesheet" type="text/css" />
-<link href="../../config-general/assets/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+	<!--bootstrap -->
+    <link href="../../config-general/assets/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+    <link href="../../config-general/assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.css" rel="stylesheet" media="screen">
+	<!-- Theme Styles -->
+    <link href="../../config-general/assets/css/pages/formlayout.css" rel="stylesheet" type="text/css" />
+	<!-- dropzone -->
+    <link href="../../config-general/assets/plugins/dropzone/dropzone.css" rel="stylesheet" media="screen">
+    <!--tagsinput-->
+    <link href="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.css" rel="stylesheet">
+    <!--select2-->
+    <link href="../../config-general/assets/plugins/select2/css/select2.css" rel="stylesheet" type="text/css" />
+    <link href="../../config-general/assets/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
 </head>
 <!-- END HEAD -->
 <?php include("../compartido/body.php");?>
-<div class="page-wrapper">
+    <div class="page-wrapper">
         <?php include("../compartido/encabezado.php");?>
-
+		
         <?php include("../compartido/panel-color.php");?>
-    <!-- start page container -->
-    <div class="page-container">
+        <!-- start page container -->
+        <div class="page-container">
  			<?php include("../compartido/menu.php");?>
-        <!-- start page content -->
-        <div class="page-content-wrapper">
-            <div class="page-content">
-                <div class="page-bar">
-                    <div class="page-title-breadcrumb">
-                        <div class=" pull-left">
-                            <div class="page-title">Acudidos</div>
+			<!-- start page content -->
+            <div class="page-content-wrapper">
+                <div class="page-content">
+                    <div class="page-bar">
+                        <div class="page-title-breadcrumb">
+                            <div class=" pull-left">
+                                <div class="page-title">Acudidos</div>
 								<?php include("../compartido/texto-manual-ayuda.php");?>
+                            </div>
+							<ol class="breadcrumb page-breadcrumb pull-right">
+                                <li><a class="parent-item" href="#" name="usuarios.php?cantidad=10&tipo=3" onClick="deseaRegresar(this)">Usuarios</a>&nbsp;<i class="fa fa-angle-right"></i></li>
+                                <li class="active">Acudidos</li>
+                            </ol>
                         </div>
-                        <ol class="breadcrumb page-breadcrumb pull-right">
-                            <li><a class="parent-item" href="#" name="usuarios.php?cantidad=10&tipo=3" onClick="deseaRegresar(this)">Usuarios</a>&nbsp;<i class="fa fa-angle-right"></i></li>
-                            <li class="active">Acudidos</li>
-                        </ol>
                     </div>
-                </div>
                 <div class="row">
                     <div class="col-12">
                         <?php include("../../config-general/mensajes-informativos.php"); ?>
@@ -73,7 +73,7 @@ require_once("../class/servicios/MatriculaServicios.php");
                                 <form name="formularioGuardar" action="usuarios-acudidos-actualizar.php" method="post">
                                     <input type="hidden" value="<?=$_GET['id'];?>" name="id">
                                     <div class="form-group row">
-                                        <label class="col-sm-2 control-label"><b>Estudiantes:</b></label>
+                                        <label class="col-sm-2 control-label"><b>Estudiantes: </b></label>
                                         <div class="col-sm-10">
                                             <?php
                                             $parametros = array("upe_id_usuario"=>$acudienteActural["uss_id"]);
@@ -101,44 +101,43 @@ require_once("../class/servicios/MatriculaServicios.php");
                         </div>
                     </div>
                 </div>
-
-            </div>
+                <script>          
+                    $(document).ready(function() {
+          		        $('#select_estudiante').select2({
+					    placeholder: 'Seleccione el estudiante...',
+					    theme: "bootstrap",
+                        multiple: true,
+                            ajax: {
+                                type: 'GET',
+                                url: 'ajax-listar-estudiantes.php',
+                                processResults: function(data) {
+                                    data = JSON.parse(data);
+                                    return {
+                                        results: $.map(data, function(item) {                                  
+                                            return {
+                                                id: item.value,
+                                                text: item.label
+                                            }
+                                        })
+                                    };
+                                }
+                            }
+				        });
+			        });
+              </script>
+                </div>
                 <!-- end page content -->
              <?php // include("../compartido/panel-configuracion.php");?>
-            </div>
-            <!-- end page container -->
-        <?php include("../compartido/footer.php");?>
-        <script>          
-                $(document).ready(function() {
-          		$('#select_estudiante').select2({
-					placeholder: 'Seleccione el estudiante...',
-					theme: "bootstrap",
-                    multiple: true,
-					ajax: {
-						type: 'GET',
-						url: 'ajax-listar-estudiantes.php',
-						processResults: function(data) {
-                            data = JSON.parse(data);
-							return {
-								results: $.map(data, function(item) {                                  
-									return {
-										id: item.value,
-										text: item.label
-									}
-								})
-							};
-						}
-					}
-				});
-			});
-        </script>
         </div>
-        <!-- start js include path -->
+        <!-- end page container -->
+        <?php include("../compartido/footer.php");?>
+    </div>
+    <!-- start js include path -->
     <script src="../../config-general/assets/plugins/jquery/jquery.min.js" ></script>
     <script src="../../config-general/assets/plugins/popper/popper.js" ></script>
     <script src="../../config-general/assets/plugins/jquery-blockui/jquery.blockui.min.js" ></script>
-        <script src="../../config-general/assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
-        <!-- bootstrap -->
+	<script src="../../config-general/assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <!-- bootstrap -->
     <script src="../../config-general/assets/plugins/bootstrap/js/bootstrap.min.js" ></script>
     <script src="../../config-general/assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js" ></script>
     <script src="../../config-general/assets/plugins/bootstrap-inputmask/bootstrap-inputmask.min.js" ></script>
@@ -146,25 +145,25 @@ require_once("../class/servicios/MatriculaServicios.php");
     <script src="../../config-general/assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker-init.js"  charset="UTF-8"></script>
     <script src="../../config-general/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js"  charset="UTF-8"></script>
     <script src="../../config-general/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker-init.js"  charset="UTF-8"></script>
-        <!-- Common js-->
+    <!-- Common js-->
 	<script src="../../config-general/assets/js/app.js" ></script>
     <script src="../../config-general/assets/js/layout.js" ></script>
 	<script src="../../config-general/assets/js/theme-color.js" ></script>
-        <!-- notifications -->
+	<!-- notifications -->
 	<script src="../../config-general/assets/plugins/jquery-toast/dist/jquery.toast.min.js" ></script>
 	<script src="../../config-general/assets/plugins/jquery-toast/dist/toast.js" ></script>	
-        <!-- Material -->
-        <script src="../../config-general/assets/plugins/material/material.min.js"></script>
-        <!-- dropzone -->
+	<!-- Material -->
+	<script src="../../config-general/assets/plugins/material/material.min.js"></script>
+	<!-- dropzone -->
     <script src="../../config-general/assets/plugins/dropzone/dropzone.js" ></script>
-        <!--tags input-->
+    <!--tags input-->
     <script src="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.js" ></script>
     <script src="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input-init.js" ></script>
-        <!--select2-->
+    <!--select2-->
     <script src="../../config-general/assets/plugins/select2/js/select2.js" ></script>
     <script src="../../config-general/assets/js/pages/select2/select2-init.js" ></script>
-        <!-- end js include path -->
-        </body>
+    <!-- end js include path -->
+</body>
 
-        <!-- Mirrored from radixtouch.in/templates/admin/smart/source/light/advance_form.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 18 May 2018 17:32:54 GMT -->
-        </html>
+<!-- Mirrored from radixtouch.in/templates/admin/smart/source/light/advance_form.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 18 May 2018 17:32:54 GMT -->
+</html>
