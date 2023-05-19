bfb458d02f298a9671b5e0801888926df622feef
Admin Softcomputo <softcomputo77@gmail.com>
2023-05-18 19:10:23 -0500

    pes2023-81 autocompletar con ajax

--- main-app/directivo/ajax-listar-estudiantes.php
+++ main-app/directivo/ajax-listar-estudiantes.php
@@ -1,16 +1,15 @@
 <?php
 include('session.php');
 require_once("../class/servicios/MatriculaServicios.php");
-if(isset($_GET['nombre']))
+if(isset($_GET['term']))
 { 
 	try {
-		$lista=MatriculaServicios::listarEstudianteNombre($_GET['nombre']);
+		$lista=MatriculaServicios::listarEstudianteNombre($_GET['term']);
 		$valores = json_encode($lista);
 		foreach($lista as $clave=> $dato){
 			$response[] = ["value"=>$dato["mat_id"],"label"=>MatriculaServicios::nombreCompleto($dato)];
 		}
-		 echo json_encode($response);
-		
+		 echo json_encode($response);		
 	} catch (Exception $e) {
 		echo "Excepción catpurada: " . $e->getMessage();
 		exit();
