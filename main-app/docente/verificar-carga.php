70925b4e2c531a7d51a580ad22047758ebebd076
Jhon Oderman <jhooderman@gmail.com>
2023-05-24 12:06:49 -0500

    PES2023-249 - Remover comentario innecesario

--- main-app/docente/verificar-carga.php
+++ main-app/docente/verificar-carga.php
@@ -36,14 +36,6 @@ if(!isset($_GET["carga"]) or !isset($_GET["periodo"]) or !is_numeric($_GET["carg
 	$periodoConsultaActual = $_GET["periodo"];
 }
 
-
-/*$consultaCargaActual = mysqli_query($conexion, "SELECT * FROM academico_cargas 
-INNER JOIN academico_materias ON mat_id=car_materia
-INNER JOIN academico_grados ON gra_id=car_curso
-INNER JOIN academico_grupos ON gru_id=car_grupo
-WHERE car_id='".$cargaConsultaActual."' AND car_docente='".$_SESSION["id"]."' AND car_activa=1");
-
-$numCargaActual = mysqli_num_rows($consultaCargaActual);*/
 $datosCargaActual = $_SESSION["infoCargaActual"]['datosCargaActual'];
 
 if($datosCargaActual['car_primer_acceso_docente']==""){
