3ee7585ece875e1de1017c41b28596934d30ee22
Jhon Oderman <jhooderman@gmail.com>
2023-05-24 12:30:00 -0500

    PES2023-249 - Crear funciones para contar estudiantes en cursos

--- main-app/class/Estudiantes.php
+++ main-app/class/Estudiantes.php
@@ -356,4 +356,52 @@ class Estudiantes {
         return $resultado;
     }
 
+    public static function contarEstudiantesParaDocentes(string $filtroDocentes = '')
+    {
+        global $conexion;
+        $cantidad = 0;
+
+        try {
+            $consulta = mysqli_query($conexion, "SELECT * FROM academico_matriculas
+            LEFT JOIN usuarios ON uss_id=mat_id_usuario
+            LEFT JOIN academico_grados ON gra_id=mat_grado
+            LEFT JOIN academico_grupos ON gru_id=mat_grupo
+            WHERE mat_eliminado=0 
+            AND (mat_estado_matricula=1 OR mat_estado_matricula=2)
+            ".$filtroDocentes."
+            ORDER BY mat_primer_apellido, mat_segundo_apellido, mat_nombres
+            ");
+            $cantidad = mysqli_num_rows($consulta);
+        } catch (Exception $e) {
+            echo "Excepción catpurada: ".$e->getMessage();
+            exit();
+        }
+
+        return $cantidad;
+    }
+
+    public static function contarEstudiantesParaDocentesMT(array $datosCargaActual = [])
+    {
+        global $conexion, $baseDatosServicios, $config;
+        $cantidad = 0;
+
+        try {
+            $consulta = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".mediatecnica_matriculas_cursos
+            LEFT JOIN academico_matriculas ON mat_eliminado=0 AND (mat_estado_matricula=1 OR mat_estado_matricula=2) AND mat_grupo='".$datosCargaActual['car_grupo']."' AND mat_id=matcur_id_matricula
+            LEFT JOIN academico_grados ON gra_id=matcur_id_curso
+            LEFT JOIN academico_grupos ON gru_id=mat_grupo
+            LEFT JOIN usuarios ON uss_id=mat_id_usuario
+            LEFT JOIN ".$baseDatosServicios.".opciones_generales ON ogen_id=mat_genero
+            WHERE matcur_id_curso='".$datosCargaActual['car_curso']."' AND matcur_id_institucion='".$config['conf_id_institucion']."'
+            ORDER BY mat_primer_apellido, mat_segundo_apellido, mat_nombres;
+            ");
+            $cantidad = mysqli_num_rows($consulta);
+        } catch (Exception $e) {
+            echo "Excepción catpurada: ".$e->getMessage();
+            exit();
+        }
+
+        return $cantidad;
+    }
+
 }
