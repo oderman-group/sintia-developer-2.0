a8cb0a4b8dcf6730f087a8cdd50b02299035bcea
Jhon Oderman <jhooderman@gmail.com>
2023-05-25 07:33:24 -0500

    Resolver conflictos - Cambiar mensaje informativo

--- config-general/mensajes-informativos.php
+++ config-general/mensajes-informativos.php
@@ -97,21 +97,21 @@ if(isset($_GET['error']) || isset($_GET['success'])){
 
             case 'ER_DT_12':
                 $tipo = 'danger';
-                $mensaje = 'Estos datos ya se encuentran registrados y asociados a la pagina <b>' . $_GET["nombrePagina"] . '</b>.<br>
-                ¿Desea mostrar toda la información de la pagina?<br>
-                <a href="dev-paginas-editar.php?idP=' . $_GET["id"] . '" id="addRow" class="btn deepPink-bgcolor">
-                    Sí, deseo mostrar la información
-                </a>';
-            break;
-
-            case 'ER_DT_12':
-                $tipo = 'danger';
                 $mensaje = 'La clave actual no es correcta. Por favor verifique.';
             break;
 
             case 'ER_DT_13':
                 $tipo = 'danger';
                 $mensaje = 'La clave nueva no coincide. Por favor verifique.';
+            break;
+
+            case 'ER_DT_14':
+                $tipo = 'danger';
+                $mensaje = 'Estos datos ya se encuentran registrados y asociados a la pagina <b>' . $_GET["nombrePagina"] . '</b>.<br>
+                ¿Desea mostrar toda la información de la pagina?<br>
+                <a href="dev-paginas-editar.php?idP=' . $_GET["id"] . '" id="addRow" class="btn deepPink-bgcolor">
+                    Sí, deseo mostrar la información
+                </a>';
             break;
 
 
