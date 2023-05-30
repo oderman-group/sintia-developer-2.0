7cb566abed3814ba1c531c3b336ee0edbe87b551
Enuar Lara <enuar2110@gmail.com>
2023-05-30 16:04:59 -0500

    PES2023-236 - Se cambia ID de la pagina

--- main-app/directivo/planilla-docentes-filtros.php
+++ main-app/directivo/planilla-docentes-filtros.php
@@ -1,5 +1,5 @@
 <?php include("session.php");?>
-<?php $idPaginaInterna = 'DT0141';?>
+<?php $idPaginaInterna = 'DT0194';?>
 <?php include("../compartido/historial-acciones-guardar.php");?>
 <?php include("../compartido/head.php");?>
 
