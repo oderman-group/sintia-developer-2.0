a8b6481583bc2c1e2d7c05f51f08c512080f8023
Admin Softcomputo <softcomputo77@gmail.com>
2023-06-01 07:18:17 -0500

    pes2023-245 crear listas para las materias de tipo individual

--- main-app/estudiante/filtro-cargas.php
+++ main-app/estudiante/filtro-cargas.php
@@ -1,3 +1,6 @@
+<?php require_once("../class/servicios/CargaServicios.php"); ?>
+<?php require_once("../class/servicios/MediaTecnicaServicios.php"); ?>
+<?php require_once("../class/servicios/GradoServicios.php"); ?>
 <div class="panel">
 										<header class="panel-heading panel-heading-purple"><?=$frases[73][$datosUsuarioActual['uss_idioma']];?> </header>
 										<div class="panel-body">
@@ -19,3 +22,33 @@
 											<?php }?>
 										</div>
                                     </div>
+									<?php if (array_key_exists(10, $arregloModulos)) { 
+										$parametros = ['matcur_id_matricula' => $datosEstudianteActual["mat_id"]];
+										$listaCursosMediaTecnica = MediaTecnicaServicios::listar($parametros);
+										foreach ($listaCursosMediaTecnica as $dato) {
+											$cursoMediaTecnica = GradoServicios::consultarCurso($dato["matcur_id_curso"]);
+
+										?>
+										
+										<div class="panel">
+										<header class="panel-heading panel-heading-purple"><?=$cursoMediaTecnica['gra_nombre'];?> </header>
+											<div class="panel-body">
+												<?php $parametros = [
+													'matcur_id_matricula' => $datosEstudianteActual["mat_id"],
+													'matcur_id_curso'     => $dato["matcur_id_curso"]
+													];
+												$listacargaMediaTecnica = MediaTecnicaServicios::listarMaterias($parametros);
+												if ($listacargaMediaTecnica != null) { 
+													foreach ($listacargaMediaTecnica as $cargaMediaTecnica) {
+													 if($cargaMediaTecnica['car_id']==$cargaConsultaActual) $estiloResaltado = 'style="color: orange;"'; else $estiloResaltado = '';?>
+													<p><a href="<?=$_SERVER['PHP_SELF'];?>?carga=<?=$cargaMediaTecnica['car_id'];?>&periodo=<?=$periodoConsultaActual;?>" <?=$estiloResaltado;?>><?=strtoupper($cargaMediaTecnica['mat_nombre']);?></a></p>
+													<?php }?>													
+												<?php } else {?>
+													<p> El curso <?=$cursoMediaTecnica["gra_nombre"]?>  no tiene carga academica.</p>
+												<?php }?>
+											
+											</div>
+										</div>
+										<?php }?>
+                                    
+									<?php } ?>
