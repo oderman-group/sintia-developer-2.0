09e2a98c208b850d6b4f52204df6dd4f2ac10b79
Enuar Lara <enuar2110@gmail.com>
2023-06-06 09:46:22 -0500

    PES2023-262 - Mejora al guardar notas masivas de MT

--- main-app/docente/ajax-calificaciones-registrar.php
+++ main-app/docente/ajax-calificaciones-registrar.php
@@ -1,10 +1,9 @@
 <?php 
 include("session.php");
 require_once("../class/Estudiantes.php");
-?>
 
-<?php include("verificar-carga.php");?>
-<?php
+include("verificar-carga.php");
+
 $consultaNum = mysqli_query($conexion, "SELECT academico_calificaciones.cal_id_actividad, academico_calificaciones.cal_id_estudiante FROM academico_calificaciones 
 WHERE academico_calificaciones.cal_id_actividad='".$_POST["codNota"]."' AND academico_calificaciones.cal_id_estudiante='".$_POST["codEst"]."'");
 $num = mysqli_num_rows($consultaNum);
@@ -59,8 +58,11 @@ if($_POST["operacion"]==2){
 
 //Para la misma nota para todos los estudiantes
 if($_POST["operacion"]==3){
-	$filtroAdicional= "AND mat_grado='".$datosCargaActual['car_curso']."' AND mat_grupo='".$datosCargaActual['car_grupo']."' AND (mat_estado_matricula=1 OR mat_estado_matricula=2)";
-	$consultaE =Estudiantes::listarEstudiantesEnGrados($filtroAdicional,"");
+	if($datosCargaActual['gra_tipo'] == GRADO_INDIVIDUAL) {
+		$consultaE = Estudiantes::listarEstudiantesParaDocentesMT($datosCargaActual);
+	} else {
+		$consultaE = Estudiantes::listarEstudiantesParaDocentes($filtroDocentesParaListarEstudiantes);
+	}
 	
 	
 	$accionBD = 0;
