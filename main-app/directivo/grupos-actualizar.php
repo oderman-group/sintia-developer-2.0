6bc2532ac3fa83698d20f00ed7bf6dc02aad59a0
Admin Softcomputo <softcomputo77@gmail.com>
2023-06-08 12:04:36 -0500

    pes2023-239 se crea el actualizar

---
+++ main-app/directivo/grupos-actualizar.php
@@ -0,0 +1,38 @@
+<?php
+include("session.php");
+$idPaginaInterna = 'DT0198';
+$accion = $_POST["accion"];
+
+include("../compartido/historial-acciones-guardar.php");
+
+//COMPROBAMOS QUE TODOS LOS CAMPOS NECESARIOS ESTEN LLENOS
+if (trim($_POST["nombreG"]) == "") {
+	include("../compartido/guardar-historial-acciones.php");
+	echo '<script type="text/javascript">window.location.href="grupos-agregar.php?error=ER_DT_4";</script>';
+	exit();
+}
+if (trim($_POST["codigoG"]) == "") {
+	include("../compartido/guardar-historial-acciones.php");
+	echo '<script type="text/javascript">window.location.href="grupos-agregar.php?error=ER_DT_4";</script>';
+	exit();
+}
+
+try {
+	if (!is_null($_POST["id"] )) {		
+		mysqli_query(
+			$conexion,
+			"UPDATE academico_grupos SET
+				gru_codigo =".$_POST['codigoG'].", 
+				gru_nombre  ='".$_POST['nombreG']."'
+				WHERE gru_id=".$_POST["id"].""
+		);
+		$idRegistro =$_POST["id"];
+	}
+} catch (Exception $e) {
+	include("../compartido/error-catch-to-report.php");
+}
+
+
+include("../compartido/guardar-historial-acciones.php");
+echo '<script type="text/javascript">window.location.href="grupos.php?success=SC_DT_2&id=' . $idRegistro . '";</script>';
+exit();
