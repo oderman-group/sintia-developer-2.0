16de445f7d36342241246889b6fdb3e9d932a7d4
Enuar Lara <enuar2110@gmail.com>
2023-06-08 07:46:33 -0500

    Mejora para inscripciones

--- main-app/admisiones/bd-conexion.php
+++ main-app/admisiones/bd-conexion.php
@@ -1,13 +1,47 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT']."/app-sintia/config-general/constantes.php");
-require_once(ROOT_PATH."/conexion-datos.php");
 require_once($_SERVER['DOCUMENT_ROOT']."/app-sintia/config-general/config-admisiones.php");
 
 $server 		   = $servidorConexion;
 $user   		   = $usuarioConexion;
 $pass   		   = $claveConexion;
 $dbName 		   = $baseDatosAdmisiones;
-$dbNameInstitucion = $BD_ADMISIONES_MOCK;
+
+if(!empty($_REQUEST['idInst']) || !empty($_REQUEST['institucion'])){
+	try{
+		$pdoAdmin = new PDO('mysql:host='.$server.';dbname='.$baseDatosServicios, $user, $pass);
+		//$pdoI->exec("SET CHARACTER SET utf-8");
+	}catch (PDOException $e) {
+		echo "Error!: " . $e->getMessage() . "<br/>";
+		die();
+	}
+	
+	if(!empty($_REQUEST['institucion'])){
+		$idInsti=$_REQUEST['institucion'];
+	}else{
+		$idInsti=$_REQUEST['idInst'];
+	}
+
+	//configuración
+	$configConsulta = "SELECT * FROM configuracion
+	WHERE conf_id_institucion = ".$idInsti." AND conf_agno = ".date("Y");
+	$config = $pdoAdmin->prepare($configConsulta);
+	$config->execute();
+	$datosConfig = $config->fetch();
+
+	//información
+	$infogConsulta = "SELECT * FROM general_informacion
+	WHERE info_institucion = ".$idInsti." AND info_year = ".date("Y");
+	$info = $pdoAdmin->prepare($infogConsulta);
+	$info->execute();
+	$datosInfo = $info->fetch();
+
+	$BD_ADMISIONES_MOCK = $datosConfig['conf_base_datos'].'_'.$datosConfig['conf_agno'];
+}
+
+if(!empty($_REQUEST['inst'])){
+	$BD_ADMISIONES_MOCK = base64_decode($_REQUEST['inst']);
+}
 
 try{
 	$pdo = new PDO('mysql:host='.$server.';dbname='.$dbName, $user, $pass);
@@ -16,6 +50,8 @@ try{
 	echo "Error!: " . $e->getMessage() . "<br/>";
 	die();
 }
+
+$dbNameInstitucion = $BD_ADMISIONES_MOCK;
 
 try{
 	$pdoI = new PDO('mysql:host='.$server.';dbname='.$dbNameInstitucion, $user, $pass);
