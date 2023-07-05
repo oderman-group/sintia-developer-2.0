393d3a9296ba6ac7d6e22913a24069274443edf4
Enuar Lara <enuar2110@gmail.com>
2023-07-05 09:22:26 -0500

    PES2023-282 - Se añade mejora para poder consultar por años

--- main-app/class/servicios/MediaTecnicaServicios.php
+++ main-app/class/servicios/MediaTecnicaServicios.php
@@ -34,14 +34,14 @@ class MediaTecnicaServicios extends Servicios
       return Servicios::SelectSql($sql);         
     }
     
-    public static function listarEstudiantes($parametrosArray=null)
+    public static function listarEstudiantes($parametrosArray=null,string $BD    = '')
     {
       global $baseDatosServicios;
       
       $sqlInicial="SELECT * FROM ".$baseDatosServicios.".mediatecnica_matriculas_cursos 
-      LEFT JOIN academico_matriculas ON matcur_id_matricula=mat_id
-			LEFT JOIN academico_grados ON gra_id=matcur_id_curso
-			LEFT JOIN usuarios ON uss_id=mat_id_usuario
+      LEFT JOIN ".$BD."academico_matriculas ON matcur_id_matricula=mat_id
+			LEFT JOIN ".$BD."academico_grados ON gra_id=matcur_id_curso
+			LEFT JOIN ".$BD."usuarios ON uss_id=mat_id_usuario
       LEFT JOIN ".$baseDatosServicios.".opciones_generales ON ogen_id=mat_genero			
       ";
       if($parametrosArray && count($parametrosArray)>0){
