aad37cbf7fafa7498cf073703441b10a629d9695
Enuar Lara <enuar2110@gmail.com>
2023-07-05 11:17:34 -0500

    PES2023-186 - Se ocultan mensajes de envio de correo

--- main-app/admisiones/index-guardar.php
+++ main-app/admisiones/index-guardar.php
@@ -134,7 +134,7 @@ if ($newId > 0) {
         $body = ob_get_clean();
 
         //Server settings
-        $mail->SMTPDebug = 2;                                     // Enable verbose debug output
+        $mail->SMTPDebug = 0;                                     // Enable verbose debug output
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host       = EMAIL_SERVER;  	                        // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
