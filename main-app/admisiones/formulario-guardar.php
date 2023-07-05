a6a49d5d19b73a69749d125551f2d0e497969794
Enuar Lara <enuar2110@gmail.com>
2023-07-05 11:18:11 -0500

    PES2023-186 - Mejora para guardar nombre completo del aspirante

--- main-app/admisiones/formulario-guardar.php
+++ main-app/admisiones/formulario-guardar.php
@@ -60,9 +60,12 @@ $filasAfectadas = $stmt->rowCount();
 
 
 //Actualiza estado en aspirantes
-$aspQuery = 'UPDATE aspirantes SET asp_estado_solicitud = 4 WHERE asp_id = :id';
+
+$nombreCompleto=$_POST['nombre'].' '.$_POST['primerApellidos'].' '.$_POST['segundoApellidos'];
+$aspQuery = 'UPDATE aspirantes SET asp_estado_solicitud = 4, asp_nombre = :nombre WHERE asp_id = :id';
 $asp = $pdo->prepare($aspQuery);
 $asp->bindParam(':id', $_POST['solicitud'], PDO::PARAM_INT);
+$asp->bindParam(':nombre', $nombreCompleto, PDO::PARAM_STR);
 $asp->execute();
 
 
