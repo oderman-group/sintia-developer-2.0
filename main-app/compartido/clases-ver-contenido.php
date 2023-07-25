ae25ee18a9f6d16bb1def7bdd1f7def0efb6e03b
Admin Softcomputo <softcomputo77@gmail.com>
2023-07-25 01:31:50 -0500

    pes2023-298 Script para listar comentarios y preguntas

--- main-app/compartido/clases-ver-contenido.php
+++ main-app/compartido/clases-ver-contenido.php
@@ -175,7 +175,7 @@ $datosConsultaBD = mysqli_fetch_array($consultaDatosBD, MYSQLI_BOTH);
 										</div>
 										
 										<div class="card-body">
-										<form id="comentario" name="comentario" class="form-horizontal" action="../compartido/guardar.php" method="post">
+										<form class="form-horizontal" action="../compartido/guardar.php" method="post">
 											<input type="hidden" name="id" value="14">
 											<input type="hidden" name="idClase" value="<?=$_GET["idR"];?>">
 											<input type="hidden" name="sesionUsuario" value="<?=$_SESSION["id"];?>">
@@ -204,133 +204,38 @@ $datosConsultaBD = mysqli_fetch_array($consultaDatosBD, MYSQLI_BOTH);
 											<p style="color: tomato;">Los comentarios y/o preguntas más recientes aparecen automáticamente en la parte de abajo.</p>	
 
 											<div id="preguntas"></div>
-									
-											<script>
-											var comentario = document.getElementById("comentario");
-											var preguntas = document.getElementById("preguntas");
-												
-											var contenido = document.getElementById("contenido");
-												
-											document.addEventListener("DOMContentLoaded", listarPreguntas, false);
-												
+											
+															<script>
+																setInterval('consultarPreguntas()',10000);
 
-											comentario.addEventListener("submit", listarPreguntas);
-											var cont = 0;
-											var numItems = 0;	
-												
-											function listarPreguntas(e)
-											{
-												
-												
-												document.getElementById("envia").value="NO";
-												
-												if(e && e.type == 'submit'){
-												  	e.preventDefault();
-													document.getElementById("envia").value="SI";
-												 }
-												
-												var datos = new FormData(comentario);
-												//console.log(datos.get('envia'));
-												
-												
-												fetch('https://plataformasintia.com/main-app/v2.0/compartido/clases-ver-contenido-api.php', {
-													method: 'POST',
-													body: datos
-												})
-												.then(resp => resp.json())
-												.then(data => {
-													//console.log(data);
-													preguntas.innerHTML = '';
-													for(valores of data.info){
-														preguntas.innerHTML += `
-															<div id="${valores.cpp_id}" class="row">
-																<div class="col-sm-12">
-																	<div class="panel">
-																		<div class="card-head">
+																window.onload = consultarPreguntas();	
+																
 
-																				
-																				${ valores.cpp_usuario == data.adicional.usuario ? `
-																				<div class = "pull-right">
-																					<a href="../compartido/guardar.php?get=24&idCom=${valores.cpp_id}" onClick="if(!confirm('Deseas eliminar este mensaje?')){return false;}">
-																						<i class="fa fa-trash"></i>
-																					</a>
-																				</div>
-																				` : ``}
-																		</div>
+																function consultarPreguntas(){
 
-																		<div class="user-panel">
-																				<div class="pull-left image">
-																					<img src="../files/fotos/${valores.uss_foto}" class="img-circle user-img-circle" alt="User Image" height="50" width="50" />
-																				</div>
+																	var claseId = <?= $_GET["idR"]; ?>;
+																	var usuarioActual = <?= $datosUsuarioActual['uss_id']; ?>;
+																	
 
-																				<div class="pull-left info">
-																					<p><a href="clases-ver.php?idR=${valores.cpp_id_clase}&usuario=${valores.uss_id}">${valores.uss_nombre}</a><br><span style="font-size: 11px; color: #000;">${valores.cpp_fecha}</span></p>
-																				</div>
-																		</div>
-																		
+																	datos = "claseId="+claseId+"&usuarioActual="+usuarioActual;
+																		$.ajax({
+																		type: "POST",
+																		url: "../compartido/ajax-comentarios-preguntas.php",
+																		data: datos,
+																		success: function(data){
+																			$('#preguntas').empty().hide().html(data).show(1);
 
-																		<div class="panel-body">
-																			<p>${valores.cpp_contenido}</p>	
-																		</div>
-
-																		<div class="card-body">
-
-																		</div>
-
-																	</div>
+																		}
+																		});
 
 
 
-																</div>
-															</div>
-														`;
+																}	
+
+																
+																															
+															</script>
 														
-													}
-													
-													//console.log(data.adicional);
-													
-													if(e && e.type == 'submit'){
-														contenido.value="";
-													 }
-													
-													
-													
-													if(data.adicional.numItems > numItems && cont > 0){
-														var numItActual = parseInt(data.adicional.numItems) - parseInt(numItems);
-														notificare(numItActual);
-													 }
-													
-													//console.log(numItems);
-													
-													numItems = data.adicional.numItems;
-													cont ++;
-
-												});
-												
-												
-											};
-												
-												
-											setInterval(listarPreguntas, 3000);
-												
-												function notificare(numI){
-															$.toast({
-																heading: 'Información',  
-																text: 'Hay '+ numI +' comentarios nuevos.',
-																position: 'botom-left',
-																loaderBg:'#ff6849',
-																icon: 'info',
-																hideAfter: 5000, 
-																stack: 6,
-																allowToastClose : false,
-																stack : 5
-															});
-														}
-														
-
-
-										</script>
-									
 									
 											
                                 </div>
