326997c1861a974c56b06d8e8e30a54e01531088
Admin Softcomputo <softcomputo77@gmail.com>
2023-08-08 09:26:42 -0500

    pes2023-95 se crean constantes para los estados de la solicitud

--- config-general/constantes.php
+++ config-general/constantes.php
@@ -13,6 +13,11 @@ define('SUB_MENU', 'sub-menu');
 
 define('ESTADO_EMAIL_ENVIADO', 'enviado');
 define('ESTADO_EMAIL_ERROR', 'error');
+
+
+define('SOLICITUD_CANCELACION_PENDIENTE', 'pendiente');
+define('SOLICITUD_CANCELACION_APROBADO', 'aprobado');
+define('SOLICITUD_CANCELACION_CANCELADO', 'cancelado');
 switch (ENVIROMENT) {
         case 'LOCAL':
 	include(ROOT_PATH."/conexion-datos.php");
@@ -38,7 +43,7 @@ switch (ENVIROMENT) {
 switch($_SERVER['HTTP_HOST']){
 	case 'localhost':
         define('REDIRECT_ROUTE', 'http://localhost/app-sintia/main-app');
-        // error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
+         error_reporting (E_ALL ^ E_NOTICE ^ E_WARNING);
         break;
 
 	case 'developer.plataformasintia.com':
