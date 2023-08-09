8fa090390ca65d97bc599ecc355b1fe80c3827e2
Admin Softcomputo <softcomputo77@gmail.com>
2023-08-08 23:52:11 -0500

    pes2023-307 Listar Solicitudes

---
+++ main-app/directivo/dev-solicitudes-cancelacion-detalle.php
@@ -0,0 +1,232 @@
+<?php
+include("session.php");
+
+$idPaginaInterna = 'DV0039';
+
+include("../compartido/historial-acciones-guardar.php");
+
+include("../compartido/head.php");
+
+require_once("../class/Solicitudes.php");
+$solicitudActual = Solicitudes::consultar($_GET["id"]);
+
+?>
+
+<!--bootstrap -->
+<link href="../../config-general/assets/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+<link href="../../config-general/assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.css" rel="stylesheet" media="screen">
+<!-- Theme Styles -->
+<link href="../../config-general/assets/css/pages/formlayout.css" rel="stylesheet" type="text/css" />
+<!-- dropzone -->
+<link href="../../config-general/assets/plugins/dropzone/dropzone.css" rel="stylesheet" media="screen">
+<!--tagsinput-->
+<link href="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.css" rel="stylesheet">
+<!--select2-->
+<link href="../../config-general/assets/plugins/select2/css/select2.css" rel="stylesheet" type="text/css" />
+<link href="../../config-general/assets/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<!-- END HEAD -->
+<?php include("../compartido/body.php"); ?>
+<div class="page-wrapper">
+    <?php include("../compartido/encabezado.php"); ?>
+
+    <?php include("../compartido/panel-color.php"); ?>
+    <!-- start page container -->
+    <div class="page-container">
+        <?php include("../compartido/menu.php"); ?>
+        <!-- start page content -->
+        <div class="page-content-wrapper">
+            <div class="page-content">
+                <div class="page-bar">
+                    <div class="page-title-breadcrumb">
+                        <div class=" pull-left">
+                            Detalle Solicitud
+                        </div>
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12">
+                        <?php include("../../config-general/mensajes-informativos.php"); ?>
+                        <div class="panel">
+                            <header class="panel-heading panel-heading-purple">Detalle</header>
+                            <div class="panel-body">
+                                <form action="solicitud-cancelacion-actualizar.php" method="post" enctype="multipart/form-data">
+                                    <i class="bi bi-eye-slash"></i>
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 ">ID Reporte:</label>
+                                        <div class="col-sm-1">
+                                            <input type="text" name="id" class="form-control" value="<?= $solicitudActual['solcan_id']; ?>" readonly>
+                                        </div>
+                                        <div class="col-sm-5">
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="material-icons">date_range</i></span>
+                                                </div>
+                                                <input type="datetime" class="form-control" value="<?= $solicitudActual['solcan_fecha_creacion']; ?>" readonly>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 ">Institución:</label>
+                                        <div class="col-sm-1">
+                                            <input type="text" class="form-control" value="<?= $solicitudActual['ins_siglas']; ?>" readonly>
+                                            <input type="text" name="ins_id" class="form-control" value="<?= $solicitudActual['ins_id']; ?>" readonly hidden>
+                                        </div>
+                                        <div class="col-sm-3">
+                                            <input type="text" name="ins_contacto" class="form-control" value="<?= $solicitudActual['ins_contacto_principal']; ?>" readonly>
+                                        </div>
+                                        <div class="col-sm-2">
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="fa fa-mobile-phone"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control" value="<?= $solicitudActual['ins_celular_contacto']; ?>" readonly>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-sm-2">
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="material-icons">email</i></span>
+                                                </div>
+                                                <input type="text" name="ins_email_contacto" class="form-control" value="<?= $solicitudActual['ins_email_contacto']; ?>" readonly>
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 ">Usuario que solicita: </label>
+                                        <div class="col-sm-4">
+                                            <input type="text" name="uss_id" class="form-control" value="<?= $solicitudActual['uss_id']; ?>" readonly hidden>
+                                            <input type="text" class="form-control" value="<?= $solicitudActual['uss_nombre']; ?> <?= $solicitudActual['uss_apellido']; ?>" readonly>
+                                        </div>
+                                        <div class="col-sm-2">
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="fa fa-mobile-phone"></i></span>
+                                                </div>
+                                                <input type="text" class="form-control" value="<?= $solicitudActual['uss_celular']; ?>" readonly>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-2">
+                                            <div class="input-group">
+                                                <div class="input-group-prepend">
+                                                    <span class="input-group-text"><i class="material-icons">email</i></span>
+                                                </div>
+                                                <input type="text" class="form-control" value="<?= $solicitudActual['uss_email']; ?>" readonly>
+                                            </div>
+
+                                        </div>
+                                    </div>
+
+
+
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 ">Motivo de cancelacion:</label>
+                                        <div class="col-sm-8">
+                                            <textarea class="form-control" readonly rows="3">
+                                              <?= $solicitudActual['solcan_motivo']; ?>
+                                             </textarea>
+                                        </div>
+                                    </div>
+
+
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 ">Respuesta:</label>
+                                        <div class="col-sm-8">
+                                            <textarea cols="80" id="editor1" name="respuesta" rows="10"><?= $solicitudActual['solcan_respuesta']; ?></textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group row">
+                                        <label class="col-sm-2 ">Estado: </label>
+                                        <div class="col-sm-4">
+                                            <select class="form-control" name="estado">
+
+                                                <option value="<?= SOLICITUD_CANCELACION_PENDIENTE ?>" <?php if (
+                                                                                                            $solicitudActual['solcan_estado'] != SOLICITUD_CANCELACION_APROBADO
+                                                                                                            && $solicitudActual['solcan_estado'] != SOLICITUD_CANCELACION_CANCELADO
+                                                                                                        ) {
+                                                                                                            echo "selected";
+                                                                                                        } ?>>Pendiente</option>
+                                                <option value="<?= SOLICITUD_CANCELACION_APROBADO ?>" <?php if ($solicitudActual['solcan_estado'] == SOLICITUD_CANCELACION_APROBADO) {
+                                                                                                            echo "selected";
+                                                                                                        } ?>>Apropado</option>
+                                                <option value="<?= SOLICITUD_CANCELACION_CANCELADO ?>" <?php if ($solicitudActual['solcan_estado'] == SOLICITUD_CANCELACION_CANCELADO) {
+                                                                                                            echo "selected";
+                                                                                                        } ?>>Cancelado</option>
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="form-group">
+                                        <div class="offset-md-3 col-md-9">
+                                            <a href="#" name="dev-solicitudes-cancelacion.php" onClick="deseaRegresar(this)" class="btn btn-round btn-primary">Regresar</a>
+                                            <button type="submit" class="btn btn-warning">Actualizar solicitud</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <!-- end page container -->
+        <?php include("../compartido/footer.php"); ?>
+    </div>
+    <!-- start js include path -->
+    <script src="../../config-general/assets/plugins/jquery/jquery.min.js"></script>
+    <script src="../../config-general/assets/plugins/popper/popper.js"></script>
+    <script src="../../config-general/assets/plugins/jquery-blockui/jquery.blockui.min.js"></script>
+    <script src="../../config-general/assets/plugins/jquery-slimscroll/jquery.slimscroll.js"></script>
+    <!-- bootstrap -->
+    <script src="../../config-general/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-inputmask/bootstrap-inputmask.min.js"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js" charset="UTF-8"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker-init.js" charset="UTF-8"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+    <script src="../../config-general/assets/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker-init.js" charset="UTF-8"></script>
+    <!-- Common js-->
+    <script src="../../config-general/assets/js/app.js"></script>
+    <script src="../../config-general/assets/js/layout.js"></script>
+    <script src="../../config-general/assets/js/theme-color.js"></script>
+    <!-- notifications -->
+    <script src="../../config-general/assets/plugins/jquery-toast/dist/jquery.toast.min.js"></script>
+    <script src="../../config-general/assets/plugins/jquery-toast/dist/toast.js"></script>
+    <!-- Material -->
+    <script src="../../config-general/assets/plugins/material/material.min.js"></script>
+    <!-- dropzone -->
+    <script src="../../config-general/assets/plugins/dropzone/dropzone.js"></script>
+    <!--tags input-->
+    <script src="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.js"></script>
+    <script src="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input-init.js"></script>
+    <!--select2-->
+    <script src="../../config-general/assets/plugins/select2/js/select2.js"></script>
+    <script src="../../config-general/assets/js/pages/select2/select2-init.js"></script>
+    <!-- end js include path -->
+    <script src="../ckeditor/ckeditor.js"></script>
+
+    <script>
+        // Replace the <textarea id="editor1"> with a CKEditor 4
+        // instance, using default configuration.
+        CKEDITOR.replace('editor1');
+    </script>
+    </body>
+
+    <!-- Mirrored from radixtouch.in/templates/admin/smart/source/light/advance_form.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 18 May 2018 17:32:54 GMT -->
+
+    </html>
---
+++ main-app/directivo/solicitud-cancelacion-actualizar.php
@@ -0,0 +1,55 @@
+<?php
+include("session.php");
+$idPaginaInterna = 'DV0040';
+require_once(ROOT_PATH."/main-app/class/EnviarEmail.php");
+
+include("../compartido/historial-acciones-guardar.php");
+
+//COMPROBAMOS QUE TODOS LOS CAMPOS NECESARIOS ESTEN LLENOS
+if (empty($_POST["respuesta"])) {
+	include("../compartido/guardar-historial-acciones.php");
+	$msj = ' Motivo de cancelacion no valido, verificar que el campo este lleno';
+	$url = $_SERVER["HTTP_REFERER"] . '?error=ER_DT_15&msj=' . $msj;;
+	echo '<script type="text/javascript">window.location.href="' . $url . '";</script>';
+	exit();
+}
+
+
+try {	
+			mysqli_query(
+			$conexion,
+			"UPDATE  " . $baseDatosServicios . ".solicitud_cancelacion 
+			SET solcan_respuesta ='".$_POST["respuesta"]."',
+			solcan_responsable ='".$datosUsuarioActual["uss_id"]."',
+			solcan_ultima_actualizacion = NOW(),
+			solcan_estado ='".$_POST["estado"]."'
+			WHERE 
+			solcan_id  ='".$_POST["id"]."'"
+			);
+			$idRegistro = $_POST["id"];
+} catch (Exception $e) {
+	include("../compartido/error-catch-to-report.php");
+}
+
+if($_POST["estado"]!=SOLICITUD_CANCELACION_PENDIENTE){
+$data = [
+	'usuario_email'    => $_POST['ins_email_contacto'],
+	'usuario_nombre'    => $_POST['ins_contacto'],
+	'solicitud_usuario'    => $_POST['ins_id'],
+	'solicitud_id'    => $idRegistro,
+	'solicitud_estado'    => $_POST["estado"],
+	'solicitud_respuesta'    => $_POST["respuesta"],
+	'solicitud_responsable'    => $datosUsuarioActual["uss_nombre"],
+	'usuario_email2'    => $datosUsuarioActual['uss_email']
+];
+
+
+$asunto = 'Respuesta solicitud de cancelacion';
+$bodyTemplateRoute = ROOT_PATH.'/config-general/template-email-respuesta-solicitud-cancelacion.php';
+
+EnviarEmail::enviar($data, $asunto, $bodyTemplateRoute,null,null);
+}
+
+include("../compartido/guardar-historial-acciones.php");
+echo '<script type="text/javascript">window.location.href="dev-solicitudes-cancelacion-detalle.php?success=SC_DT_2&id=' . $idRegistro . '";</script>';
+exit();
