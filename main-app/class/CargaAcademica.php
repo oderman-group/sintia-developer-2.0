5f8c68581dc6b9d642fb0f99fb0d7f69fdea5385
Jhon Oderman <jhooderman@gmail.com>
2023-09-08 07:49:40 -0400

    HF - Create new method for load data in a sesion

--- main-app/class/CargaAcademica.php
+++ main-app/class/CargaAcademica.php
@@ -24,4 +24,35 @@ class CargaAcademica {
 
     }
 
+    /**
+     * Este función consulta los datos de la carga académica actual y
+     * los almacena en sesion
+     * 
+     * @param int $carga
+     * @param int $sesion
+     * 
+     * @return array
+     */
+    public static function cargasDatosEnSesion(Int $carga, Int $sesion): array 
+    {
+        global $conexion;
+
+        $infoCargaActual = [];
+		try{
+			$consultaCargaActual = mysqli_query($conexion, "SELECT * FROM academico_cargas 
+			INNER JOIN academico_materias ON mat_id=car_materia
+			INNER JOIN academico_grados ON gra_id=car_curso
+			INNER JOIN academico_grupos ON gru_id=car_grupo
+			WHERE car_id='".$carga."' AND car_docente='".$sesion."' AND car_activa=1");
+		} catch (Exception $e) {
+			include("../compartido/error-catch-to-report.php");
+		}
+		$datosCargaActual = mysqli_fetch_array($consultaCargaActual, MYSQLI_BOTH);
+		$infoCargaActual = [
+			'datosCargaActual'  => $datosCargaActual
+		];
+
+        return $infoCargaActual;
+    }
+
 }
