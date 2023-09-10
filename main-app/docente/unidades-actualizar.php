586dc29c930fd6a47d72a75c324e57d5afddafc2
Enuar Lara <enuar2110@gmail.com>
2023-09-10 18:40:49 -0500

    PES2023-352 - Se crea sxript para actualizar unidades en BD

---
+++ main-app/docente/unidades-actualizar.php
@@ -0,0 +1,26 @@
+<?php
+	include("session.php");
+
+	Modulos::validarAccesoDirectoPaginas();
+	$idPaginaInterna = 'DC0097';
+
+	include("verificar-carga.php");
+	include("verificar-periodos-diferentes.php");
+
+	include("../compartido/historial-acciones-guardar.php");
+
+	//COMPROBAMOS QUE TODOS LOS CAMPOS NECESARIOS ESTEN LLENOS
+	if(empty($_POST["nombre"])){
+		include("../compartido/guardar-historial-acciones.php");
+		echo '<script type="text/javascript">window.location.href="unidades-editar.php?error=ER_DT_4";</script>';
+		exit();
+	}
+	try{
+		mysqli_query($conexion, "UPDATE academico_unidades SET uni_nombre='".$_POST["nombre"]."', uni_id_carga='".$cargaConsultaActual."', uni_periodo='".$periodoConsultaActual."', uni_descripcion='".$_POST["contenido"]."' WHERE uni_id='".$_POST["idR"]."'");
+	} catch (Exception $e) {
+		include("../compartido/error-catch-to-report.php");
+	}
+	
+	include("../compartido/guardar-historial-acciones.php");
+	echo '<script type="text/javascript">window.location.href="unidades.php?carga='.base64_encode($cargaConsultaActual).'&periodo='.base64_encode($periodoConsultaActual).'";</script>';
+	exit();
