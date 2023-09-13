f202b084d6f7b897d8583e3cf7f2a7b600fd5284
Enuar Lara <enuar2110@gmail.com>
2023-09-13 13:51:49 -0500

    PES2023-MPS - Mejora en modal de detalles de productos

--- main-app/compartido/modal-marketplace-detalles.php
+++ main-app/compartido/modal-marketplace-detalles.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="modalMarketplaceDetalles<?= $datosConsulta['prod_id']; ?>" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true" style="z-index:9999;">
 	<div class="modal-dialog" style="max-width: 1350px!important;">
-		<div class="modal-content">
+		<div class="modal-content" style="max-width: 1350px!important;">
 			<div class="modal-body">
 				<div class="row">
 					<div class="col-sm-12">
@@ -9,13 +9,13 @@
 								<header><?= strtoupper($datosConsulta['prod_nombre']); ?></header>
 							</div>
 							<div class="card-body" id="bar-parent6">
-								<div class="blogThumb" style="height: 100%;">
-									<div class="thumb-center" style="height: 55%;">
-										<img class="img-responsive" style="height: 100%;" src="<?= $foto; ?>"></a>
+								<div style="display: grid; grid-template-columns: 3fr 4fr; gap: 10px;">
+									<div >
+										<img class="img-responsive" src="<?= $foto; ?>"></a>
 									</div>
-									<div class="course-box" style="height: 45%;  display: flex; flex-direction: column; justify-content: flex-end;">
+									<div>
 										<div class="text-muted">
-											<span class="m-r-10" style="font-size: 10px;"> <?= $datosConsulta['catp_nombre']; ?></span>
+											<span class="m-r-10" style="font-size: 20px;">Cat: <?= $datosConsulta['catp_nombre']; ?></span>
 										</div>
 										<p><span style="font-weight: bold;"> $<?= number_format($datosConsulta['prod_precio'], 0, ",", "."); ?></span></p>
 										<p><?= $datosConsulta['prod_descripcion']; ?></p>
