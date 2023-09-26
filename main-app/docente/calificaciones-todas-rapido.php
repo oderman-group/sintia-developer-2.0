429ecca54321ca34de8a56fc1b7810e191b73cbc
Enuar Lara <enuar2110@gmail.com>
2023-09-26 17:01:11 -0500

    PES2023-402 - Se añaden archivos necesarios para alertas

--- main-app/docente/calificaciones-todas-rapido.php
+++ main-app/docente/calificaciones-todas-rapido.php
@@ -91,6 +91,12 @@ th {
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<script src="../../config-general/assets/plugins/sweetalert/sweetalert2.all.min.js"></script>
+    <link rel="stylesheet" href="./../config-general/assets/plugins/sweetalert/sweetalert2.all.min.css">
+	<!-- Axios -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.js"></script>
+	<!-- Mis funciones JS -->
+	<script src="../compartido/funciones.js" ></script>
 	
 </head>
 <!-- END HEAD -->
