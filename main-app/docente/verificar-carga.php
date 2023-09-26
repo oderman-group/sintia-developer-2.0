109f732fed422a68ce138af310bc19dd43478042
Enuar Lara <enuar2110@gmail.com>
2023-09-26 09:46:20 -0500

    PES2023-410 - Se quita codigo comentado

--- main-app/docente/verificar-carga.php
+++ main-app/docente/verificar-carga.php
@@ -29,14 +29,6 @@ if(!isset($_GET["carga"]) or !isset($_GET["periodo"]) or !is_numeric($carga) or 
 	$periodoConsultaActual = $periodo;
 }
 
-
-/*$consultaCargaActual = mysqli_query($conexion, "SELECT * FROM academico_cargas 
-INNER JOIN academico_materias ON mat_id=car_materia
-INNER JOIN academico_grados ON gra_id=car_curso
-INNER JOIN academico_grupos ON gru_id=car_grupo
-WHERE car_id='".$cargaConsultaActual."' AND car_docente='".$_SESSION["id"]."' AND car_activa=1");
-
-$numCargaActual = mysqli_num_rows($consultaCargaActual);*/
 $datosCargaActual = $_SESSION["infoCargaActual"]['datosCargaActual'];
 
 if($datosCargaActual['car_primer_acceso_docente']==""){
