0bf0409f12fc40f02c170b6ee2bc47511b588352
Admin Softcomputo <softcomputo77@gmail.com>
2023-10-01 15:56:46 -0500

    pes2023-399 crear nueva publicacion

--- main-app/compartido/noticias-agregar-modal.php
+++ main-app/compartido/noticias-agregar-modal.php
@@ -1,6 +1,19 @@
 <!--select2-->
 <link href="../../config-general/assets/plugins/select2/css/select2.css" rel="stylesheet" type="text/css" />
 <link href="../../config-general/assets/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+<!-- END HEAD -->
+<?php include("../compartido/body.php"); ?>
+<?php include("../compartido/noticias-agregar-contenido.php"); ?>
+
+<!--select2-->
+<script src="../../config-general/assets/plugins/select2/js/select2.js"></script>
+<script src="../../config-general/assets/js/pages/select2/select2-init.js"></script>
+<!-- end js include path -->
+</body>
+<!--select2-->
+<link href="../../config-general/assets/plugins/select2/css/select2.css" rel="stylesheet" type="text/css" />
+<link href="../../config-general/assets/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
 <!--tagsinput-->
 <link href="../../config-general/assets/plugins/jquery-tags-input/jquery-tags-input.css" rel="stylesheet">
 
