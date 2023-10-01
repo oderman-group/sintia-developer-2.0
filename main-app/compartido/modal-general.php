bbc53c4e8e8aaa4a795c9ca5b38568e754d9c212
Jhon Oderman <jhooderman@gmail.com>
2023-10-01 16:55:00 -0400

    IM - Crear modal general

---
+++ main-app/compartido/modal-general.php
@@ -0,0 +1,43 @@
+<div class="modal fade" id="modalGeneral" tabindex="-1" role="dialog" aria-labelledby="basicModal" aria-hidden="true">
+	<div class="modal-dialog" style="max-width: 1350px!important;">
+		<div class="modal-content">
+			<div class="modal-body">
+				<h4 id="respuestaTituloGeneral"></h4>
+				<p id="respuestaGeneral"></p>
+			</div>
+			<div class="modal-footer">
+			<a href="#" data-dismiss="modal" class="btn btn-danger">Cerrar</a>
+		</div>
+		</div>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
