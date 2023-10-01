e76b8c929aec04b0389763bc7fafe8c8b6075089
Admin Softcomputo <softcomputo77@gmail.com>
2023-10-01 16:20:40 -0500

    pes2023-399 Modal transferir Cargar

---
+++ main-app/directivo/cargas-transferir-modal.php
@@ -0,0 +1,71 @@
+<?php
+if (!Modulos::validarSubRol([$idPaginaInterna])) {
+    echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
+    exit();
+}
+
+$disabledPermiso = "";
+if (!Modulos::validarPermisoEdicion()) {
+    $disabledPermiso = "disabled";
+} ?>
+<!--select2-->
+<link href="../../config-general/assets/plugins/select2/css/select2.css" rel="stylesheet" type="text/css" />
+<link href="../../config-general/assets/plugins/select2/css/select2-bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+
+<div class="panel">
+    <header class="panel-heading panel-heading-purple">Transferir Cargas </header>
+    <div class="panel-body">
+
+
+        <form name="formularioGuardar" action="cargas-transferir-actualizar.php" method="post">
+
+            <div class="form-group row">
+                <label class="col-sm-2 control-label">Del Docente</label>
+                <div class="col-sm-4">
+                    <select style="width: 100%" class="form-control  select2" name="de" required <?= $disabledPermiso; ?>>
+                        <option value="">Seleccione una opci�n</option>
+                        <?php
+                        try {
+                            $docen = mysqli_query($conexion, "SELECT * FROM usuarios WHERE uss_tipo=2");
+                        } catch (Exception $e) {
+                            include("../compartido/error-catch-to-report.php");
+                        }
+                        while ($nDocen = mysqli_fetch_array($docen, MYSQLI_BOTH)) {
+                            echo "<option value='" . $nDocen["uss_id"] . "'>" . UsuariosPadre::nombreCompletoDelUsuario($nDocen) . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group row">
+                <label class="col-sm-2 control-label">Al Docente</label>
+                <div class="col-sm-4">
+                    <select  style="width: 100%" class="form-control  select2" name="para" required <?= $disabledPermiso; ?>>
+                        <option value="">Seleccione una opci�n</option>
+                        <?php
+                        try {
+                            $docen = mysqli_query($conexion, "SELECT * FROM usuarios WHERE uss_tipo=2");
+                        } catch (Exception $e) {
+                            include("../compartido/error-catch-to-report.php");
+                        }
+                        while ($nDocen = mysqli_fetch_array($docen, MYSQLI_BOTH)) {
+                            echo "<option value='" . $nDocen["uss_id"] . "'>" . UsuariosPadre::nombreCompletoDelUsuario($nDocen) . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+
+            <?php if (Modulos::validarPermisoEdicion()) { ?>
+                <input type="submit" class="btn btn-primary" value="Guardar cambios">&nbsp;
+            <?php } ?>
+        </form>
+    </div>
+</div>
+
+
+<!--select2-->
+<script src="../../config-general/assets/plugins/select2/js/select2.js"></script>
+<script src="../../config-general/assets/js/pages/select2/select2-init.js"></script>
