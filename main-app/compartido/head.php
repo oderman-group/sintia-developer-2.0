a9f375695a9e1b0514a111867c76317c1e4a9f9d
Jhon Oderman <jhooderman@gmail.com>
2023-10-01 16:54:33 -0400

    IM - Incluir dos funciones nuevas

--- main-app/compartido/head.php
+++ main-app/compartido/head.php
@@ -127,6 +127,8 @@ require_once("../class/Grupos.php");
 	<!-- Mis funciones JS -->
 	<script src="../compartido/funciones.js" ></script>
 	<script src="../js/Utilidades.js" ></script>
+	<script src="../js/Estudiantes.js" ></script>
+	<script src="../js/Docentes.js" ></script>
 	
 	<?php 
 	include("sintia-funciones-js.php");
@@ -169,7 +171,41 @@ require_once("../class/Grupos.php");
 	  width: 100%;
 	  height: 100%;
 	}
-	</style>
+
+	/* Para bloquear la pagina mientras carga un modal*/
+#overlay {
+	display: none;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background-color: rgba(0, 0, 0, 0.5); /* Fondo semitransparente */
+	z-index: 9999;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+}
+
+#loader {
+	border: 6px solid #f3f3f3; /* Light gray */
+	border-top: 6px solid #3498db; /* Blue */
+	border-radius: 50%;
+	width: 50px;
+	height: 50px;
+	animation: spin 2s linear infinite;
+}
+
+#loading-text {
+	margin-top: 10px;
+	color: white;
+}
+
+@keyframes spin {
+	0% { transform: rotate(0deg); }
+	100% { transform: rotate(360deg); }
+}
+</style>
 
 	<script type="text/javascript">
 	$(window).load(function() {
