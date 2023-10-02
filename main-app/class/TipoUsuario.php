afba02dede83fd892d331349736d7975cd983c7a
Jhon Oderman <jhooderman@gmail.com>
2023-10-02 17:54:11 -0400

    IM - Establecer conexión con PDO y utilizar su instancia para consultas preparadas

--- main-app/class/TipoUsuario.php
+++ main-app/class/TipoUsuario.php
@@ -4,24 +4,22 @@ require_once 'Tables/BDT_general_perfiles.php';
 
 class TipoUsuario {
 
-    public static function listarTiposUsuarios($baseDatosServicios)
+    public static function listarTiposUsuarios($baseDatosServicios, $conexionPDO)
     {
-        $conexion = Plataforma::getConexion();
         $tableName = BDT_GeneralPerfiles::getTableName();
 
         try {
             $consulta = "SELECT * FROM {$baseDatosServicios}.{$tableName}";
-            $stmt = mysqli_prepare($conexion, $consulta);
+            $stmt = $conexionPDO->prepare($consulta);
 
             if ($stmt) {
-                mysqli_stmt_execute($stmt);
-                $resultado = mysqli_stmt_get_result($stmt);
+                $stmt->execute();
 
-                return $resultado;
+                return $stmt;
             } else {
                 throw new Exception("Error al preparar la consulta.");
             }
-        } catch (Exception $e) {
+        } catch (PDOException  $e) {
             echo "Excepción capturada: " . $e->getMessage();
             return null;
         }
