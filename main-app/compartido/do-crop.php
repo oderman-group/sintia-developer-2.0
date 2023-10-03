6f4df43503f5ff64cb75cb3170d42d67e0c6c26e
Enuar Lara <enuar2110@gmail.com>
2023-10-03 07:30:57 -0500

    PES2023-478 - Se modifica la forma de guardar la foto segun nueva libreria

--- main-app/compartido/do-crop.php
+++ main-app/compartido/do-crop.php
@@ -1,38 +1,16 @@
 <?php
-session_start();
-include("../../config-general/config.php");
-require_once("../class/UsuariosPadre.php");
-include("../compartido/sintia-funciones.php");
-$datosUsuarioActual =UsuariosPadre::sesionUsuario($_SESSION['id']);
+extract($_POST);
+$dir = "../files/fotos/";
+if(!is_dir($dir))
+mkdir($dir);
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$img = base64_decode($img);
+$save = file_put_contents($dir.$fname, $img);
+if($save){
+    $resp['status'] = 'success';
+}else{
+    $resp['status'] = 'failed';
 
-if(isset($_POST['crop_image']))
-{
-  $y1=$_POST['top'];
-  $x1=$_POST['left'];
-  $w=$_POST['right'];
-  $h=$_POST['bottom'];
-  $image="../files/fotos/".$datosUsuarioActual['uss_foto'];
-
-  list( $width,$height ) = getimagesize( $image );
-  $newwidth = 800;
-  $newheight = 600;
-
-  $thumb = imagecreatetruecolor( $newwidth, $newheight );
-  $source = imagecreatefromjpeg($image);
-
-  imagecopyresized($thumb, $source, 0, 0, 0, 0, $newwidth, $newheight, $width, $height);
-  imagejpeg($thumb,$image,100); 
-
-
-  $im = imagecreatefromjpeg($image);
-  $dest = imagecreatetruecolor($w,$h);
-	
-  imagecopyresampled($dest,$im,0,0,$x1,$y1,$w,$h,$w,$h);
-  imagejpeg($dest,"../files/fotos/".$datosUsuarioActual['uss_foto'], 100);
-
-  $destinos = validarUsuarioActual($datosUsuarioActual);
-	
-  echo '<script type="text/javascript">window.location.href="' .$destinos. 'perfil.php"</script>';
-  exit();
 }
-?>
+echo json_encode($resp);
