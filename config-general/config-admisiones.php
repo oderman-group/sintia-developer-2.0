beba0bf288c2a48c7aa0e574cd17126c4beea507
Jhon Oderman <jhooderman@gmail.com>
2023-10-08 19:08:45 -0400

    HF - Reorganizar los estados

--- config-general/config-admisiones.php
+++ config-general/config-admisiones.php
@@ -1,24 +1,39 @@
 <?php
-#VARIABLES PARA SUBMODULO DE ADMISIONES
-$estadosSolicitud = array(
+#VARIABLES PARA MODULO DE ADMISIONES
+$estadosSolicitud = [
+	8 => 'VERIFICACIÓN DE CUPO DISPONIBLE',
+	4 => 'EN PROCESO',
+	3 => 'PENDIENTE POR DILIGENCIAR EL FORMULARIO',
+	5 => 'EXAMEN Y ENTREVISTA', 
 	1 => 'VERIFICACIÓN DE PAGO', 
 	2 => 'PAGO RECHAZADO', 
-	3 => 'PENDIENTE POR DILIGENCIAR EL FORMULARIO',
-	4 => 'EN PROCESO',
-	5 => 'EXAMEN Y ENTREVISTA', 
 	6 => 'APROBADO', 
 	7 => 'NO APROBADO',
-	8 => 'VERIFICACIÓN DE CUPO DISPONIBLE',
 	9 => 'MOVIDO AL AÑO SIGUIENTE'
-);
-$progresoSolicitud = array(
-	1 => '15%', 
-	2 => '15%', 
-	3 => '30%', 
-	4 => '60%',
-	5 => '75%', 
+];
+
+$ordenReal = [8, 4, 3, 5, 1, 2, 6, 7, 9];
+
+$progresoSolicitud = [
+	1 => '75%', 
+	2 => '75%', 
+	3 => '45%', 
+	4 => '30%',
+	5 => '60%', 
 	6 => '90%',
 	7 => '100%',
 	8 => '15%',
 	9 => '100%',
-);
+];
+
+$fondoSolicitud = [
+	1 => 'cadetblue',
+	2 => '#DB6503',
+	3 => 'cadetblue',
+	4 => '#AFB372',
+	5 => '#AFB372',
+	6 => 'green',
+	7 => 'red',
+	8 => '#DA9E00',
+	9 => '#009B7A'
+];
