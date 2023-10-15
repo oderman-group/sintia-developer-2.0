ef92d2e9b0acc6a1afda02ece09cad7b97f4504d
Jhon Oderman <jhooderman@gmail.com>
2023-10-14 20:26:33 -0400

    HF - Modificar función ya que otra tiene la misma validación

--- main-app/class/Modulos.php
+++ main-app/class/Modulos.php
@@ -4,18 +4,13 @@ class Modulos {
 
     public static function verificarPermisosPaginas($idPaginaInterna): bool
     {
-        global $conexion, $baseDatosServicios, $config;
 
-        $consultaPaginaActualUsuarios = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".paginas_publicidad
-        INNER JOIN ".$baseDatosServicios.".instituciones_modulos 
-        ON ipmod_modulo=pagp_modulo 
-        AND ipmod_institucion='".$config['conf_id_institucion']."'
-        WHERE pagp_id='".$idPaginaInterna."'");
-        $paginaActualUsuario = mysqli_fetch_array($consultaPaginaActualUsuarios, MYSQLI_BOTH);
-        if ($paginaActualUsuario[0]=="") { 
+        $datosPaginaActual = self::datosPaginaActual($idPaginaInterna);
+        if( empty($datosPaginaActual) ) {
             return false;
         }
         return true;
+
     }
 
     public static function verificarPermisoDev(){
