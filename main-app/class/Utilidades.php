1e859b905397a35795213ae09311433d0025264a
Jhon Oderman <jhooderman@gmail.com>
2023-10-15 17:36:46 -0400

    HF - Crear función para validar si un archivo existe

--- main-app/class/Utilidades.php
+++ main-app/class/Utilidades.php
@@ -2,12 +2,31 @@
 class Utilidades {
     public static  function getToString($valor)
     {   
-    // validammos que las variables no sean null 
-    if (isset($valor)) {
-        return  $valor;
-    }else{
-        return "";
-    }
+        // validammos que las variables no sean null 
+        if (isset($valor)) {
+            return  $valor;
+        }else{
+            return "";
+        }
     
-}
+    }
+
+    /**
+     * Comprueba si un archivo existe en la ruta especificada.
+     *
+     * Esta función verifica si el archivo especificado por la ruta existe en el sistema de archivos.
+     *
+     * @param string $ruta La ruta completa al archivo que se va a comprobar.
+     *
+     * @return bool Devuelve true si el archivo existe, o false en caso contrario.
+     */
+    public static  function ArchivoExiste($ruta)
+    {   
+        if ( file_exists($ruta) ) {
+            return  true;
+        }
+
+        return false;
+    
+    }
 }
