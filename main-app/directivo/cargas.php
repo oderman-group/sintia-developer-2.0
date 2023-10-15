53d4b3087d8950db33f8b2b135ec5ec3d63bc80f
Jhon Oderman <jhooderman@gmail.com>
2023-10-15 17:42:40 -0400

    HF - Quitar titulo que no corresponde

--- main-app/directivo/cargas.php
+++ main-app/directivo/cargas.php
@@ -152,7 +152,7 @@ if(!Modulos::validarSubRol([$idPaginaInterna])){
 																				</li>
 																			<?php }?>
 																	  		<li>
-																			    <a href="javascript:void(0);" title="Eliminar" onClick="sweetConfirmacion('Alerta!','Esta acción te permitirá entrar como docente y ver todos los detalles de esta carga. Deseas continuar?','question','auto-login.php?user=<?=base64_encode($resultado['car_docente']);?>&tipe=<?=base64_encode(2)?>&carga=<?=base64_encode($resultado['car_id']);?>&periodo=<?=base64_encode($resultado['car_periodo']);?>')">Ver como docente</a>
+																			    <a href="javascript:void(0);" onClick="sweetConfirmacion('Alerta!','Esta acción te permitirá entrar como docente y ver todos los detalles de esta carga. Deseas continuar?','question','auto-login.php?user=<?=base64_encode($resultado['car_docente']);?>&tipe=<?=base64_encode(2)?>&carga=<?=base64_encode($resultado['car_id']);?>&periodo=<?=base64_encode($resultado['car_periodo']);?>')">Ver como docente</a>
 																			<?php }?>
 																	  <li><a href="cargas-horarios.php?id=<?=base64_encode($resultado[0]);?>" title="Ingresar horarios">Ingresar Horarios</a></li>
 																	  <li><a href="periodos-resumen.php?carga=<?=base64_encode($resultado[0]);?>" title="Resumen Periodos"><?=$frases[84][$datosUsuarioActual[8]];?></a></li>
