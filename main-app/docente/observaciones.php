e48fc6571694e7e17f09a649cacd84fbce0fc612
Jhon Oderman <jhooderman@gmail.com>
2023-10-17 13:15:03 -0400

    HF - Remover parte que se pasó a sección independiente (Estas paginas pueden eliminarse después)

--- main-app/docente/observaciones.php
+++ main-app/docente/observaciones.php
@@ -30,30 +30,30 @@ if( !CargaAcademica::validarPermisoPeriodosDiferentes($datosCargaActual, $period
 
 <script type="application/javascript">
 //CALIFICACIONES	
-function notas(enviada){
-  const idSplit = enviada.id.split('-');
-  var carga = <?=$cargaConsultaActual;?>;
-  var periodo = <?=$periodoConsultaActual;?>; 
-  var nota = enviada.value;
-  var codEst = idSplit[0];
-  var nombreEst = idSplit[1];
-  var operacion = enviada.title;
-$('#respRC').empty().hide().html("Guardando información, espere por favor...").show(1);
-	datos = "nota="+(nota)+
-			"&operacion="+(operacion)+
-			"&nombreEst="+(nombreEst)+
-			"&carga="+(carga)+
-			"&periodo="+(periodo)+
-			"&codEst="+(codEst);
-		   $.ajax({
-			   type: "POST",
-			   url: "ajax-calificaciones-registrar.php",
-			   data: datos,
-			   success: function(data){
-			   	$('#respRC').empty().hide().html(data).show(1);
-		   	   }
-		  });
-}
+// function notas(enviada){
+//   const idSplit = enviada.id.split('-');
+//   var carga = <?=$cargaConsultaActual;?>;
+//   var periodo = <?=$periodoConsultaActual;?>; 
+//   var nota = enviada.value;
+//   var codEst = idSplit[0];
+//   var nombreEst = idSplit[1];
+//   var operacion = enviada.title;
+// $('#respRC').empty().hide().html("Guardando información, espere por favor...").show(1);
+// 	datos = "nota="+(nota)+
+// 			"&operacion="+(operacion)+
+// 			"&nombreEst="+(nombreEst)+
+// 			"&carga="+(carga)+
+// 			"&periodo="+(periodo)+
+// 			"&codEst="+(codEst);
+// 		   $.ajax({
+// 			   type: "POST",
+// 			   url: "ajax-calificaciones-registrar.php",
+// 			   data: datos,
+// 			   success: function(data){
+// 			   	$('#respRC').empty().hide().html(data).show(1);
+// 		   	   }
+// 		  });
+// }
 </script>
 </head>
 <!-- END HEAD -->
@@ -76,91 +76,13 @@ $('#respRC').empty().hide().html("Guardando información, espere por favor...").
                             </div>
                         </div>
                     </div>
-                    
+                    <?php include("includes/barra-superior-informacion-actual.php"); ?>
                     <div class="row">
                         <div class="col-md-12">
                             <div class="row">
-                                
-								<div class="col-md-4 col-lg-3">
 									
-									<?php include("info-carga-actual.php");?>
-									
-									<?php include("filtros-cargas.php");?>
-									
-									<?php include("../compartido/publicidad-lateral.php");?>
-									
-								</div>
-									
-								<div class="col-md-8 col-lg-9">
-                                    <div class="card card-topline-purple">
-                                        <div class="card-head">
-                                            <header>Observaciones</header>
-                                            <div class="tools">
-                                                <a class="fa fa-repeat btn-color box-refresh" href="javascript:;"></a>
-			                                    <a class="t-collapse btn-color fa fa-chevron-down" href="javascript:;"></a>
-			                                    <a class="t-close btn-color fa fa-times" href="javascript:;"></a>
-                                            </div>
-                                        </div>
-										
-										
-									
-										
-                                        <div class="card-body">
-											
-											
-											
-										<span style="color: blue; font-size: 15px;" id="respRC"></span>
-											
-											
-                                        <div class="table-responsive">
-                                            <table class="table table-striped custom-table table-hover">
-                                                <thead>
-                                                    <tr>
-                                                        <th>#</th>
-														<th>ID</th>
-														<th><?=$frases[61][$datosUsuarioActual[8]];?></th>
-														<th>DEF</th>
-														<th><?=$frases[109][$datosUsuarioActual[8]];?></th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-													<?php
-													 $consulta = Estudiantes::listarEstudiantesParaDocentes($filtroDocentesParaListarEstudiantes);
-													 $contReg = 1;
-													 $colorNota = "black";
-													 while($resultado = mysqli_fetch_array($consulta, MYSQLI_BOTH)){
-														 $consultaNotas=mysqli_query($conexion, "SELECT * FROM academico_boletin WHERE bol_estudiante=".$resultado[0]." AND bol_periodo='".$periodoConsultaActual."' AND bol_carga='".$cargaConsultaActual."'");
-														$notas = mysqli_fetch_array($consultaNotas, MYSQLI_BOTH);
-                                                        $definitiva = isset($notas['bol_nota']) ? $notas['bol_nota'] : null;
-														if($definitiva<$config[5] and $definitiva!="") $colorNota = $config[6]; elseif($definitiva>=$config[5]) $colorNota = $config[7]; else {$colorNota = 'black'; $definitiva='';} 
-														
-													 ?>
-                                                    
-													<tr>
-                                                        <td><?=$contReg;?></td>
-														<td><?=$resultado['mat_id'];?></td>
-														<td width="60%">
-															<img src="../files/fotos/<?=$resultado['uss_foto'];?>" width="50">
-															<?=Estudiantes::NombreCompletoDelEstudiante($resultado);?>
-														</td>
-														
-														<td><a href="calificaciones-estudiante.php?usrEstud=<?=base64_encode($resultado['mat_id_usuario']);?>&periodo=<?=base64_encode($periodoConsultaActual);?>&carga=<?=base64_encode($cargaConsultaActual);?>" style="text-decoration:underline; color:<?=$colorNota;?>;"><?=$definitiva;?></a><br><span style="font-size: 10px;"><?php if(isset($notas)) echo $notas['bol_observaciones'];?></span></td>
-														
-														<td width="25%">
-															
-															<textarea rows="7" cols="80" name="O<?=$contReg;?>" id="<?=$resultado['mat_id'].'-'.$resultado['mat_nombres'];?>" title="8" onChange="notas(this)" <?=$disabled;?>><?php if(isset($notas)) echo $notas['bol_observaciones_boletin'];?></textarea>
-														</td>
-                                                    </tr>
-													<?php 
-														 $contReg++;
-													  }
-
-													  ?>
-                                                </tbody>
-                                            </table>
-                                            </div>
-                                        </div>
-                                    </div>
+								<div class="col-md-12">
+                                    
                                 </div>
 								
 							
