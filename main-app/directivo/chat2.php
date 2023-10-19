988f5578a2942697c7714e18c438446da201e154
Admin Softcomputo <softcomputo77@gmail.com>
2023-10-18 22:59:46 -0500

    pes2023-390 actalizacion de botonoes

--- main-app/directivo/chat2.php
+++ main-app/directivo/chat2.php
@@ -1,5 +1,5 @@
 <?php include("session.php"); ?>
-<?php $idPaginaInterna = 'DT0004'; ?>
+<?php $idPaginaInterna = 'DT0209'; ?>
 <?php include("../compartido/historial-acciones-guardar.php"); ?>
 <?php include("../compartido/head.php"); ?>
 
@@ -77,20 +77,36 @@
                                                 ON(uss_id=chat_destino_usuario)
 
                                                 WHERE (chat_remite_usuario = '" . $_SESSION['id'] . "'  OR  chat_destino_usuario = '" . $_SESSION['id'] . "' ) 
-                                                AND uss_id!='" . $_SESSION['id'] . "'  
+                                               
                                                 GROUP BY chat_remite_usuario,chat_destino_usuario
                                                 ORDER BY fecha_ulitmo_msj DESC"
                                             );
                                             if (mysqli_num_rows($consultaUsuariosOffline) > 0) {
                                                 while ($datosUsuriosOffline = mysqli_fetch_array($consultaUsuariosOffline, MYSQLI_BOTH)) {
-                                                    $fotoPerfilUsrOffline = $usuariosClase->verificarFoto($datosUsuriosOffline['uss_foto']);
+                                                    $fotoPerfil = $usuariosClase->verificarFoto($datosUsuriosOffline['uss_foto']);
+                                                    $ussId = $datosUsuriosOffline['uss_id'];
+                                                    $nombre = $datosUsuriosOffline['uss_nombre'] . ' ' . $datosUsuriosOffline['uss_apellido1'];
+                                                    $cantidad = $datosUsuriosOffline['cantidad'];
+                                                    $estado = $datosUsuriosOffline['uss_estado'] == "1" ? "online" : "offline";
+                                                    if ($ussId == $_SESSION['id']) {
+                                                        $miId=$_SESSION['id'];
+                                                        $ussId = $datosUsuriosOffline['chat_remite_usuario'];
+                                                        $consultaUsuario = mysqli_query($conexion, "SELECT uss_id, uss_nombre, uss_apellido1, uss_foto, uss_estado,$baseDatosSocial.cantNoLeidos($miId,$ussId)as cantidad FROM usuarios WHERE uss_estado=1 AND uss_bloqueado=0 AND uss_id ='" . $ussId . "' ");
+                                                        while ($datosUsuarios = mysqli_fetch_array($consultaUsuario, MYSQLI_BOTH)) {
+                                                            $fotoPerfil = $usuariosClase->verificarFoto($datosUsuarios['uss_foto']);
+                                                            $nombre = $datosUsuarios['uss_nombre'];
+                                                            if ($datosUsuarios['uss_apellido1'] != "" || $datosUsuarios['uss_apellido1'] != NULL) {
+                                                                $nombre .= " " . $datosUsuarios['uss_apellido1'];
+                                                            }
+                                                            $cantidad=$datosUsuarios['cantidad'];
+                                                        }
+                                                    }
                                                     if (strlen($datosUsuriosOffline['ulitmo_msj']) > 20) {
                                                         $mensaje = substr($datosUsuriosOffline['ulitmo_msj'], 0, 20) . "...";
                                                     } else {
                                                         $mensaje = $datosUsuriosOffline['ulitmo_msj'];
                                                     }
-                                                    $cantidad = $datosUsuriosOffline['cantidad'];
-                                                    $estado = $datosUsuriosOffline['uss_estado'] == "1" ? "online" : "offline";
+                                                    
                                                     if ($cantidad != "0") {
                                                         $styleName = "font-weight: 700;";
                                                         $className = "badge headerBadgeColor2";
@@ -101,11 +117,11 @@
                                                     }
 
                                             ?>
-                                                    <li class="clearfix" onclick="mostrarChat(this)" id="<?= $datosUsuriosOffline['uss_id'] ?>">
-                                                        <img src="<?= $fotoPerfilUsrOffline ?>" alt="avatar">
+                                                    <li class="clearfix" onclick="mostrarChat(this)" id="<?= $ussId ?>">
+                                                        <img src="<?= $fotoPerfil ?>" alt="avatar">
                                                         <div class="about">
-                                                            <div class="name" Style="<?= $styleName ?>" id="nombre_<?= $datosUsuriosOffline['uss_id'] ?>"><?= $datosUsuriosOffline['uss_nombre'] . ' ' . $datosUsuriosOffline['uss_apellido1'] ?></div>
-                                                            <div class="status"> <i class="fa fa-circle <?= $estado ?>"></i> <?= $mensaje ?> <span class="<?= $className ?>" id="notificacion_<?= $datosUsuriosOffline['uss_id'] ?>"> <?= $cantidad ?></div>
+                                                            <div class="name" Style="<?= $styleName ?>" id="nombre_<?= $ussId ?>"><?= $nombre ?></div>
+                                                            <div class="status"> <i class="fa fa-circle <?= $estado ?>"></i> <?= $mensaje ?> <span class="<?= $className ?>" id="notificacion_<?= $ussId ?>"> <?= $cantidad ?></div>
                                                         </div>
                                                     </li>
                                             <?php
@@ -163,11 +179,11 @@
                         </div>
                     </div>
                     <div class="col-9" style="padding-left: 0px;padding-right: 0px;">
-                        <div class="chat-height" id="contenedorChat">
+                        <div class="contenedor" id="contenedorChat">
                             <div class="table-cell__container">
                                 <span class="table-cell__content">
                                     <div class="div-center image">
-                                        <img src="<?= $fotoPerfilUsr; ?>" height="400px" class="img-circle user-img-circle" alt="User Image">
+                                        <img src="<?= $fotoPerfilUsr; ?>" height="200px" class="img-circle user-img-circle" alt="User Image">
                                         <h1>
                                             <span style='font-family:Arial; font-weight:bold;'>Te damos la bienvenida, <?= $datosUsuarioActual['uss_nombre'] ?></samp>
                                         </h1>
