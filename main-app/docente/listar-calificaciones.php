ba2bc7040c654e2af0ec55b8702ad889dd83fdd0
Enuar Lara <enuar2110@gmail.com>
2023-10-23 17:41:40 -0500

    PES2023-517 - Se muestra cantidad de estudiantes MT

--- main-app/docente/listar-calificaciones.php
+++ main-app/docente/listar-calificaciones.php
@@ -116,7 +116,7 @@ $porcentajeRestante = 100 - $valores[0];
 						");
 					}
 					$numerosEstudiantes = mysqli_fetch_array($consultaNumEstudiante, MYSQLI_BOTH);
-					if($numerosEstudiantes[0]<$cantidadEstudiantes) $bg = '#FCC';
+					if($numerosEstudiantes[0]<$cantidadEstudiantesParaDocentes) $bg = '#FCC';
 						
 						$porcentajeActual +=$resultado['act_valor'];
 						
@@ -141,7 +141,7 @@ $porcentajeRestante = 100 - $valores[0];
 						<td><?=$evidencia['evid_nombre']." (".$evidencia['evid_valor']."%)";?></td>
 					<?php }?>
 					
-					<td style="background-color:<?=$bg;?>"><a href="../compartido/reporte-calificaciones.php?idActividad=<?=base64_encode($resultado['act_id']);?>&grado=<?=base64_encode($datosCargaActual[2]);?>&grupo=<?=base64_encode($datosCargaActual[3]);?>" target="_blank" style="text-decoration: underline;"><?=$numerosEstudiantes[0];?>/<?=$cantidadEstudiantes;?></a></td>
+					<td style="background-color:<?=$bg;?>"><a href="../compartido/reporte-calificaciones.php?idActividad=<?=base64_encode($resultado['act_id']);?>&grado=<?=base64_encode($datosCargaActual[2]);?>&grupo=<?=base64_encode($datosCargaActual[3]);?>" target="_blank" style="text-decoration: underline;"><?=$numerosEstudiantes[0];?>/<?=$cantidadEstudiantesParaDocentes;?></a></td>
 					<td>
 						
 						<?php
