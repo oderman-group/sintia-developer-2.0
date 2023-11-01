dbca0ecbf5756b67d5e6b1555ecf8ab3bed078aa
Enuar Lara <enuar2110@gmail.com>
2023-11-01 11:45:07 -0500

    PES2023-458 - Se crea archivo para actualizar indicadores

--- main-app/directivo/guardar.php
+++ main-app/directivo/guardar.php
@@ -142,59 +142,6 @@ if ($_POST["id"] == 53) {
 	exit();
 }
 //EDITAR INDICADORES DE LOS DOCENTES
-if ($_POST["id"] == 56) {
-	include("verificar-carga.php");
-	//include("verificar-periodos-diferentes.php");
-	try{
-		$consultaSumaIndicadores=mysqli_query($conexion, "SELECT
-		(SELECT sum(ipc_valor) FROM academico_indicadores_carga 
-		WHERE ipc_carga='" . $cargaConsultaActual . "' AND ipc_periodo='" . $periodoConsultaActual . "' AND ipc_creado=0),
-		(SELECT sum(ipc_valor) FROM academico_indicadores_carga 
-		WHERE ipc_carga='" . $cargaConsultaActual . "' AND ipc_periodo='" . $periodoConsultaActual . "' AND ipc_creado=1),
-		(SELECT count(*) FROM academico_indicadores_carga 
-		WHERE ipc_carga='" . $cargaConsultaActual . "' AND ipc_periodo='" . $periodoConsultaActual . "' AND ipc_creado=1)");
-	} catch (Exception $e) {
-		include("../compartido/error-catch-to-report.php");
-	}
-	$sumaIndicadores = mysqli_fetch_array($consultaSumaIndicadores, MYSQLI_BOTH);
-	$porcentajePermitido = 100 - $sumaIndicadores[0];
-	$porcentajeRestante = ($porcentajePermitido - $sumaIndicadores[1]);
-	$porcentajeRestante = ($porcentajeRestante + $_POST["valorIndicador"]);
-
-	try{
-		mysqli_query($conexion, "UPDATE academico_indicadores SET ind_nombre='" . $_POST["contenido"] . "' WHERE ind_id='" . $_POST["idInd"] . "'");
-	} catch (Exception $e) {
-		include("../compartido/error-catch-to-report.php");
-	}
-
-	//Si vamos a relacionar los indicadores con los SABERES
-	if ($datosCargaActual['car_saberes_indicador'] == 1) {
-		try{
-			mysqli_query($conexion, "UPDATE academico_indicadores_carga SET ipc_evaluacion='" . $_POST["saberes"] . "' WHERE ipc_id='" . $_POST["idR"] . "'");
-		} catch (Exception $e) {
-			include("../compartido/error-catch-to-report.php");
-		}
-	}
-
-	//Para los DIRECTIVOS los valores de los indicadores son de forma manual
-	if (!is_numeric($_POST["valor"])) {
-		$_POST["valor"] = 1;
-	}
-	//Si el valor es mayor al adecuado lo ajustamos al porcentaje restante; Siempre que este último sea mayor a 0.
-	if ($_POST["valor"] > $porcentajeRestante and $porcentajeRestante > 0) {
-		$_POST["valor"] = $porcentajeRestante;
-	}
-
-	try{
-		mysqli_query($conexion, "UPDATE academico_indicadores_carga SET ipc_valor='" . $_POST["valor"] . "', ipc_creado='" . $_POST["creado"] . "' WHERE ipc_id='" . $_POST["idR"] . "'");
-	} catch (Exception $e) {
-		include("../compartido/error-catch-to-report.php");
-	}
-
-	include("../compartido/guardar-historial-acciones.php");
-	echo '<script type="text/javascript">window.location.href="cargas-indicadores.php?carga=' . $_GET["carga"] . '&docente=' . $_GET["docente"] . '";</script>';
-	exit();
-}
 //AGREGAR INDICADORES
 //ACTUALIZAR CATEGORÍAS FALTAS
 //ACTUALIZAR FALTAS
