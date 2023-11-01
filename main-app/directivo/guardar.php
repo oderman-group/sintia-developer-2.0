8d31d65d0fd84ce9cc7e5635363393b516a3bd6a
Enuar Lara <enuar2110@gmail.com>
2023-11-01 11:36:51 -0500

    PES2023-458 - Se crea archivo para guardar movimientos

--- main-app/directivo/guardar.php
+++ main-app/directivo/guardar.php
@@ -10,51 +10,6 @@ require_once(ROOT_PATH."/main-app/class/Estudiantes.php");
 
 if (!empty($_POST["id"])) {
 //GUARDAR MOVIMIENTO
-if ($_POST["id"] == 8) {
-	if (trim($_POST["fecha"]) == "" or trim($_POST["detalle"]) == "" or trim($_POST["valor"]) == "" or trim($_POST["tipo"]) == "" or trim($_POST["forma"]) == "") {
-		include("../compartido/guardar-historial-acciones.php");
-		echo "<span style='font-family:Arial; color:red;'>Debe llenar todos los campos.</samp>";
-		exit();
-	}
-	if ($_POST["tipo"] == 1) {
-
-		try{
-			$consultaConsecutivoActual=mysqli_query($conexion, "SELECT * FROM finanzas_cuentas WHERE fcu_tipo=1 ORDER BY fcu_id DESC");
-		} catch (Exception $e) {
-			include("../compartido/error-catch-to-report.php");
-		}
-		$consecutivoActual = mysqli_fetch_array($consultaConsecutivoActual, MYSQLI_BOTH);
-		if ($consecutivoActual['fcu_consecutivo'] == "") {
-			$consecutivo = $config['conf_inicio_recibos_ingreso'];
-		} else {
-			$consecutivo = $consecutivoActual['fcu_consecutivo'] + 1;
-		}
-	}
-	if ($_POST["tipo"] == 2) {
-
-		try{
-			$consultaConsecutivoActual=mysqli_query($conexion, "SELECT * FROM finanzas_cuentas WHERE fcu_tipo=2 ORDER BY fcu_id DESC");
-		} catch (Exception $e) {
-			include("../compartido/error-catch-to-report.php");
-		}
-		$consecutivoActual = mysqli_fetch_array($consultaConsecutivoActual, MYSQLI_BOTH);
-		if ($consecutivoActual['fcu_consecutivo'] == "") {
-			$consecutivo = $config['conf_inicio_recibos_egreso'];
-		} else {
-			$consecutivo = $consecutivoActual['fcu_consecutivo'] + 1;
-		}
-	}
-
-	try{
-		mysqli_query($conexion, "INSERT INTO finanzas_cuentas(fcu_fecha, fcu_detalle, fcu_valor, fcu_tipo, fcu_observaciones, fcu_usuario, fcu_anulado, fcu_forma_pago, fcu_cerrado, fcu_consecutivo)VALUES('" . $_POST["fecha"] . "','" . $_POST["detalle"] . "','" . $_POST["valor"] . "','" . $_POST["tipo"] . "','" . $_POST["obs"] . "','" . $_POST["usuario"] . "',0,'" . $_POST["forma"] . "',0,'" . $consecutivo . "')");
-	} catch (Exception $e) {
-		include("../compartido/error-catch-to-report.php");
-	}
-
-	include("../compartido/guardar-historial-acciones.php");
-	echo '<script type="text/javascript">window.location.href="movimientos.php";</script>';
-	exit();
-}
 //GUARDAR REPORTE
 if ($_POST["id"] == 9) {
 	if (trim($_POST["fecha"]) == "" or trim($_POST["codigo"]) == "" or trim($_POST["falta"]) == "" or trim($_POST["tipo"]) == "") {
