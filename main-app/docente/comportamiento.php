e9f5adb22c2b98798d2fd2f334e2cb4e48a22175
Enuar Lara <enuar2110@gmail.com>
2023-11-07 07:20:03 -0500

    PES2023-482 - Se crea archivo para eliminar nota de comportamiento

--- main-app/docente/comportamiento.php
+++ main-app/docente/comportamiento.php
@@ -88,6 +88,7 @@ $('#respRC').empty().hide().html("Guardando información, espere por favor...").
                             </div>
                         </div>
                     </div>
+					<?php include(ROOT_PATH."/config-general/mensajes-informativos.php"); ?>
                     <?php include("includes/barra-superior-informacion-actual.php"); ?>
                     <div class="row">
                         <div class="col-md-12">
@@ -161,7 +162,7 @@ $('#respRC').empty().hide().html("Guardando información, espere por favor...").
 														<td width="15%">
 															<input type="text" style="text-align: center; color:<?=$colorNota;?>" size="5" maxlength="3" value="<?php if(!empty($notas['dn_nota'])){ echo $notas['dn_nota'];}?>" name="N<?=$contReg;?>" id="<?=$resultado['mat_id'];?>" alt="<?=$resultado['mat_nombres'];?>" title="5" onChange="notas(this)" tabindex="<?=$contReg;?>">
 															<?php if(!empty($notas['dn_nota'])){?>
-															<a href="#" name="guardar.php?get=<?=base64_encode(31);?>&id=<?=base64_encode($notas['dn_id']);?>" onClick="deseaEliminar(this)">X</a>
+															<a href="#" name="comportamiento-nota-eliminar.php?id=<?=base64_encode($notas['dn_id']);?>" onClick="deseaEliminar(this)">X</a>
 															<?php }?>
 														</td>
 														<td width="50%">
