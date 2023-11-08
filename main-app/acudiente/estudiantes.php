11873ff2489c3d268f13017897b07503af8d3bde
Jhon Oderman <jhooderman@gmail.com>
2023-11-08 01:34:07 -0500

    TS - Mostrar icono para destacar estudiante con resportes pendientes, y mostrar estado de solicitud

--- main-app/acudiente/estudiantes.php
+++ main-app/acudiente/estudiantes.php
@@ -135,12 +135,22 @@ require_once("../class/Estudiantes.php");
 
 															$aspectos = mysqli_fetch_array(mysqli_query($conexion, "SELECT * FROM ".BD_DISCIPLINA.".disiplina_nota 
 															WHERE dn_cod_estudiante=" . $resultado['mat_id'] . " AND dn_periodo='" . $config['conf_periodo'] . "' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}"), MYSQLI_BOTH);
+
+															$numReportesDis = mysqli_num_rows(mysqli_query($conexion, "SELECT * FROM ".BD_DISCIPLINA.".disciplina_reportes 
+															INNER JOIN academico_matriculas ON mat_id_usuario=dr_estudiante AND mat_acudiente='".$_SESSION["id"]."'
+															WHERE dr_aprobacion_acudiente=0 AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}
+															AND dr_estudiante='".$resultado['mat_id_usuario']."'"));
+
+															$iconoReportesDisciplinario = '';
+															if($numReportesDis > 0) {
+																$iconoReportesDisciplinario = '<i class="fa fa-warning text-warning" aria-hidden="true" data-toggle="tooltip" data-placement="top" title="Reporte disciplinario pendiente por firmar ('.$numReportesDis.')"></i> ';
+															}
 													 ?>
 													<tr>
                                                         <td><?=$contReg;?></td>
                                                         <td><?php echo $resultado['uss_usuario'];?></td>
 														<td><?php echo $resultado['mat_id'];?></td>
-														<td><?=Estudiantes::NombreCompletoDelEstudiante($resultado);?></td>
+														<td><?=$iconoReportesDisciplinario."".Estudiantes::NombreCompletoDelEstudiante($resultado);?></td>
 														<td><?php if(!empty($genero[1])) echo $genero[1];?></td>
 														<td><?=strtoupper($resultado['gra_nombre']." ".$resultado['gru_nombre']);?></td>
 														<td>
@@ -148,7 +158,7 @@ require_once("../class/Estudiantes.php");
 															$respuesta =0;
 														 	if($config['conf_activar_encuesta']==1){
 																$respuesta = mysqli_num_rows(mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".general_encuestas 
-																WHERE genc_estudiante='".$resultado['mat_id']."'"));
+																WHERE genc_estudiante='".$resultado['mat_id']."' AND genc_institucion={$config['conf_id_institucion']} AND genc_year={$_SESSION["bd"]}"));
 															}
 														 
 														 if($config['conf_activar_encuesta']!=1 or $respuesta>0){	
@@ -200,11 +210,25 @@ require_once("../class/Estudiantes.php");
 																	  </ul>
 																  </div>
 															<?php
-																	}else{
-																		echo "
-																		<span style='color:red;'>".$frases[268][$datosUsuarioActual[8]]."</span><br>
-																		<a href='".$_SERVER['PHP_SELF']."?req=1&idE=".base64_encode($resultado['mat_id'])."&nameE=".base64_encode($resultado['uss_nombre'])."' style='text-decoration:underline;'>".$frases[269][$datosUsuarioActual[8]]."</a>
-																		";
+																	} else {
+																		$consultaSolicitudes = mysqli_query($conexion, "SELECT * FROM ".$baseDatosServicios.".general_solicitudes 
+																		LEFT JOIN usuarios ON uss_id=soli_remitente
+																		LEFT JOIN academico_matriculas ON mat_id=soli_id_recurso
+																		WHERE soli_institucion='".$config['conf_id_institucion']."' 
+																		AND soli_year='".$_SESSION["bd"]."' AND soli_id_recurso={$resultado['mat_id']} AND soli_estado!=3");
+																		$solicitudPendiente = mysqli_fetch_array($consultaSolicitudes, MYSQLI_BOTH);
+																		
+																		if( !empty($solicitudPendiente) ) {
+															?>
+																			<span style='color:darkblue;'>Solicitud de desbloqueo <b><?=$estadosSolicitudes[$solicitudPendiente['soli_estado']];?></b></span>
+															<?php
+
+																		} else {
+																			echo "
+																			<span style='color:red;'>".$frases[268][$datosUsuarioActual[8]]."</span><br>
+																			<a href='".$_SERVER['PHP_SELF']."?req=1&idE=".base64_encode($resultado['mat_id'])."&nameE=".base64_encode($resultado['uss_nombre'])."' style='text-decoration:underline;'>".$frases[269][$datosUsuarioActual[8]]."</a>
+																			";
+																		}
 																	}	
 																}
 																else{}
