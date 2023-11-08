f0c0be3f076c2a37364f816711bfab77d15a29f9
Enuar Lara <enuar2110@gmail.com>
2023-11-08 09:48:34 -0500

    PES2023-546 - Se crea archivo para gestionar foros

---
+++ main-app/compartido/foros-eliminar-comentario.php
@@ -0,0 +1,25 @@
+<?php
+include("session-compartida.php");
+Modulos::validarAccesoDirectoPaginas();
+$idPaginaInterna = 'CM0032';
+include(ROOT_PATH."/main-app/compartido/historial-acciones-guardar.php");
+include(ROOT_PATH."/main-app/compartido/sintia-funciones.php");
+$usuariosClase = new Usuarios;
+
+try{
+    mysqli_query($conexion, "DELETE FROM academico_actividad_foro_respuestas WHERE fore_id_comentario='" . base64_decode($_GET["idCom"]) . "'");
+} catch (Exception $e) {
+	include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
+}
+
+try{
+    mysqli_query($conexion, "DELETE FROM academico_actividad_foro_comentarios WHERE com_id='" . base64_decode($_GET["idCom"]) . "'");
+} catch (Exception $e) {
+	include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
+}
+
+$url= $usuariosClase->verificarTipoUsuario($datosUsuarioActual['uss_tipo'],'foros-detalles.php?idR='.$_GET['idR']);
+
+include(ROOT_PATH."/main-app/compartido/guardar-historial-acciones.php");
+echo '<script type="text/javascript">window.location.href="' . $url . '";</script>';
+exit();
