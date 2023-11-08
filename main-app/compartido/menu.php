2661bde0e375c09b7cd81de2fae2363875227ced
Jhon Oderman <jhooderman@gmail.com>
2023-11-08 01:38:09 -0500

    TS - Mover el marketplace de acudientes a la última opción

--- main-app/compartido/menu.php
+++ main-app/compartido/menu.php
@@ -481,15 +481,15 @@
 	                            </a>
 	                        </li>
 							
-							<li class="nav-item active" data-step="11" data-intro="<b><?=$frases[175][$datosUsuarioActual[8]];?>:</b> Encuentra los mejores productos y servicios complementarios." data-position='left'>
-	                            <a href="marketplace.php" class="nav-link nav-toggle"> <i class="fa fa-shopping-cart fa-spin"></i>
-	                                <span class="title">Marketplace</span> 
-	                            </a>
-	                        </li>
-							
 							<li class="nav-item" data-step="12" data-intro="<b><?=$frases[104][$datosUsuarioActual[8]];?>:</b> Aquí verás toda la información relacionada con tu estado de cuenta financiero." data-position='left'>
 	                            <a href="estado-de-cuenta.php" class="nav-link nav-toggle"> <i class="material-icons">attach_money</i>
 	                                <span class="title"><?=$frases[104][$datosUsuarioActual['uss_idioma']];?></span> 
+	                            </a>
+	                        </li>
+
+							<li class="nav-item active" data-step="11" data-intro="<b><?=$frases[175][$datosUsuarioActual[8]];?>:</b> Encuentra los mejores productos y servicios complementarios." data-position='left'>
+	                            <a href="marketplace.php" class="nav-link nav-toggle bg-warning text-dark"> <i class="fa fa-shopping-cart text-dark"></i>
+	                                <span class="title">Marketplace</span> 
 	                            </a>
 	                        </li>
 							
