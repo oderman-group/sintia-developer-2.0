5997bca18e8d877267f4092dbeacba08f513572c
Enuar Lara <enuar2110@gmail.com>
2023-11-13 13:55:36 -0500

    PES2023-522 - Se crea archivo ajax para traer nota caulitativa

---
+++ main-app/compartido/ajax-estilo-notas.php
@@ -0,0 +1,13 @@
+<?php 
+include("session-compartida.php");
+require_once(ROOT_PATH."/main-app/class/Boletin.php");
+
+// Modulos::validarAccesoDirectoPaginas();
+
+$idPaginaInterna = 'CM0059';
+include(ROOT_PATH."/main-app/compartido/historial-acciones-guardar.php");
+
+$estiloNota = Boletin::obtenerDatosTipoDeNotas($config['conf_notas_categoria'], $_GET['nota']);
+echo $estiloNota['notip_nombre'];
+
+include(ROOT_PATH."/main-app/compartido/guardar-historial-acciones.php");
