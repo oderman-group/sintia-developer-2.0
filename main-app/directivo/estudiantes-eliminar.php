426cb7fb2931c318e2811ca2c7187aad9ef92e70
Enuar Lara <enuar2110@gmail.com>
2023-11-16 22:52:35 -0500

    PES-554 - Se actualizan consultas a tabla academico_recuperaciones_notas

--- main-app/directivo/estudiantes-eliminar.php
+++ main-app/directivo/estudiantes-eliminar.php
@@ -61,7 +61,7 @@ try{
 	include("../compartido/error-catch-to-report.php");
 }
 try{
-    mysqli_query($conexion, "DELETE FROM academico_recuperaciones_notas WHERE rec_cod_estudiante='" . $idE . "'");
+    mysqli_query($conexion, "DELETE FROM ".BD_ACADEMICA.".academico_recuperaciones_notas WHERE rec_cod_estudiante='" . $idE . "' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
 } catch (Exception $e) {
 	include("../compartido/error-catch-to-report.php");
 }
