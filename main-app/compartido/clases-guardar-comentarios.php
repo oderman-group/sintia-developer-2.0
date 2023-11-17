9f88a899c524bf41004d7ae09cff1e1edbf7878d
Enuar Lara <enuar2110@gmail.com>
2023-11-16 20:10:35 -0500

    PES-553 - Se actualizan consultas a tabla academico_clases_preguntas

--- main-app/compartido/clases-guardar-comentarios.php
+++ main-app/compartido/clases-guardar-comentarios.php
@@ -4,10 +4,12 @@ Modulos::validarAccesoDirectoPaginas();
 $idPaginaInterna = 'CM0026';
 include(ROOT_PATH."/main-app/compartido/historial-acciones-guardar.php");
 include(ROOT_PATH."/main-app/compartido/sintia-funciones.php");
+require_once(ROOT_PATH."/main-app/class/Utilidades.php");
+$codigo=Utilidades::generateCode("CPP");
 $usuariosClase = new Usuarios;
 
 try{
-    mysqli_query($conexion, "INSERT INTO academico_clases_preguntas(cpp_usuario, cpp_fecha, cpp_id_clase, cpp_contenido)VALUES('" . $_SESSION["id"] . "', now(), '" . $_POST["idClase"] . "', '" . mysqli_real_escape_string($conexion,$_POST["contenido"]) . "')");
+    mysqli_query($conexion, "INSERT INTO ".BD_ACADEMICA.".academico_clases_preguntas(cpp_id, cpp_usuario, cpp_fecha, cpp_id_clase, cpp_contenido, institucion, year)VALUES('".$codigo."', '" . $_SESSION["id"] . "', now(), '" . $_POST["idClase"] . "', '" . mysqli_real_escape_string($conexion,$_POST["contenido"]) . "', {$config['conf_id_institucion']}, {$_SESSION["bd"]})");
 } catch (Exception $e) {
 	include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
 }
