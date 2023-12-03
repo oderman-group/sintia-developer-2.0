00cb90f119db9082b036239e26886f1f7d2e7e64
El gran Sherman <jhooderman@gmail.com>
2023-12-03 14:40:18 -0500

    JM - Correción en consultas SQL

--- main-app/directivo/estudiantes-eliminar.php
+++ main-app/directivo/estudiantes-eliminar.php
@@ -76,7 +76,7 @@ try{
 	include("../compartido/error-catch-to-report.php");
 }
 try{
-    mysqli_query($conexion, "DELETE FROM ".BD_DISCIPLINA.".disiplina_nota WHERE dn_cod_estudiante='" . $idE . "' WHERE institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
+    mysqli_query($conexion, "DELETE FROM ".BD_DISCIPLINA.".disiplina_nota WHERE dn_cod_estudiante='" . $idE . "' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
 } catch (Exception $e) {
 	include("../compartido/error-catch-to-report.php");
 }
@@ -86,7 +86,7 @@ try{
 	include("../compartido/error-catch-to-report.php");
 }
 try{
-    mysqli_query($conexion, "DELETE FROM ".$baseDatosServicios.".general_resultados WHERE resg_id_estudiante='" . $idE . "'");
+    mysqli_query($conexion, "DELETE FROM ".$baseDatosServicios.".general_resultados WHERE resg_id_usuario='" . $idU . "'");
 } catch (Exception $e) {
 	include("../compartido/error-catch-to-report.php");
 }
