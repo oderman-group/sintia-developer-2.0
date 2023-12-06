2577b6b770bb7a488cc90654bfa8044e8d72c06b
El gran Sherman <jhooderman@gmail.com>
2023-12-06 11:19:52 -0500

    JM - Mejora en reserva de cupos

--- main-app/directivo/estudiantes-reservar-cupo.php
+++ main-app/directivo/estudiantes-reservar-cupo.php
@@ -1,9 +1,15 @@
 <?php
 include("session.php");
 include("../modelo/conexion.php");
+require_once(ROOT_PATH."/main-app/class/UsuariosPadre.php");
 
 try{
-	mysqli_query($conexion, "INSERT INTO ".$baseDatosServicios.".general_encuestas(genc_estudiante, genc_fecha, genc_respuesta, genc_comentario, genc_institucion, genc_year) VALUES('".base64_decode($_GET["idEstudiante"])."', now(), 1, 'Reservado por un directivo.','" . $config['conf_id_institucion'] . "','" . $_SESSION["bd"] . "')");
+	mysqli_query($conexion, "INSERT INTO ".$baseDatosServicios.".general_encuestas(genc_estudiante, genc_fecha, genc_respuesta, genc_comentario, genc_institucion, genc_year) VALUES('".base64_decode($_GET["idEstudiante"])."', now(), 1, 'Reservado por un directivo (".UsuariosPadre::nombreCompletoDelUsuario($datosUsuarioActual).").','" . $config['conf_id_institucion'] . "','" . $_SESSION["bd"] . "') 
+	ON DUPLICATE KEY UPDATE
+	genc_fecha = VALUES(genc_fecha),
+	genc_comentario = VALUES(genc_comentario),
+	genc_respuesta = VALUES(genc_respuesta)
+	");
 } catch (Exception $e) {
 	include("../compartido/error-catch-to-report.php");
 }
