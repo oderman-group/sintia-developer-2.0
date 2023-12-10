f51c460dcb5167987f014e52854342020358ae89
ing-enuarlara <enuar2110@gmail.com>
2023-12-09 23:54:05 -0500

    PES-440 - Mejora para la paginación

--- main-app/directivo/includes/consulta-paginacion-estudiantes.php
+++ main-app/directivo/includes/consulta-paginacion-estudiantes.php
@@ -4,7 +4,7 @@
     $numRegistros = count($keys);
     $registros= $config['conf_num_registros'];
     $pagina=base64_decode($_REQUEST["nume"]);
-    $pagina = isset($_REQUEST['nume']) ? intval($pagina) : 1;
+    $pagina = isset($_REQUEST['nume']) ? intval($pagina) : base64_encode(1);
     if (is_numeric($pagina)){
         $inicio= (($pagina-1)*$registros);
     }			     
