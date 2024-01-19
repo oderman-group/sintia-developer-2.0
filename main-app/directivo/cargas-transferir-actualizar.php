460daaa4708e4c285109af7b56ab3a9c7edaaf59
Enuar Lara <enuar2110@gmail.com>
2024-01-19 08:52:08 -0500

    PES-656 - Se modifica archivo para nuevo funcionamiento

--- main-app/directivo/cargas-transferir-actualizar.php
+++ main-app/directivo/cargas-transferir-actualizar.php
@@ -1,21 +1,23 @@
 <?php
-include("session.php");
+	include("session.php");
 
-Modulos::validarAccesoDirectoPaginas();
-$idPaginaInterna = 'DT0171';
+	Modulos::validarAccesoDirectoPaginas();
+	$idPaginaInterna = 'DT0171';
 
-if(!Modulos::validarSubRol([$idPaginaInterna])){
-	echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
-	exit();
-}
-include("../compartido/historial-acciones-guardar.php");
+	if(!Modulos::validarSubRol([$idPaginaInterna])){
+		echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
+		exit();
+	}
+	include("../compartido/historial-acciones-guardar.php");
 
-try{
-	mysqli_query($conexion, "UPDATE ".BD_ACADEMICA.".academico_cargas SET car_docente='" . $_POST["para"] . "' WHERE car_docente='" . $_POST["de"] . "' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
-} catch (Exception $e) {
-	include("../compartido/error-catch-to-report.php");
-}
+	foreach ($_POST["cargas"] as $idCarga) {
+		try{
+			mysqli_query($conexion, "UPDATE ".BD_ACADEMICA.".academico_cargas SET car_docente='" . $_POST["para"] . "' WHERE car_id='" . $idCarga . "' AND car_docente='" . $_POST["de"] . "' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
+		} catch (Exception $e) {
+			include("../compartido/error-catch-to-report.php");
+		}
+	}
+
 	include("../compartido/guardar-historial-acciones.php");
-
-	echo '<script type="text/javascript">window.location.href="cargas-transferir.php?success='.base64_encode('SC_DT_13').'";</script>';
+	echo '<script type="text/javascript">window.location.href="cargas.php?success='.base64_encode('SC_DT_13').'";</script>';
 	exit();
