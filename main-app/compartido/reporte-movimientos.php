5f0cec0f404f4cae4e9d2601f39718d2607041e4
Enuar Lara <enuar2110@gmail.com>
2024-01-19 14:22:39 -0500

    PES-590 - Se actualiza encabezado, footer y colores de reporte financiero

--- main-app/compartido/reporte-movimientos.php
+++ main-app/compartido/reporte-movimientos.php
@@ -12,16 +12,15 @@ require_once("../class/UsuariosPadre.php");
 <head>
 	<title>Movimientos Financieros</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link rel="shortcut icon" href="../files/images/ico.png">
+    <link rel="shortcut icon" href="<?= $Plataforma->logo; ?>">
 </head>
 <body style="font-family:Arial;">
-<div align="center" style="margin-bottom:20px;">
-    <img src="../files/images/logo/<?=$informacion_inst["info_logo"]?>" height="150" width="250"><br>
-    <?=$informacion_inst["info_nombre"]?><br>
-    INFORME DE MOVIMIENTOS</br>
-</div>   
-  <table bgcolor="#FFFFFF" width="100%" cellspacing="5" cellpadding="5" rules="all" border="<?php echo $config[13] ?>" style="border:solid; border-color:<?php echo $config[11] ?>;" align="center">
-  <tr style="font-weight:bold; font-size:12px; height:30px; background:<?php echo $config[12] ?>;">
+  <?php
+    $nombreInforme = "INFORME DE MOVIMIENTOS";
+    include("../compartido/head-informes.php");
+  ?>
+  <table bgcolor="#FFFFFF" width="100%" cellspacing="5" cellpadding="5" rules="all" border="<?php echo $config[13] ?>" style="border:solid; border-color:<?= $Plataforma->colorUno; ?>;" align="center">
+  <tr style="font-weight:bold; font-size:12px; height:30px; background:<?= $Plataforma->colorUno; ?>; color:#FFF;">
         <th>ID</th>
                                         <th>Usuario</th>
                                         <th>Fecha</th>
@@ -58,10 +57,7 @@ require_once("../class/UsuariosPadre.php");
   ?>
   </table>
   </center>
-	<div align="center" style="font-size:10px; margin-top:10px;">
-      <img src="../files/images/sintia.png" height="50" width="100"><br>
-      SINTIA -  SISTEMA INTEGRAL DE GESTI&Oacute;N INSTITUCIONAL - <?=date("l, d-M-Y");?>
-     </div>
+    <?php include("../compartido/footer-informes.php"); ?>
 </body>
 </html>
 <?php
