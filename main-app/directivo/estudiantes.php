b8aea9b9a3af6db64ed70207b748be70db1e3f21
Enuar Lara <enuar2110@gmail.com>
2024-01-23 17:26:37 -0500

    PES-714 - Se valida que boletín se le imprima a estudiantes retirados, asistentes y matriculados

--- main-app/directivo/estudiantes.php
+++ main-app/directivo/estudiantes.php
@@ -281,7 +281,7 @@ if($config['conf_doble_buscador'] == 1) {
 																	<?php }?>
 																	
 																	<?php if(!empty($resultado['mat_grado']) && !empty($resultado['mat_grupo'])) {?>
-																		<?php if(Modulos::validarSubRol(['DT0224'])){?>
+																		<?php if(Modulos::validarSubRol(['DT0224']) && ($resultado['mat_estado_matricula'] != 4 && $resultado['mat_estado_matricula'] != 5)){?>
 																		<li><a href="../compartido/matricula-boletin-curso-<?=$resultado['gra_formato_boletin'];?>.php?id=<?=base64_encode($resultado["mat_id"]);?>&periodo=<?=base64_encode($config[2]);?>" target="_blank">Boletín</a></li>
                                                         				<?php }?>
 																		<?php if(Modulos::validarSubRol(['DT0247'])){?>
