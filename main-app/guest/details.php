e1f0fc2f848379b4488ad537976b13ef88f27dc4
Admin Softcomputo <softcomputo77@gmail.com>
2024-01-26 01:19:10 -0500

    PES-631 -Crear usuario matricula y registro de media tecnica

--- main-app/guest/details.php
+++ main-app/guest/details.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once($_SERVER['DOCUMENT_ROOT'] . "/app-sintia/config-general/constantes.php");
-$disabled = "";
+$readonly = "";
 $usuario = "";
 $nombre = "";
 $apellido = "";
@@ -9,6 +9,10 @@ $correo = "";
 $tipoUsuario = "Estudiante";
 $mensaje = "";
 $identificacion = "";
+$hidden = "";
+$inscrito = "";
+
+
 if (isset($_SESSION["id"]) and $_SESSION["id"] != "") {
 
     require_once(ROOT_PATH . "/main-app/modelo/conexion.php");
@@ -16,7 +20,8 @@ if (isset($_SESSION["id"]) and $_SESSION["id"] != "") {
 
     $consultaSesion = UsuariosPadre::obtenerTodosLosDatosDeUsuarios(" AND uss_id='" . $_SESSION["id"] . "'");
     $sesionAbierta = mysqli_fetch_array($consultaSesion, MYSQLI_BOTH);
-    $disabled = "disabled";
+    $readonly = "readonly";
+    $hidden = "hidden";
     $identificacion = $sesionAbierta['uss_documento'];;
     $tipoUsuario = $sesionAbierta['pes_nombre'];
     $usuario = $sesionAbierta['uss_usuario'];
@@ -33,9 +38,27 @@ if (isset($_SESSION["id"]) and $_SESSION["id"] != "") {
     $conexion = mysqli_connect($servidorConexion, $usuarioConexion, $claveConexion, $baseDatosServicios);
 }
 
+
 $consultaCurso = mysqli_query($conexion, "SELECT * FROM " . BD_ACADEMICA . ".academico_grados 
 WHERE id_nuevo = '" . $_GET["course"] . "' AND gra_estado=1 AND gra_active=1 AND gra_auto_enrollment=1 AND gra_tipo='" . GRADO_INDIVIDUAL . "'");
 $resultado = mysqli_fetch_array($consultaCurso, MYSQLI_BOTH);
+
+
+if (isset($_SESSION["id"]) and $_SESSION["id"] != "") {
+    require_once("../class/servicios/MediaTecnicaServicios.php");
+
+    $consultaInscrito = mysqli_query($conexion, "SELECT mts.* FROM " . BD_ACADEMICA . ".academico_matriculas
+    INNER JOIN mobiliar_sintia_admin.mediatecnica_matriculas_cursos mts ON(mat_id=matcur_id_matricula) 
+    WHERE mat_documento = '" . $identificacion . "' 
+    AND matcur_id_curso='" . $resultado["gra_codigo"] . "'  
+    AND matcur_id_institucion='" . $resultado["institucion"] . "'
+    AND matcur_years='" . $resultado["year"] . "'");
+    $matriculaCurso = mysqli_fetch_array($consultaInscrito, MYSQLI_BOTH);
+    if ($matriculaCurso != null) {
+        $inscrito = "hidden";
+    }
+}
+
 
 require_once(ROOT_PATH . "/main-app/class/Plataforma.php");
 $datosContactoSintia = Plataforma::infoContactoSintia();
@@ -55,6 +78,7 @@ $datosContactoSintia = Plataforma::infoContactoSintia();
 
 <body>
     <div class="course-details">
+        <?php include("../../config-general/mensajes-informativos.php"); ?>
         <div class="card" style="width:100%">
             <img class="card-img-top course-image" src="<?= $resultado['gra_cover_image']; ?>" alt="Card image cap">
             <div class="card-body">
@@ -62,7 +86,7 @@ $datosContactoSintia = Plataforma::infoContactoSintia();
                 <p class="card-text">
                     <?= $resultado['gra_overall_description']; ?>
                 </p>
-                <p class="text-right" style=" flex-flow: row wrap; font-size: 3.2rem;"> $<?= number_format($resultado['gra_price'], 0, ",", "."); ?></p>
+                <p class="text-right" style=" flex-flow: row wrap; font-size: 3.2rem;color:green"> $<?= number_format($resultado['gra_price'], 0, ",", "."); ?></p>
                 <i class="fa fa-check"></i>
 
             </div>
@@ -71,7 +95,7 @@ $datosContactoSintia = Plataforma::infoContactoSintia();
 
                 <div class="card">
                     <div class="card-header" id="head1" data-target="#card1" data-toggle="collapse">
-                        <h2 class="mb-0">Contenido del curso <?= $disabled ?></h2>
+                        <h2 class="mb-0">Contenido del curso </h2>
                     </div>
                     <div id="card1" class="collapse show" aria-labelledby="head1" data-parent="#accordion">
                         <div class="card-body">
@@ -87,8 +111,7 @@ $datosContactoSintia = Plataforma::infoContactoSintia();
                         <div class="card-body">
                             <ul>
                                 <li>Duración: <?= $resultado['gra_duration_hours']; ?> horas</li>
-                                <li>Nivel: <?= $resultado['gra_duration_hours']; ?></li>
-                                <!-- <li>Instructor: Juan Pérez, María Gómez</li> -->
+                                <li>Nivel: <?= $resultado['gra_nivel']; ?></li>
 
                             </ul>
                             <i class="fas fa-car"></i>
@@ -98,7 +121,9 @@ $datosContactoSintia = Plataforma::infoContactoSintia();
                 <div class="card">
                     <div class="card-header text-center">
                         <i class="bi bi-cart-check"></i>
-                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Inscribirme</button>
+                        <button type="button" <?= $inscrito ?> class="btn btn-primary" data-toggle="modal" data-target="#Modal2">Inscribirme</button>
+                        <h2 class="mb-0" style="color:green" <?= empty($inscrito) ? "hidden" : "" ?>>Estoy inscrito</h2>
+                        <button type="button" <?= $hidden ?> class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Iniciar session</button>
                     </div>
                 </div>
 
@@ -106,9 +131,43 @@ $datosContactoSintia = Plataforma::infoContactoSintia();
         </div>
     </div>
 
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="container" style="text-align: center;">
+                    <form method="post" action="../controlador/autentico.php" class="needs-validation" novalidate>
+
+                        <input type="hidden" name="bd" value="<?= $resultado["institucion"] ?>" />
+                        <img class="mb-4" src="../../config-general/assets-login-2023/img/logo.png" width="100">
 
 
-    <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+                        <div class=" form-floating mt-3">
+                            <label for="emailInput">Usuario</label>
+                            <input type="text" class="form-control input-login" id="emailInput" name="Usuario" placeholder="Usuario" required>
+                            <div class="invalid-feedback">Por favor ingrese un correo electrónico válido.</div>
+                        </div>
+                        <div class=" form-floating mt-3">
+                            <label for="emailInput">Contraseña</label>
+                            <input type="password" class="form-control input-login" id="password" name="Clave" placeholder="Password" required>
+
+                            <div class="invalid-feedback">Por favor ingrese un correo electrónico válido.</div>
+                        </div>
+
+
+
+
+
+                        <button class="w-75 btn btn-lg btn-primary btn-rounded mt-3" type="submit">Empezar la aventura</button>
+                        <div class="d-flex justify-content-center mt-5">
+                            <p><a href="https://docs.google.com/forms/d/e/1FAIpQLSdiugXhzAj0Ysmt2gthO07tbvjxTA7CHcZqgzBpkefZC6T2qg/viewform" class="text-body" target="_blank">¿Requieres soporte?</a></p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade bd-example-modal-lg" id="Modal2" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="container">
@@ -119,43 +178,44 @@ $datosContactoSintia = Plataforma::infoContactoSintia();
                         <div class="row m-2">
                             <div class="col-md-12">
                                 <h3 class="card-title">INSCRIBIR AL CURSO</h3>
+                                <input type="text" hidden name="institucion" class="form-control" id="tipoUsuario" value="<?= $resultado["institucion"] ?>">
+                                <input type="text" hidden name="year" class="form-control" id="tipoUsuario" value="<?= $resultado["year"] ?>">
+                                <input type="text" hidden name="curso" class="form-control" id="curso" value="<?= $resultado["gra_codigo"] ?>">
+                                <input type="text" hidden name="curso_id" class="form-control" id="curso_id" value="<?= $_GET["course"] ?>">
                             </div>
                             <div class="col-md-6">
-
-
                                 <div class="form-group">
                                     <label for="nombre">Tipo de usuario actual:</label>
-                                    <input type="text" class="form-control" id="usuario" placeholder="Ingrese su nombre" value="<?= $tipoUsuario ?>" disabled>
+                                    <input type="text" name="tipoUsuario" class="form-control" id="tipoUsuario" placeholder="Ingrese su nombre" value="<?= $tipoUsuario ?>" readonly>
                                 </div>
                                 <div class="form-group">
-                                    <label for="nombre">Identificacion:</label>
-                                    <div class="input-group mb-3">
-                                        <input type="text" class="form-control" id="identificacion" onChange="nuevoEstudiante(this)" placeholder="Ingrese su Identificacion " value="<?= $identificacion ?>" <?= $disabled; ?>>
-                                        <div class="input-group-append">
-                                            <button class="btn btn-outline-secondary" type="button">Button</button>
-                                        </div>
+                                    <label for="nombre">Identificacion:<span style="color: blue; font-size: 15px;" id="nDocu"></span></label>
+                                    <div id="grupo-identificacion" class="input-group mb-3">
+                                        <input type="text" name="identificacion" class="form-control" required id="identificacion" onChange="validarExistencia(this,'<?= IDENTIFICAION ?>','nDocu','grupo-identificacion')" placeholder="Ingrese su Identificacion " value="<?= $identificacion ?>" <?= $readonly; ?>>
                                     </div>
-                                    <span style="color: blue; font-size: 15px;" id="nDocu"></span>
-
                                 </div>
 
 
                                 <div class="form-group">
-                                    <label for="nombre">Usuario:</label>
-                                    <input type="text" class="form-control" id="usuario" placeholder="Ingrese su nombre" value="<?= $usuario ?>" <?= $disabled; ?>>
+                                    <label for="nombre">Usuario: <span style="color: blue; font-size: 15px;" id="vUsua"></label>
+                                    <div id="grupo-usuario" class="input-group mb-3">
+                                        <input type="text" name="usuario" required class="form-control" id="usuario" onChange="validarExistencia(this,'<?= USUARIO ?>','vUsua','grupo-usuario')" placeholder="Ingrese su nombre" value="<?= $usuario ?>" <?= $readonly; ?>>
+                                    </div>
                                     <span style="font-size: 10px; color:darkblue;"><?= $mensaje; ?></span>
                                 </div>
                                 <div class="form-group">
                                     <label for="nombre">Nombre:</label>
-                                    <input type="text" class="form-control" id="nombre" placeholder="Ingrese su nombre" value="<?= $nombre ?>" <?= $disabled; ?>>
+                                    <input type="text" name="nombre" required class="form-control" id="nombre" placeholder="Ingrese su nombre" value="<?= $nombre ?>" <?= $readonly; ?>>
                                 </div>
                                 <div class="form-group">
                                     <label for="nombre">Apellido:</label>
-                                    <input type="text" class="form-control" id="apellido" placeholder="Ingrese su apellido" value="<?= $apellido; ?>" <?= $disabled; ?>>
+                                    <input type="text" name="apellido" class="form-control" id="apellido" placeholder="Ingrese su apellido" value="<?= $apellido; ?>" <?= $readonly; ?>>
                                 </div>
                                 <div class="form-group">
-                                    <label for="tarjeta">Correo:</label>
-                                    <input type="text" class="form-control" id="correo" placeholder="Ingrese correo Electronico" value="<?= $correo; ?>" <?= $disabled; ?>>
+                                    <label for="tarjeta">Correo: <span style="color: blue; font-size: 15px;" id="vCorreo"></label>
+                                    <div id="grupo-correo" class="input-group mb-3">
+                                        <input type="text" name="correo" required class="form-control" id="correo" onChange="validarExistencia(this,'<?= CORREO ?>','vCorreo','grupo-correo')" placeholder="Ingrese correo Electronico" value="<?= $correo; ?>" <?= $readonly; ?>>
+                                    </div>
                                 </div>
                                 <!-- Agrega más campos según tus necesidades -->
 
@@ -202,38 +262,95 @@ $datosContactoSintia = Plataforma::infoContactoSintia();
     <link href="../../config-general/assets/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
     <link href="../../config-general/assets/plugins/bootstrap-colorpicker/css/bootstrap-colorpicker.css" rel="stylesheet" media="screen">
     <script type="application/javascript">
-        function nuevoEstudiante(enviada) {
-            var nDoct = enviada.value;
-            $('#nDocu').empty().hide().html("Validando documento...").show(1);
+        function validarExistencia(enviada, tipo, span, grupo) {
+            var valor = enviada.value;
+            if (valor.trim() != '') {
+                var spanMensage = $('#' + span);
+                // se inicia el mensaje validando en azul
+                spanMensage.css("color", "blue");
+                spanMensage.empty().hide().html("Validando...").show(1);
 
 
+
+                if (tipo === '<?php echo CORREO ?>') {
+                    var regexCorreo = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                    if (!regexCorreo.test(valor)) {
+                        var response = new Array("ok", 2, 3, 4, 5);
+                        response["ok"] = "false";
+                        validar(response, spanMensage, grupo, "El correo no es correcto");
+                    } else {
+                        enviarPeticion(tipo, valor, spanMensage, grupo, tipo + " ya existe en el sitema");
+                    }
+                } else {
+                    enviarPeticion(tipo, valor, spanMensage, grupo, tipo + " ya existe en el sitema");
+                }
+
+            } else {
+                idGrupo = "sub-" + grupo;
+                subDiv = document.getElementById(idGrupo);
+                if (subDiv) {
+                    subDiv.remove();
+                }
+            }
+
+
+        }
+
+        function enviarPeticion(tipo, valor, spanMensage, grupo, msgError) {
             var url = "ajax-details-validar.php";
             var data = {
-                "nDoct": (nDoct),
-                "tipo": "Identificacion"
+                "tipo": tipo,
+                "valor": (valor),
+                "year": <?php echo $resultado["year"] ?>,
+                "institucion": <?php echo $resultado["institucion"] ?>
             };
             fetch(url, {
                     method: "POST", // or 'PUT'
                     body: JSON.stringify(data), // data can be `string` or {object}!
                     headers: {
-                        "Content-Type": "application/json",
+                        "Content-Type": "application/json"
                     },
                 })
                 .then((res) => res.json())
                 .catch((error) => console.error("Error:", error))
                 .then(
                     function(response) {
-                        $('#nDocu').empty().hide().show(1);
-                        if (response.ok) {
-                            console.log("Respuesta de red OK");
-                        } else {
-                            console.log("Respuesta de red OK pero respuesta HTTP no OK");
-                        }
-                    })
-                .catch(function(error) {
-                    console.log("Hubo un problema con la petición Fetch:" + error.message);
-                });
+                        spanMensage.empty().hide().show(1);
+                        validar(response, spanMensage, grupo, msgError);
 
+                    });
+        }
+
+        function validar(response, spanMensage, grupo, msgError) {
+            idGrupo = "sub-" + grupo;
+            subDiv = document.getElementById(idGrupo);
+            if (subDiv) {
+                subDiv.remove();
+            }
+            grupo = document.getElementById(grupo); // identificamos el grupo a editar
+            
+            const nuevoDiv = document.createElement('div'); // se crea el div del grupo
+            nuevoDiv.classList.add('input-group-append');
+            nuevoDiv.id = idGrupo;
+
+            const nuevoBoton = document.createElement('button'); // se crea un  boton para mostrar la validacion
+            nuevoBoton.classList.add('btn', 'btn-outline-secondary');
+
+            const nuevaImg = document.createElement('img'); // se crea la imagen
+
+            if (response.ok) {
+                nuevaImg.src = "../files/iconos/1363803022_001_05.png";
+                nuevoBoton.appendChild(nuevaImg); // sea agrega la imagen al boton
+                nuevoDiv.name = "formIvalido";
+                nuevoDiv.value= "false";
+                spanMensage.css("color", "red");
+                spanMensage.empty().hide().html(msgError + "...").show(1);
+            } else {
+                nuevaImg.src = "../files/iconos/check1.png";
+                nuevoBoton.appendChild(nuevaImg); // sea agrega la imagen al boton
+            }
+            nuevoDiv.appendChild(nuevoBoton); // se agrega el boton al div
+            grupo.appendChild(nuevoDiv); // se añade el boton al div
         }
     </script>
     <!-- start js include path -->
