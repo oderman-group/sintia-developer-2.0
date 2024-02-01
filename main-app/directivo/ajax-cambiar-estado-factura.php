04c833fc885cdf6a4a5c77e369b0ef4be1d184a2
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-02-01 01:11:42 -0500

    PES-711 - Mejora para el cambio de estado

--- main-app/directivo/ajax-cambiar-estado-factura.php
+++ main-app/directivo/ajax-cambiar-estado-factura.php
@@ -5,12 +5,19 @@ Modulos::validarAccesoDirectoPaginas();
 $idPaginaInterna = 'DT0301';
 require_once(ROOT_PATH."/main-app/compartido/historial-acciones-guardar.php");
 
+    $estado = $_REQUEST['estado'] == 1 ? COBRADA : POR_COBRAR;
+
     try {
-        mysqli_query($conexion, "UPDATE ".BD_FINANCIERA.".finanzas_cuentas SET fcu_status='COBRADA' WHERE fcu_id='".$_REQUEST['idFactura']."' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
+        mysqli_query($conexion, "UPDATE ".BD_FINANCIERA.".finanzas_cuentas SET fcu_status='".$estado."' WHERE fcu_id='".$_REQUEST['idFactura']."' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
     } catch(Exception $e) {
         echo $e->getMessage();
         exit();
     }
+
+    $arrayEstado=["estado"=>$estado];
+    
+    header('Content-Type: application/json');
+    echo json_encode($arrayEstado);
 
 require_once(ROOT_PATH."/main-app/compartido/guardar-historial-acciones.php");
 exit;
