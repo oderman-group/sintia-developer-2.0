5d2f79b4482a52cf216bc3be573c56ecc92d5f0e
Luis Guillermo <softcomputo77@gmail.com>
2024-02-02 20:15:17 -0500

    PES-625 -Correcion de guardado al matricularse

--- main-app/directivo/cursos-agregar-modal.php
+++ main-app/directivo/cursos-agregar-modal.php
@@ -25,6 +25,80 @@ if (!Modulos::validarPermisoEdicion()) {
                 <div class="col-sm-10">
                     <input type="text" name="nombreC" class="form-control" required <?= $disabledPermiso; ?>>
                 </div>
+
+                <div class="tab-pane fade" id="nav-configuracion" role="tabpanel" aria-labelledby="nav-configuracion-tab">
+                    <div class="panel">
+                        <div class="panel-body">
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">URL imagen</label>
+                                <div class="col-sm-10">
+                                    <input type="text" name="imagen" class="form-control"  <?= $disabledPermiso; ?>>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">Descripcion</label>
+                                <div class="col-sm-10">
+                                <textarea cols="80" id="editor1" name="descripcion" class="form-control" rows="8" placeholder="Escribe tu mensaje" style="margin-top: 0px; margin-bottom: 0px; height: 100px; resize: none;" <?= $disabledPermiso; ?>></textarea>
+                                    
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">Contenido</label>
+                                <div class="col-sm-10">
+                                <textarea cols="80" id="editor2" name="contenido" class="form-control" rows="8" placeholder="Escribe tu mensaje" style="margin-top: 0px; margin-bottom: 0px; height: 100px; resize: none;" <?= $disabledPermiso; ?>></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">Precio</label>
+                                <div class="col-sm-10">
+                                    <input type="number"  name="precio"  class="form-control" value="0"  <?= $disabledPermiso; ?>>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">Minimo de estudiantes</label>
+                                <div class="col-sm-10">
+                                    <input type="number" name="minEstudiantes" class="form-control" value="1"  min="1" <?= $disabledPermiso; ?>>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">Maximo de estudiantes</label>
+                                <div class="col-sm-10">
+                                    <input type="number" name="maxEstudiantes" disabled class="form-control" value="1" min="1"<?= $disabledPermiso; ?>>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">Duracion en horas</label>
+                                <div class="col-sm-10">
+                                    <input type="number" id="horas" name="horas" class="form-control" value="0"   <?= $disabledPermiso; ?>>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">Auto Enrollment</label>
+                                <div class="col-sm-10">
+                                    <label class="switchToggle">
+                                        <input name="autoenrollment" type="checkbox">
+                                        <span class="slider green round"></span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-2 control-label">Activo</label>
+                                <div class="col-sm-10">
+                                    <label class="switchToggle">
+                                        <input name="activo" type="checkbox">
+                                        <span class="slider green round"></span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <?php if (Modulos::validarPermisoEdicion()) { ?>
+                    <button type="submit" class="btn  btn-info">
+                        <i class="fa fa-save" aria-hidden="true"></i> Guardar cambios
+                    </button>
+                <?php } ?>
+
             </div>
 
             <?php
