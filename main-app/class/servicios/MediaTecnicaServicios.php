82e7d13a909a57c41a1c4bb9a7b704826c2d9060
Luis Guillermo <softcomputo77@gmail.com>
2024-02-08 10:04:49 -0500

    PES-625 -crear metodo para contar matriculas y cursos.

--- main-app/class/servicios/MediaTecnicaServicios.php
+++ main-app/class/servicios/MediaTecnicaServicios.php
@@ -23,6 +23,26 @@ class MediaTecnicaServicios extends Servicios
     }
 
     /**
+     * Realiza un conteo teniendo encuenta los parametros ingresados.
+     *
+     * @param array|null $parametrosArray Arreglo de parámetros para filtrar la consulta (opcional).
+     *
+     * @return array|mysqli_result|false Arreglo de datos del resultado, objeto mysqli_result o false si hay un error.
+     */
+    public static function contar($parametrosArray=null)
+    {
+      global $baseDatosServicios;
+      $sqlInicial="SELECT Count(matcur_id) as cantidad FROM ".$baseDatosServicios.".mediatecnica_matriculas_cursos";
+      if($parametrosArray && count($parametrosArray)>0){
+        $parametrosValidos=array('matcur_id_curso','matcur_id_matricula','matcur_id_institucion','matcur_years');
+        $sqlInicial=Servicios::concatenarWhereAnd($sqlInicial,$parametrosValidos,$parametrosArray);
+      };
+      $sqlFinal ="";
+      $sql=$sqlInicial.$sqlFinal;
+      return Servicios::SelectSql($sql)[0]["cantidad"];         
+    }
+
+    /**
      * Lista las materias de Media Técnica con información adicional.
      *
      * @param array|null $parametrosArray Arreglo de parámetros para filtrar la consulta (opcional).
