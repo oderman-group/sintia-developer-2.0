a17ab2ce50c694858886b058661c2e2dcd37f8b5
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-02-09 12:58:59 -0500

    PES-738 - Se envia id de la evaluación

--- main-app/directivo/asignaciones-guardar.php
+++ main-app/directivo/asignaciones-guardar.php
@@ -15,5 +15,5 @@ Asignaciones::guardarAsignaciones($conexion, $config, $_POST);
 
 include(ROOT_PATH."/main-app/compartido/guardar-historial-acciones.php");
 
-echo '<script type="text/javascript">window.location.href="asignaciones.php?success=SC_DT_1&idE='.base64_encode($_POST['idE']).'";</script>';
+echo '<script type="text/javascript">window.location.href="asignaciones.php?success=SC_DT_1&idE='.base64_encode($_POST['idE']).'&id='.base64_encode($_POST['idE']).'";</script>';
 exit();
