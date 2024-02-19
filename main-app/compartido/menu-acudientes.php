76565075e3d719d12d9dfd413604dbee54b6028c
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-02-19 12:20:47 -0500

    HTF - Se muestran solo modulos activos

--- main-app/compartido/menu-acudientes.php
+++ main-app/compartido/menu-acudientes.php
@@ -8,16 +8,20 @@
 	                            </a>
 	                        </li>
 							
+							<?php if(Modulos::validarModulosActivos($conexion, 2)){ ?>
 							<li class="nav-item" data-step="12" data-intro="<b><?=$frases[104][$datosUsuarioActual['uss_idioma']];?>:</b> Aquí verás toda la información relacionada con tu estado de cuenta financiero." data-position='left'>
 	                            <a <?php validarModuloMenu(2, "estado-de-cuenta.php", MENU) ?> class="nav-link nav-toggle"> <i class="material-icons">attach_money</i>
 	                                <span class="title"><?=$frases[104][$datosUsuarioActual['uss_idioma']];?></span> 
 	                            </a>
 	                        </li>
+							<?php }?>
 
+							<?php if(Modulos::validarModulosActivos($conexion, 6)){ ?>
 							<li class="nav-item active" data-step="11" data-intro="<b><?=$frases[175][$datosUsuarioActual['uss_idioma']];?>:</b> Encuentra los mejores productos y servicios complementarios." data-position='left'>
 	                            <a <?php validarModuloMenu(6, "marketplace.php", MENU) ?> class="nav-link nav-toggle bg-warning text-dark"> <i class="fa fa-shopping-cart text-dark"></i>
 	                                <span class="title">Marketplace</span> 
 	                            </a>
 	                        </li>
+							<?php }?>
 							
 							<?php }?>
