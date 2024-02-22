0d54fb51ab8f1f158fb039cd0ae5f2f024d692f8
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-02-22 13:05:20 -0500

    PES-789 - Se valida que solo haga abono cuando el monto sea mayor a cero

--- main-app/directivo/movimientos-guardar.php
+++ main-app/directivo/movimientos-guardar.php
@@ -66,23 +66,25 @@ try{
 if (!empty($_POST["abonoAutomatico"]) && $_POST["abonoAutomatico"] == 1) {
     $totalNeto    = Movimientos::calcularTotalNeto($conexion, $config, $idInsercion, $_POST["valor"]);
 
-    $codigoUnico=Utilidades::generateCode("ABO");
-    try {
-        mysqli_query($conexion, "INSERT INTO ".BD_FINANCIERA.".payments (responsible_user, invoiced, cod_payment, type_payments, payment_method, observation, institucion, year)VALUES('{$_SESSION["id"]}', '".$_POST["usuario"]."', '".$codigoUnico."', '".INVOICE."', 'TRANSFERENCIA', 'Abono automatico', {$config['conf_id_institucion']}, {$_SESSION["bd"]});");
-    } catch (Exception $e) {
-        include("../compartido/error-catch-to-report.php");
-    }
+    if ($totalNeto > 0) {
+        $codigoUnico=Utilidades::generateCode("ABO");
+        try {
+            mysqli_query($conexion, "INSERT INTO ".BD_FINANCIERA.".payments (responsible_user, invoiced, cod_payment, type_payments, payment_method, observation, institucion, year)VALUES('{$_SESSION["id"]}', '".$_POST["usuario"]."', '".$codigoUnico."', '".INVOICE."', 'TRANSFERENCIA', 'Abono automatico', {$config['conf_id_institucion']}, {$_SESSION["bd"]});");
+        } catch (Exception $e) {
+            include("../compartido/error-catch-to-report.php");
+        }
 
-    try {
-        mysqli_query($conexion, "INSERT INTO ".BD_FINANCIERA.".payments_invoiced (invoiced, payments, payment, institucion, year)VALUES('".$idInsercion."', '".$codigoUnico."', '".$totalNeto."', {$config['conf_id_institucion']}, {$_SESSION["bd"]});");
-    } catch (Exception $e) {
-        include("../compartido/error-catch-to-report.php");
-    }
+        try {
+            mysqli_query($conexion, "INSERT INTO ".BD_FINANCIERA.".payments_invoiced (invoiced, payments, payment, institucion, year)VALUES('".$idInsercion."', '".$codigoUnico."', '".$totalNeto."', {$config['conf_id_institucion']}, {$_SESSION["bd"]});");
+        } catch (Exception $e) {
+            include("../compartido/error-catch-to-report.php");
+        }
 
-    try{
-        mysqli_query($conexion, "UPDATE ".BD_FINANCIERA.".finanzas_cuentas SET fcu_status='" .COBRADA. "' WHERE fcu_id='" . $idInsercion . "' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
-    } catch (Exception $e) {
-        include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
+        try{
+            mysqli_query($conexion, "UPDATE ".BD_FINANCIERA.".finanzas_cuentas SET fcu_status='" .COBRADA. "' WHERE fcu_id='" . $idInsercion . "' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
+        } catch (Exception $e) {
+            include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
+        }
     }
 }
 
