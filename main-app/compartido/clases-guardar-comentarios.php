735d131a46f2dbd3d5da918ed6b216e13ff5b054
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-02-29 13:56:23 -0500

    PES-718 - Se centralizan consultas para tabla academico_clases_preguntas

--- main-app/compartido/clases-guardar-comentarios.php
+++ main-app/compartido/clases-guardar-comentarios.php
@@ -5,14 +5,11 @@ $idPaginaInterna = 'CM0026';
 include(ROOT_PATH."/main-app/compartido/historial-acciones-guardar.php");
 include(ROOT_PATH."/main-app/compartido/sintia-funciones.php");
 require_once(ROOT_PATH."/main-app/class/Utilidades.php");
+require_once(ROOT_PATH."/main-app/class/Clases.php");
 $codigo=Utilidades::generateCode("CPP");
 $usuariosClase = new Usuarios;
 
-try{
-    mysqli_query($conexion, "INSERT INTO ".BD_ACADEMICA.".academico_clases_preguntas(cpp_id, cpp_usuario, cpp_fecha, cpp_id_clase, cpp_contenido, institucion, year)VALUES('".$codigo."', '" . $_SESSION["id"] . "', now(), '" . $_POST["idClase"] . "', '" . mysqli_real_escape_string($conexion,$_POST["contenido"]) . "', {$config['conf_id_institucion']}, {$_SESSION["bd"]})");
-} catch (Exception $e) {
-	include(ROOT_PATH."/main-app/compartido/error-catch-to-report.php");
-}
+Clases::guardarPreguntasClases($conexion, $config, $_POST);
 
 try{
     $datos = mysqli_fetch_array(mysqli_query($conexion, "SELECT * FROM ".BD_ACADEMICA.".academico_clases cls
