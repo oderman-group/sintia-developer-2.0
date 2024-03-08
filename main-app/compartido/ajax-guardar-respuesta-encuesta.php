ae6e82fd75d349564ca7b6a28df09fe3cff9aaad
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-03-08 06:16:15 -0500

    PES-876 - Se valida limite para la encuesta

--- main-app/compartido/ajax-guardar-respuesta-encuesta.php
+++ main-app/compartido/ajax-guardar-respuesta-encuesta.php
@@ -1,10 +1,21 @@
 <?php
 include("session-compartida.php");
 require_once(ROOT_PATH . "/main-app/class/PreguntaGeneral.php");
+require_once(ROOT_PATH . "/main-app/class/Asignaciones.php");
 
 Modulos::validarAccesoDirectoPaginas();
 $idPaginaInterna = 'CM0061';
 require_once(ROOT_PATH."/main-app/compartido/historial-acciones-guardar.php");
+
+$asignacion = Asignaciones::traerDatosAsignaciones($conexion, $config, $_GET['idAsignacion']);
+
+$iniciadas = Asignaciones::consultarCantAsignacionesEmpezadas($conexion, $config, $asignacion['gal_id']);
+if ($iniciadas >= $asignacion['gal_limite_evaluadores'] ) { 
+	$enlace = UsuariosPadre::verificarTipoUsuario($datosUsuarioActual['uss_tipo'], "encuestas-pendientes.php");
+
+	echo $enlace.'?error=ER_DT_21';
+	exit();
+}
 
 $existeRespuestas = PreguntaGeneral::existeRespuestaPregunta($conexion, $config, $_GET['idPregunta'], $_GET['idAsignacion'], $datosUsuarioActual['uss_id']);
 if(empty($existeRespuestas)) {
