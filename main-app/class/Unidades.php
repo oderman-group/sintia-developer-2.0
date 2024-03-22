94fe6ff5f310ab5c2f56e4b372a98cacdcd75ae3
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-03-22 12:10:33 -0500

    PES-583 - Se sanitiza consultas a academico_unidades

--- main-app/class/Unidades.php
+++ main-app/class/Unidades.php
@@ -7,19 +7,37 @@ class Unidades{
      * @param mysqli $conexion
      * @param array $config
      * @param string $idCarga
-     * @param int $pertiodo
+     * @param int $periodo
      * 
-     * @return mysqli_result $consulta
+     * @return mysqli_result|false $consulta
      */
-    public static function consultarUnidades(mysqli $conexion, array $config, string $idCarga, int $pertiodo){
-        try{
-            $consulta = mysqli_query($conexion, "SELECT * FROM ".BD_ACADEMICA.".academico_unidades 
-            WHERE uni_id_carga='" . $idCarga . "' AND uni_periodo='" . $pertiodo . "' AND uni_eliminado!=1 AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
+    public static function consultarUnidades(mysqli $conexion, array $config, string $idCarga, int $periodo)
+    {
+        try {
+            // Preparar la consulta SQL con marcadores de posición
+            $consulta = mysqli_prepare($conexion, "SELECT * FROM " . BD_ACADEMICA . ".academico_unidades WHERE uni_id_carga=? AND uni_periodo=? AND uni_eliminado!=1 AND institucion=? AND year=?");
+
+            if ($consulta) {
+                // Vincular los valores de las variables a los marcadores de posición en la consulta preparada
+                mysqli_stmt_bind_param($consulta, "siii", $idCarga, $periodo, $config['conf_id_institucion'], $_SESSION["bd"]);
+
+                // Ejecutar la consulta preparada
+                mysqli_stmt_execute($consulta);
+
+                // Obtener el resultado de la consulta
+                $resultado = mysqli_stmt_get_result($consulta);
+
+                return $resultado;
+            } else {
+                // Si la preparación de la consulta falla, mostrar un mensaje de error
+                echo "Error en la preparación de la consulta.";
+                exit();
+            }
         } catch (Exception $e) {
-            echo "Excepción catpurada: ".$e->getMessage();
+            // Manejar la excepción
+            echo "Excepción capturada: " . $e->getMessage();
             exit();
         }
-        return $consulta;
     }
     
     /**
@@ -27,38 +45,75 @@ class Unidades{
      * @param mysqli $conexion
      * @param array $config
      * @param string $idCarga
-     * @param int $pertiodo
+     * @param int $periodo
      * @param int $idR
      * 
-     * @return mysqli_result $consulta
+     * @return mysqli_result|false $consulta
      */
-    public static function consultarUnidadesDiferentes(mysqli $conexion, array $config, string $idCarga, int $pertiodo, int $idR){
-        try{
-            $consulta = mysqli_query($conexion, "SELECT * FROM ".BD_ACADEMICA.".academico_unidades 
-            WHERE uni_id_carga='" . $idCarga . "' AND uni_periodo='" . $pertiodo . "' AND uni_eliminado!=1 AND id_nuevo!='" . $idR . "' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
+    public static function consultarUnidadesDiferentes(mysqli $conexion, array $config, string $idCarga, int $periodo, int $idR)
+    {
+        try {
+            // Preparar la consulta SQL con marcadores de posición
+            $consulta = mysqli_prepare($conexion, "SELECT * FROM " . BD_ACADEMICA . ".academico_unidades WHERE uni_id_carga=? AND uni_periodo=? AND uni_eliminado!=1 AND id_nuevo!=? AND institucion=? AND year=?");
+
+            if ($consulta) {
+                // Vincular los valores de las variables a los marcadores de posición en la consulta preparada
+                mysqli_stmt_bind_param($consulta, "siiii", $idCarga, $periodo, $idR, $config['conf_id_institucion'], $_SESSION["bd"]);
+
+                // Ejecutar la consulta preparada
+                mysqli_stmt_execute($consulta);
+
+                // Obtener el resultado de la consulta
+                $resultado = mysqli_stmt_get_result($consulta);
+
+                return $resultado;
+            } else {
+                // Si la preparación de la consulta falla, mostrar un mensaje de error
+                echo "Error en la preparación de la consulta.";
+                exit();
+            }
         } catch (Exception $e) {
-            echo "Excepción catpurada: ".$e->getMessage();
+            // Manejar la excepción
+            echo "Excepción capturada: " . $e->getMessage();
             exit();
         }
-        return $consulta;
     }
 
     /**
-     * Este metodo me trae los datos de una unidad
+     * Este metodo me trae los datos de una unidad por su ID
      * @param mysqli $conexion
      * @param int $idR
      * 
      * @return array $resultado
      */
-    public static function consultarUnidadesPorID( mysqli $conexion, int $idR){
-        $resultado=[];
-        try{
-            $consulta=mysqli_query($conexion, "SELECT * FROM ".BD_ACADEMICA.".academico_unidades WHERE id_nuevo='".$idR."'");
+    public static function consultarUnidadesPorID(mysqli $conexion, int $idR)
+    {
+        $resultado = [];
+        try {
+            // Preparar la consulta SQL con marcadores de posición
+            $consulta = mysqli_prepare($conexion, "SELECT * FROM " . BD_ACADEMICA . ".academico_unidades WHERE id_nuevo=?");
+
+            if ($consulta) {
+                // Vincular el valor de la variable al marcador de posición en la consulta preparada
+                mysqli_stmt_bind_param($consulta, "i", $idR);
+
+                // Ejecutar la consulta preparada
+                mysqli_stmt_execute($consulta);
+
+                // Obtener el resultado de la consulta
+                $resultadoC = mysqli_stmt_get_result($consulta);
+                $resultado = mysqli_fetch_array($resultadoC, MYSQLI_BOTH);
+            } else {
+                // Si la preparación de la consulta falla, mostrar un mensaje de error
+                echo "Error en la preparación de la consulta.";
+                exit();
+            }
         } catch (Exception $e) {
-            echo "Excepción catpurada: ".$e->getMessage();
+            // Manejar la excepción
+            echo "Excepción capturada: " . $e->getMessage();
             exit();
         }
-        $resultado = mysqli_fetch_array($consulta, MYSQLI_BOTH);
+
         return $resultado;
     }
 
@@ -67,15 +122,30 @@ class Unidades{
      * @param mysqli $conexion
      * @param array $config
      * @param string $idCarga
-     * @param int $pertiodo
+     * @param int $periodo
      * @param array $POST
      */
-    public static function guardarUnidades( mysqli $conexion, array $config, string $idCarga, int $pertiodo, array $POST){
-        $codigo=Utilidades::generateCode("UNI");
-        try{
-            mysqli_query($conexion, "INSERT INTO ".BD_ACADEMICA.".academico_unidades (uni_id, uni_nombre,uni_id_carga,uni_periodo,uni_descripcion, institucion, year)VALUES('".$codigo."', '".$POST["nombre"]."','".$idCarga."','".$pertiodo."','".$POST["contenido"]."', {$config['conf_id_institucion']}, {$_SESSION["bd"]})");
+    public static function guardarUnidades(mysqli $conexion, PDO $conexionPDO, array $config, string $idCarga, int $periodo, array $POST)
+    {
+        $codigo = Utilidades::getNextIdSequence($conexionPDO, BD_ACADEMICA, 'academico_unidades');
+        try {
+            // Preparar la consulta SQL con marcadores de posición
+            $consulta = mysqli_prepare($conexion, "INSERT INTO " . BD_ACADEMICA . ".academico_unidades (uni_id, uni_nombre, uni_id_carga, uni_periodo, uni_descripcion, institucion, year) VALUES (?, ?, ?, ?, ?, ?, ?)");
+
+            if ($consulta) {
+                // Vincular los valores de las variables a los marcadores de posición en la consulta preparada
+                mysqli_stmt_bind_param($consulta, "sssissi", $codigo, $POST["nombre"], $idCarga, $periodo, $POST["contenido"], $config['conf_id_institucion'], $_SESSION["bd"]);
+
+                // Ejecutar la consulta preparada
+                mysqli_stmt_execute($consulta);
+            } else {
+                // Si la preparación de la consulta falla, mostrar un mensaje de error
+                echo "Error en la preparación de la consulta.";
+                exit();
+            }
         } catch (Exception $e) {
-            echo "Excepción catpurada: ".$e->getMessage();
+            // Manejar la excepción
+            echo "Excepción capturada: " . $e->getMessage();
             exit();
         }
     }
@@ -88,11 +158,26 @@ class Unidades{
      * @param int $pertiodo
      * @param array $POST
      */
-    public static function actualizarUnidades( mysqli $conexion, array $config, string $idCarga, int $pertiodo, array $POST){
-        try{
-            mysqli_query($conexion, "UPDATE ".BD_ACADEMICA.".academico_unidades SET uni_nombre='".$POST["nombre"]."', uni_id_carga='".$idCarga."', uni_periodo='".$pertiodo."', uni_descripcion='".$POST["contenido"]."' WHERE id_nuevo='".$POST["idR"]."' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
+    public static function actualizarUnidades(mysqli $conexion, array $config, string $idCarga, int $pertiodo, array $POST)
+    {
+        try {
+            // Preparar la consulta SQL con marcadores de posición
+            $consulta = mysqli_prepare($conexion, "UPDATE " . BD_ACADEMICA . ".academico_unidades SET uni_nombre=?, uni_id_carga=?, uni_periodo=?, uni_descripcion=? WHERE id_nuevo=? AND institucion=? AND year=?");
+
+            if ($consulta) {
+                // Vincular los valores de las variables a los marcadores de posición en la consulta preparada
+                mysqli_stmt_bind_param($consulta, "ssissii", $POST["nombre"], $idCarga, $pertiodo, $POST["contenido"], $POST["idR"], $config['conf_id_institucion'], $_SESSION["bd"]);
+
+                // Ejecutar la consulta preparada
+                mysqli_stmt_execute($consulta);
+            } else {
+                // Si la preparación de la consulta falla, mostrar un mensaje de error
+                echo "Error en la preparación de la consulta.";
+                exit();
+            }
         } catch (Exception $e) {
-            echo "Excepción catpurada: ".$e->getMessage();
+            // Manejar la excepción
+            echo "Excepción capturada: " . $e->getMessage();
             exit();
         }
     }
@@ -103,11 +188,26 @@ class Unidades{
      * @param array $config
      * @param array $GET
      */
-    public static function eliminarUnidades( mysqli $conexion, array $config, array $GET){
-        try{
-            mysqli_query($conexion, "UPDATE ".BD_ACADEMICA.".academico_unidades SET uni_eliminado=1 WHERE id_nuevo='".base64_decode($GET["idR"])."' AND institucion={$config['conf_id_institucion']} AND year={$_SESSION["bd"]}");
+    public static function eliminarUnidades(mysqli $conexion, array $config, array $GET)
+    {
+        try {
+            // Preparar la consulta SQL con marcadores de posición
+            $consulta = mysqli_prepare($conexion, "UPDATE " . BD_ACADEMICA . ".academico_unidades SET uni_eliminado=1 WHERE id_nuevo=? AND institucion=? AND year=?");
+
+            if ($consulta) {
+                // Vincular los valores de las variables a los marcadores de posición en la consulta preparada
+                mysqli_stmt_bind_param($consulta, "sii", base64_decode($GET["idR"]), $config['conf_id_institucion'], $_SESSION["bd"]);
+
+                // Ejecutar la consulta preparada
+                mysqli_stmt_execute($consulta);
+            } else {
+                // Si la preparación de la consulta falla, mostrar un mensaje de error
+                echo "Error en la preparación de la consulta.";
+                exit();
+            }
         } catch (Exception $e) {
-            echo "Excepción catpurada: ".$e->getMessage();
+            // Manejar la excepción
+            echo "Excepción capturada: " . $e->getMessage();
             exit();
         }
     }
