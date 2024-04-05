69ec43facd237984063e444721a61499398edf15
Luis Guillermo <softcomputo77@gmail.com>
2024-04-05 14:26:08 -0500

    feat: :white_check_mark: PES-1015 Cambio de proyecto firebase

--- config-general/config.php
+++ config-general/config.php
@@ -62,5 +62,5 @@ require_once(ROOT_PATH . "/librerias/Firebase/vendor/autoload.php");
 use Kreait\Firebase\Factory;
 $factory = (new Factory)
 	->withServiceAccount(ROOT_PATH . '/librerias/Firebase/key/firebase_credentials.json')
-	->withDatabaseUri('https://sintia-firebase-default-rtdb.firebaseio.com');
+	->withDatabaseUri('https://sintia-app-default-rtdb.firebaseio.com');
 $storage = $factory->createStorage();
