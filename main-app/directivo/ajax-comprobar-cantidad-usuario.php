7acc0318f8509f5bbb54fd544c98fe0c8b4e5f0f
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-05-15 08:23:27 -0500

    HTF - se trae cantidad de usuarios dinamicamente

--- main-app/directivo/ajax-comprobar-cantidad-usuario.php
+++ main-app/directivo/ajax-comprobar-cantidad-usuario.php
@@ -1,64 +1,53 @@
 <?php
 include('session.php');
+require_once(ROOT_PATH."/main-app/class/Plataforma.php");
 require_once(ROOT_PATH."/main-app/class/UsuariosPadre.php");
 if($_REQUEST['tipoUsuario']!=""){
-$tipoUsuario = $_REQUEST['tipoUsuario'];
-/** Cambiar estos datos por consulta a planes y paquetes para ver cantidad de usuarios permitidos**/
-switch ($datosUnicosInstitucion['ins_id_plan']) {
-    case 1:
-        $cantDirectivos = 10;
-        $cantDocentes = 20;
-        $cantEstudianteAcudientes = 500;
-    break;
-    case 2:
-        $cantDirectivos = 50;
-        $cantDocentes = 100;
-        $cantEstudianteAcudientes = 1000;
-    break;
-    case 3:
-        $cantEstudianteAcudientes = 2000;
-    break;
-}
-/**HASTA AQUI**/
+    $tipoUsuario = $_REQUEST['tipoUsuario'];
 
-$jsonData = array();
-$totalCliente = UsuariosPadre::contarUsuariosPorTipo($conexion, $tipoUsuario);
+    $datosPlan = Plataforma::traerDatosPlanes($conexion, $datosUnicosInstitucion['ins_id_plan']);
+    $cantDirectivos = $datosPlan['plns_cant_directivos'];
+    $cantDocentes = $datosPlan['plns_cant_docentes'];
+    $cantEstudianteAcudientes = $datosPlan['plns_cant_estudiantes'];
 
-$limite = 0;
-switch ($tipoUsuario) {
-    case TIPO_DIRECTIVO:
-        if ($datosUnicosInstitucion['ins_id_plan'] != 3 && $totalCliente >= $cantDirectivos) {
-            $limite = 1;
-        }
-    break;
-    case TIPO_DOCENTE:
-        if ($datosUnicosInstitucion['ins_id_plan'] != 3 && $totalCliente >= $cantDocentes) {
-            $limite = 1;
-        }
-    break;
-    case TIPO_ACUDIENTE:
-        if ($totalCliente >= $cantEstudianteAcudientes) {
-            $limite = 1;
-        }
-    break;
-    case TIPO_ESTUDIANTE:
-        if ($totalCliente >= $cantEstudianteAcudientes) {
-            $limite = 1;
-        }
-    break;
-}
+    $jsonData = array();
+    $totalCliente = UsuariosPadre::contarUsuariosPorTipo($conexion, $tipoUsuario);
 
-if( $limite == 0 ){
-    $jsonData['success'] = 0;
-    $jsonData['message'] = '';
-} else{
-    
-    $jsonData['success'] = 1;
-    $jsonData['message'] = '<div class="alert alert-danger">
-                            <button type="button" class="close" data-dismiss="alert">&times;</button>
-                            <i class="icon-exclamation-sign"></i>Lo sentimos no puedes crear este usuario,<br> haz llegado al limete de usuarios de este tipo,<br> puedes adquirir un paquete de usuarios para continiar.</div>';
-}
+    $limite = 0;
+    switch ($tipoUsuario) {
+        case TIPO_DIRECTIVO:
+            if ($datosUnicosInstitucion['ins_id_plan'] != 3 && $totalCliente >= $cantDirectivos) {
+                $limite = 1;
+            }
+        break;
+        case TIPO_DOCENTE:
+            if ($datosUnicosInstitucion['ins_id_plan'] != 3 && $totalCliente >= $cantDocentes) {
+                $limite = 1;
+            }
+        break;
+        case TIPO_ACUDIENTE:
+            if ($totalCliente >= $cantEstudianteAcudientes) {
+                $limite = 1;
+            }
+        break;
+        case TIPO_ESTUDIANTE:
+            if ($totalCliente >= $cantEstudianteAcudientes) {
+                $limite = 1;
+            }
+        break;
+    }
 
-header('Content-type: application/json; charset=utf-8');
-echo json_encode( $jsonData );
+    if( $limite == 0 ){
+        $jsonData['success'] = 0;
+        $jsonData['message'] = '';
+    } else{
+        
+        $jsonData['success'] = 1;
+        $jsonData['message'] = '<div class="alert alert-danger">
+                                <button type="button" class="close" data-dismiss="alert">&times;</button>
+                                <i class="icon-exclamation-sign"></i>Lo sentimos no puedes crear este usuario,<br> haz llegado al limete de usuarios de este tipo,<br> puedes adquirir un paquete de usuarios para continiar.</div>';
+    }
+
+    header('Content-type: application/json; charset=utf-8');
+    echo json_encode( $jsonData );
 }
