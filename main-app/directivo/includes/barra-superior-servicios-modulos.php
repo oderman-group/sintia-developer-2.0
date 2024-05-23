32f5925020df77d27984824e1659f7a62b13a6e5
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-05-23 17:26:52 -0500

    PES-905  - Se cambia titulo botón

--- main-app/directivo/includes/barra-superior-servicios-modulos.php
+++ main-app/directivo/includes/barra-superior-servicios-modulos.php
@@ -19,7 +19,7 @@ if (!empty($_GET["busqueda"])) {
 
         <form class="form-inline my-2 my-lg-0" action="<?= $_SERVER['PHP_SELF']; ?>" method="get">
             <?php if (!empty($filtro)) { ?>
-                <a class="btn bg-orange my-2 my-sm-0 mr-2" href="<?= $_SERVER['PHP_SELF']; ?>">Quitar filtros</a>
+                <a class="btn bg-orange my-2 my-sm-0 mr-2" href="<?= $_SERVER['PHP_SELF']; ?>">Borrar Busqueda</a>
             <?php } ?>
             <input class="form-control mr-sm-2" type="search" placeholder="Búscar Módulo..." aria-label="Search" name="busqueda" value="<?= $busqueda; ?>">
             <button class="btn deepPink-bgcolor my-2 my-sm-0" type="submit"><?=$frases[8][$datosUsuarioActual['uss_idioma']];?></button>
