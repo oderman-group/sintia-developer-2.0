1c4cdf16cead4c6823795ad75fc2ee3ea4da759d
El gran Sherman <jhooderman@gmail.com>
2024-07-29 18:07:50 -0500

    HF - Solo llamar al acudiente cuando sea necesario

--- main-app/compartido/matricula-contenido.php
+++ main-app/compartido/matricula-contenido.php
@@ -1,7 +1,10 @@
 						
 <?php
 require_once(ROOT_PATH."/main-app/class/Grados.php");
-$acudiente = UsuariosPadre::sesionUsuario($datosEstudianteActual["mat_acudiente"]);
+$acudiente = [];
+if (!empty($datosEstudianteActual["mat_acudiente"])){
+	$acudiente = UsuariosPadre::sesionUsuario($datosEstudianteActual["mat_acudiente"]);
+}
 
 $classDiv="col-sm-12";
 if($config['conf_mostrar_pasos_matricula'] == 1){
@@ -148,11 +151,11 @@ if($config['conf_mostrar_pasos_matricula'] == 1){
 						                    </div>
 						                </div>
 						            -->
-
+									<?php if(!empty($acudiente)) {?>
                                         <h3>Datos Acudiente</h3>
                                         <p style="color: navy;"><?= $frases[325][$datosUsuarioActual['uss_idioma']]; ?></p>
 
-                                        <input type="hidden" name="idAcudiente" value="<?= $acudiente["uss_id"]; ?>">
+                                        <input type="hidden" name="idAcudiente" value="<?php if(!empty($acudiente["uss_id"])) echo $acudiente["uss_id"]; ?>">
 
                                         <div class="form-group row">
 						                    <label class="col-sm-2 control-label">ID</label>
@@ -195,6 +198,7 @@ if($config['conf_mostrar_pasos_matricula'] == 1){
 						                        <input type="text" value="<?= $acudiente["uss_ocupacion"]; ?>" name="ocupacion" class="form-control">
 						                    </div>
 						                </div>
+									<?php }?>
 
 
 
