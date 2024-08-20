d465bb4edeed5f7ec5441ff66f7bd8887e5023cf
Luis Guillermo <softcomputo77@gmail.com>
2024-08-20 18:06:09 -0500

    :rocket: PES-1046  Implmentar metodo en visatas part 2

--- main-app/directivo/cargas-editar.php
+++ main-app/directivo/cargas-editar.php
@@ -7,7 +7,7 @@ require_once(ROOT_PATH."/main-app/class/Grupos.php");
 require_once(ROOT_PATH."/main-app/class/Grados.php");
 require_once(ROOT_PATH."/main-app/class/Asignaturas.php");
 require_once(ROOT_PATH."/main-app/class/CargaAcademica.php");
-
+Utilidades::validarParametros($_GET);
 if(!Modulos::validarSubRol([$idPaginaInterna])){
 	echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
 	exit();
