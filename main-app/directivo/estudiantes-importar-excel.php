cf543bc89a284efb5aecda3c2402754c73303178
Luis Guillermo <softcomputo77@gmail.com>
2024-08-20 18:06:24 -0500

    :rocket: PES-1046  Implmentar metodo en visatas part 3

--- main-app/directivo/estudiantes-importar-excel.php
+++ main-app/directivo/estudiantes-importar-excel.php
@@ -3,7 +3,7 @@
 <?php include("../compartido/historial-acciones-guardar.php");?>
 <?php include("../compartido/head.php");?>
 <?php require_once("../class/Sysjobs.php");
-
+Utilidades::validarParametros($_GET);
 if(!Modulos::validarSubRol([$idPaginaInterna])){
 	echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
 	exit();
