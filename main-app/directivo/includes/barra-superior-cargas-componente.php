d693fbd63542906dbbdebcbca6b2e95542a2ae18
Luis Guillermo <softcomputo77@gmail.com>
2024-08-27 18:51:27 -0500

    :rocket: PES-1038 corregir comentarios

--- main-app/directivo/includes/barra-superior-cargas-componente.php
+++ main-app/directivo/includes/barra-superior-cargas-componente.php
@@ -1,61 +1,61 @@
 <?php
-require_once("../class/componentes/barra-superior.php");
+require_once("../class/componentes/componente-filtro.php");
 $grados = Grados::listarGrados(1);
 $count = 0;
 while ($grado = mysqli_fetch_array($grados, MYSQLI_BOTH)) {
     $filtroCurso[$count] = [
-        COMPB_FILTRO_LISTA_ID => $grado['gra_id'],
-        COMPB_FILTRO_LISTA_TEXTO => $grado['gra_nombre'],
-        COMPB_FILTRO_LISTA_URL => $_SERVER['PHP_SELF'] . "?curso=" . base64_encode($grado['gra_id']) 
+        componenteFiltro::COMPB_FILTRO_LISTA_ID => $grado['gra_id'],
+        componenteFiltro::COMPB_FILTRO_LISTA_TEXTO => $grado['gra_nombre'],
+        componenteFiltro::COMPB_FILTRO_LISTA_URL => $_SERVER['PHP_SELF'] . "?curso=" . base64_encode($grado['gra_id']) 
     ];
     $count++;
 }
 $filtroCurso[$count] = [
-    COMPB_FILTRO_LISTA_TEXTO => 'VER TODOS',
-    COMPB_FILTRO_LISTA_URL => $_SERVER['PHP_SELF'] . "?curso="
+    componenteFiltro::COMPB_FILTRO_LISTA_TEXTO => 'VER TODOS',
+    componenteFiltro::COMPB_FILTRO_LISTA_URL => $_SERVER['PHP_SELF'] . "?curso="
 ];
 $filtros[0] = [
-    COMPB_FILTRO_GET => 'curso',
-    COMPB_FILTRO_TEXTO => 'Filtrar por curso',
-    COMPB_FILTRO_LISTA => $filtroCurso,
-    COMPB_FILTRO_TIPO => 'enlace',
+    componenteFiltro::COMPB_FILTRO_GET => 'curso',
+    componenteFiltro::COMPB_FILTRO_TEXTO => 'Filtrar por curso',
+    componenteFiltro::COMPB_FILTRO_LISTA => $filtroCurso,
+    componenteFiltro::COMPB_FILTRO_TIPO => 'enlace',
 ];
 
 for($i=1; $i<=$config['conf_periodos_maximos']; $i++){
     $filtroPeriodo[$i] = [
-        COMPB_FILTRO_LISTA_ID => $i,
-        COMPB_FILTRO_LISTA_TEXTO => 'Periodo '.$i,
-        COMPB_FILTRO_LISTA_URL => $_SERVER['PHP_SELF'] . "?periodo=" . base64_encode($i) 
+        componenteFiltro::COMPB_FILTRO_LISTA_ID => $i,
+        componenteFiltro::COMPB_FILTRO_LISTA_TEXTO => 'Periodo '.$i,
+        componenteFiltro::COMPB_FILTRO_LISTA_URL => $_SERVER['PHP_SELF'] . "?periodo=" . base64_encode($i) 
     ];
 }
 $filtros[1] = [
-    COMPB_FILTRO_GET => 'periodo',
-    COMPB_FILTRO_TEXTO => 'Filtrar por periodo',
-    COMPB_FILTRO_LISTA => $filtroPeriodo,
-    COMPB_FILTRO_TIPO => COMPB_FILTRO_TIPO_CHECK,
+    componenteFiltro::COMPB_FILTRO_GET => 'periodo',
+    componenteFiltro::COMPB_FILTRO_TEXTO => 'Filtrar por periodo',
+    componenteFiltro::COMPB_FILTRO_LISTA => $filtroPeriodo,
+    componenteFiltro::COMPB_FILTRO_TIPO => componenteFiltro::COMPB_FILTRO_TIPO_CHECK,
 ];
 
 $opciones[0] = [
-    COMPB_OPCIONES_TEXTO => 'Más opciones',
-    COMPB_OPCIONES_URL => '',
-    COMPB_OPCIONES_PERMISO => Modulos::validarPermisoEdicion() && Modulos::validarSubRol(['DT0035']),
-    COMPB_OPCIONES_PAGINAS => $paginas = [
+    componenteFiltro::COMPB_OPCIONES_TEXTO => 'Más opciones',
+    componenteFiltro::COMPB_OPCIONES_URL => '',
+    componenteFiltro::COMPB_OPCIONES_PERMISO => Modulos::validarPermisoEdicion() && Modulos::validarSubRol(['DT0035']),
+    componenteFiltro::COMPB_OPCIONES_PAGINAS => $paginas = [
         [
-            COMPB_OPCIONES_PAGINAS_TEXTO => 'Indicadores obligatorios',
-            COMPB_OPCIONES_PAGINAS_URL => 'cargas-indicadores-obligatorios.php'
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_TEXTO => 'Indicadores obligatorios',
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_URL => 'cargas-indicadores-obligatorios.php'
         ],
         [
-            COMPB_OPCIONES_PAGINAS_TEXTO => 'Notas de Comportamiento',
-            COMPB_OPCIONES_PAGINAS_URL => 'cargas-comportamiento-filtros.php'
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_TEXTO => 'Notas de Comportamiento',
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_URL => 'cargas-comportamiento-filtros.php'
         ],
         [
-            COMPB_OPCIONES_PAGINAS_TEXTO => 'Transferir cargas',
-            COMPB_OPCIONES_PAGINAS_URL => 'javascript:void(0);',
-            COMPB_OPCIONES_PAGINAS_TARGET => '#modalTranferirCargas'
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_TEXTO => 'Transferir cargas',
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_URL => 'javascript:void(0);',
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_TARGET => '#modalTranferirCargas'
         ],
         [
-            COMPB_OPCIONES_PAGINAS_TEXTO => 'Estilo de notas',
-            COMPB_OPCIONES_PAGINAS_URL => 'cargas-estilo-notas.php'
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_TEXTO => 'Estilo de notas',
+            componenteFiltro::COMPB_OPCIONES_PAGINAS_URL => 'cargas-estilo-notas.php'
         ]
     ]
 ];
