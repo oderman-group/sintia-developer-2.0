c3d709573cfaa3aed2920348de67d6251f94e84f
El gran Sherman <jhooderman@gmail.com>
2024-09-03 05:30:46 -0500

    PES-1086 - Actualizar asignación de estudiantes a las actividades

--- main-app/docente/actividades-actualizar.php
+++ main-app/docente/actividades-actualizar.php
@@ -6,11 +6,21 @@ include(ROOT_PATH."/main-app/compartido/historial-acciones-guardar.php");
 
 include(ROOT_PATH."/main-app/compartido/sintia-funciones.php");
 require_once(ROOT_PATH."/main-app/class/Actividades.php");
+require_once(ROOT_PATH."/main-app/class/Tables/BDT_academico_asignaciones_estudiantes.php");
+
 include("verificar-carga.php");
 include("verificar-periodos-diferentes.php");
 
 Actividades::actualizarActividad($conexion, $config, $_POST, $_FILES, $storage);
 
+try {
+    $estudiantes = isset($_POST["estudiantes"]) ? $_POST["estudiantes"] : [];
+    Actividades::actualizarAsignacionActividad($_POST["idR"], $estudiantes);
+} catch (Exception $e) {
+    include(ROOT_PATH . "/main-app/compartido/error-catch-to-report.php");
+    exit();
+}
+
 include(ROOT_PATH."/main-app/compartido/guardar-historial-acciones.php");
-echo '<script type="text/javascript">window.location.href="actividades.php?success=SC_DT_2&id='.base64_encode($_POST["idR"]).'";</script>';
+echo '<script type="text/javascript">window.location.href="actividades-editar.php?success=SC_DT_2&idR='.base64_encode($_POST["idR"]).'";</script>';
 exit();
