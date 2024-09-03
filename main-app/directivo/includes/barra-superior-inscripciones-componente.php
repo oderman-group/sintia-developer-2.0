0069ded9e8299c0af3665bd9cd61df0788712261
Luis Guillermo <softcomputo77@gmail.com>
2024-09-03 14:52:57 -0500

    🚀 PES-1038 implementar cargarPopover dentro del componente

--- main-app/directivo/includes/barra-superior-inscripciones-componente.php
+++ main-app/directivo/includes/barra-superior-inscripciones-componente.php
@@ -9,50 +9,50 @@ if (!empty($_GET['estado'])) {
   $estado = base64_decode($_GET['estado']);
   $filtro .= " AND asp_estado_solicitud='".$estado."'";
 }
-require_once("../class/componentes/componenteFiltro.php");
+require_once("../class/componentes/ComponenteFiltro.php");
 $grados = Grados::listarGrados(1);
 $count=0;
 while ($grado = mysqli_fetch_array($grados, MYSQLI_BOTH)) {
   $filtroCurso[$count] = [
-    componenteFiltro::COMPB_FILTRO_LISTA_ID    => $grado['gra_id'],
-    componenteFiltro::COMPB_FILTRO_LISTA_TEXTO => $grado['gra_nombre'],
-    componenteFiltro::COMPB_FILTRO_LISTA_URL   => $_SERVER['PHP_SELF']."?estado=".base64_encode($estado)."&curso=".base64_encode($grado['gra_id'])
+    ComponenteFiltro::COMPB_FILTRO_LISTA_ID    => $grado['gra_id'],
+    ComponenteFiltro::COMPB_FILTRO_LISTA_TEXTO => $grado['gra_nombre'],
+    ComponenteFiltro::COMPB_FILTRO_LISTA_URL   => $_SERVER['PHP_SELF']."?estado=".base64_encode($estado)."&curso=".base64_encode($grado['gra_id'])
   ];
   $count++;
 }
 $filtroCurso[$count] = [
-  componenteFiltro::COMPB_FILTRO_LISTA_TEXTO => 'VER TODOS',
-  componenteFiltro::COMPB_FILTRO_LISTA_URL   => $_SERVER['PHP_SELF']."?estado=".base64_encode($estado)."&curso=",
-  componenteFiltro::COMPB_FILTRO_LISTA_STYLE => 'font-weight: bold; text-align: center;'
+  ComponenteFiltro::COMPB_FILTRO_LISTA_TEXTO => 'VER TODOS',
+  ComponenteFiltro::COMPB_FILTRO_LISTA_URL   => $_SERVER['PHP_SELF']."?estado=".base64_encode($estado)."&curso=",
+  ComponenteFiltro::COMPB_FILTRO_LISTA_STYLE => 'font-weight: bold; text-align: center;'
 ];
 $count=0;
 foreach($ordenReal as $clave) {
   $filtroEstado[$count] = [
-    componenteFiltro::COMPB_FILTRO_LISTA_ID    => $clave,
-    componenteFiltro::COMPB_FILTRO_LISTA_TEXTO => $estadosSolicitud[$clave],
-    componenteFiltro::COMPB_FILTRO_LISTA_URL   => $_SERVER['PHP_SELF']."?estado=".base64_encode($clave)."&curso=".base64_encode($curso)
+    ComponenteFiltro::COMPB_FILTRO_LISTA_ID    => $clave,
+    ComponenteFiltro::COMPB_FILTRO_LISTA_TEXTO => $estadosSolicitud[$clave],
+    ComponenteFiltro::COMPB_FILTRO_LISTA_URL   => $_SERVER['PHP_SELF']."?estado=".base64_encode($clave)."&curso=".base64_encode($curso)
   ];
   $count++;
 }
 $filtroEstado[$count] = [
-  componenteFiltro::COMPB_FILTRO_LISTA_TEXTO => 'VER TODOS',
-  componenteFiltro::COMPB_FILTRO_LISTA_URL   => $_SERVER['PHP_SELF']."?estado=&curso=".base64_encode($curso),
-  componenteFiltro::COMPB_FILTRO_LISTA_STYLE => 'font-weight: bold; text-align: center;'
+  ComponenteFiltro::COMPB_FILTRO_LISTA_TEXTO => 'VER TODOS',
+  ComponenteFiltro::COMPB_FILTRO_LISTA_URL   => $_SERVER['PHP_SELF']."?estado=&curso=".base64_encode($curso),
+  ComponenteFiltro::COMPB_FILTRO_LISTA_STYLE => 'font-weight: bold; text-align: center;'
 ];
 
 $filtros[0] = [
-  componenteFiltro::COMPB_FILTRO_GET   => 'curso',
-  componenteFiltro::COMPB_FILTRO_TEXTO => 'Filtrar por curso',
-  componenteFiltro::COMPB_FILTRO_LISTA => $filtroCurso,
+  ComponenteFiltro::COMPB_FILTRO_GET   => 'curso',
+  ComponenteFiltro::COMPB_FILTRO_TEXTO => 'Filtrar por curso',
+  ComponenteFiltro::COMPB_FILTRO_LISTA => $filtroCurso,
 ];
 $filtros[1] = [
-  componenteFiltro::COMPB_FILTRO_GET   => 'estado',
-  componenteFiltro::COMPB_FILTRO_TEXTO => 'Filtrar por estado',
-  componenteFiltro::COMPB_FILTRO_LISTA => $filtroEstado,
+  ComponenteFiltro::COMPB_FILTRO_GET   => 'estado',
+  ComponenteFiltro::COMPB_FILTRO_TEXTO => 'Filtrar por estado',
+  ComponenteFiltro::COMPB_FILTRO_LISTA => $filtroEstado,
 ];
 
 
-$barraSuperior = new componenteFiltro('inscripciones', 'filter-inscripciones.php', 'inscripciones-tbody.php',$filtros,null,'crearDatos');
+$barraSuperior = new ComponenteFiltro('inscripciones', 'filter-inscripciones.php', 'inscripciones-tbody.php',$filtros,null,'crearDatos');
 $barraSuperior->generarComponente();
 ?>
 
