b8b851c82c1db2b21342cfc11dcb9efa5cbe0f2a
El gran Sherman <jhooderman@gmail.com>
2024-09-03 05:31:20 -0500

    PES-1086 - Agregar asignación de estudiantes a las actividades

--- main-app/docente/actividades-guardar.php
+++ main-app/docente/actividades-guardar.php
@@ -9,8 +9,17 @@ require_once(ROOT_PATH."/main-app/class/Actividades.php");
 include("verificar-carga.php");
 include("verificar-periodos-diferentes.php");
 require_once(ROOT_PATH."/main-app/class/Utilidades.php");
+require_once(ROOT_PATH."/main-app/class/Tables/BDT_academico_asignaciones_estudiantes.php");
 
 $codigo = Actividades::guardarActividad($conexion, $config, $_POST, $_FILES, $storage, $cargaConsultaActual, $periodoConsultaActual);
+
+try {
+    $estudiantes = isset($_POST["estudiantes"]) ? $_POST["estudiantes"] : [];
+    Actividades::guardarAsignacionActividad($codigo, $estudiantes);
+} catch (Exception $e) {
+    include(ROOT_PATH . "/main-app/compartido/error-catch-to-report.php");
+    exit();
+}
 
 include(ROOT_PATH."/main-app/compartido/guardar-historial-acciones.php");
 
