c266103bbda872a7821d86b2d9c69aae9e7d5a7a
Luis Guillermo <softcomputo77@gmail.com>
2024-09-03 12:17:59 -0500

    🚀 PES-1065 Utilizar nuevas consulta para buscar el usuario o los usuarios

--- main-app/recuperar-clave-guardar.php
+++ main-app/recuperar-clave-guardar.php
@@ -1,58 +1,58 @@
 <?php
-require_once($_SERVER['DOCUMENT_ROOT']."/app-sintia/config-general/constantes.php");
+require_once($_SERVER['DOCUMENT_ROOT'] . "/app-sintia/config-general/constantes.php");
 //include(ROOT_PATH."/conexion-datos.php");
 
-require_once(ROOT_PATH."/main-app/class/Usuarios.php");
-require_once(ROOT_PATH."/main-app/class/EnviarEmail.php");
+require_once(ROOT_PATH . "/main-app/class/Usuarios.php");
+require_once(ROOT_PATH . "/main-app/class/EnviarEmail.php");
 
-$conexionBaseDatosServicios = mysqli_connect($servidorConexion, $usuarioConexion, $claveConexion, $baseDatosServicios);
-$institucionConsulta = mysqli_query($conexionBaseDatosServicios, "SELECT * FROM ".$baseDatosServicios.".instituciones 
-WHERE ins_id='".$_POST["bd"]."'");
+$conexion = mysqli_connect($servidorConexion, $usuarioConexion, $claveConexion, $baseDatosServicios);
 
-$institucion = mysqli_fetch_array($institucionConsulta, MYSQLI_BOTH);
-
-$yearArray = explode(",", $institucion['ins_years']);
-$yearStart = $yearArray[0];
-$yearEnd = $yearArray[1];
-
-$_SESSION["inst"] = $institucion['ins_bd'];
-$_SESSION["idInstitucion"] = $institucion['ins_id'];
-
-if(isset($yearEnd) and is_numeric($yearEnd)){
-	$_SESSION["bd"] = $yearEnd;
-}else{
-	$_SESSION["bd"] = date("Y");
+if(!empty($_POST["Usuario"])){
+	$datosUsuario = Usuarios::datosUsuarioRecuperarClave($_POST["Usuario"]);
+}
+if(!empty($_POST["usuarioId"])){
+	$datosUsuario = Usuarios::datosUsuarioRecuperarClaveId($_POST["usuarioId"]);
 }
 
-include("modelo/conexion.php");
+$contar = count($datosUsuario);
 
-$variable = 1;
+if ($contar == 1) {
+	$datosUsuario = $datosUsuario[0];
+	if (!empty($datosUsuario)) {
 
-$datosUsuario = Usuarios::datosUsuarioParaRecuperarClave($institucion['ins_id'],$_POST["Usuario"]);
+		$data = [
+			'institucion_id'   => $datosUsuario['institucion'],
+			'institucion_agno' => $_SESSION["bd"],
+			'usuario_id'       => $datosUsuario['uss_id'],
+			'usuario_email'    => $datosUsuario['uss_email'],
+			'usuario_nombre'   => $datosUsuario['uss_nombre'],
+			'usuario_usuario'  => $datosUsuario['uss_usuario'],
+			'usuario_clave'    => $datosUsuario['uss_clave'],
+			'nueva_clave'      => Usuarios::generatePassword(8)
+		];
+		$asunto = 'Tus credenciales han llegado';
+		$bodyTemplateRoute = ROOT_PATH . '/config-general/template-email-recuperar-clave.php';
 
+		EnviarEmail::enviar($data, $asunto, $bodyTemplateRoute, null, null);
+		Usuarios::guardarRegistroRestauracion($data);
 
-if(!empty($datosUsuario)){
-
-	$data = [
-		'institucion_id'   => $institucion['ins_id'],
-		'institucion_agno' => $_SESSION["bd"],
-		'usuario_id'       => $datosUsuario['uss_id'],
-		'usuario_email'    => $datosUsuario['uss_email'],
-		'usuario_nombre'   => $datosUsuario['uss_nombre'],
-		'usuario_usuario'  => $datosUsuario['uss_usuario'],
-		'usuario_clave'    => $datosUsuario['uss_clave'],
-		'nueva_clave'      => Usuarios::generatePassword(8)
-	];
-	$asunto = 'Tus credenciales han llegado';
-	$bodyTemplateRoute = ROOT_PATH.'/config-general/template-email-recuperar-clave.php';
-	
-	EnviarEmail::enviar($data, $asunto, $bodyTemplateRoute,null,null);
-	Usuarios::guardarRegistroRestauracion($data);
-
-	echo '<script type="text/javascript">window.location.href="index.php?success=SC_DT_5&email='.$datosUsuario['uss_email'].'";</script>';
-	exit();	
-	
-}else{
-	echo '<script type="text/javascript">window.location.href="recuperar-clave.php?error=1&inst='.$institucion['ins_id'].'";</script>';
-	exit();	
+		echo '<script type="text/javascript">window.location.href="index.php?success=SC_DT_5&email=' . $datosUsuario['uss_email'] . '";</script>';
+		exit();
+	} else {
+		echo '<script type="text/javascript">window.location.href="recuperar-clave.php?error=1&inst=' . $institucion['ins_id'] . '";</script>';
+		exit();
+	}
+}
+if ($contar > 1) {
+	// Serializar el array para pasarlo como un campo oculto
+	$arraySerialized = serialize($datosUsuario);
+	// Crear el formulario de redirección automática
+	echo '<form id="form" method="post" action="recuperar-clave.php?valor=' . base64_encode($_POST["Usuario"]) . '">';
+	echo '<input type="hidden" name="usuariosEncontrados" value="' . htmlspecialchars($arraySerialized) . '">';
+	echo '</form>';
+	echo '<script>document.getElementById("form").submit();</script>';
+	exit();
+} else {
+	echo '<script type="text/javascript">window.location.href="recuperar-clave.php?error=1&inst=' . $institucion['ins_id'] . '";</script>';
+	exit();
 }
