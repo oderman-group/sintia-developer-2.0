8c35fe41235a1713dd851649628bb95a251cdd50
El gran Sherman <jhooderman@gmail.com>
2024-09-13 04:39:33 -0500

    PES-1052 - Creación y utilización de constantes

--- main-app/class/Tables/BDT_academico_cargas.php
+++ main-app/class/Tables/BDT_academico_cargas.php
@@ -3,6 +3,12 @@ require_once 'BDT_tablas.php';
 
 class BDT_AcademicoCargas extends BDT_Tablas {
 
+    public const ESTADO_DIRECTIVO = 'DIRECTIVO';
+    public const ESTADO_SINTIA    = 'SINTIA';
+
+    public const GENERACION_MANUAL = 'MANUAL';
+    public const GENERACION_AUTO   = 'AUTOMATICA';
+
     public static $tableName = 'academico_cargas';
 
 }
--- main-app/class/Tables/BDT_temp_calculo_boletin_estudiantes.php
+++ main-app/class/Tables/BDT_temp_calculo_boletin_estudiantes.php
@@ -3,6 +3,8 @@ require_once 'BDT_tablas.php';
 
 class BDT_tempCalculoBoletinEstudiantes extends BDT_Tablas {
 
+    public const ESTADO_ESTUDIANTE_PENDIENTE = 'PENDIENTE';
+
     public static $tableName = 'temp_calculo_boletin_estudiantes';
 
 }
