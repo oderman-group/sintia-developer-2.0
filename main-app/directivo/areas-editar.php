75e07e641db53539e89193e1f2e32a37d1cff895
Luis Guillermo <softcomputo77@gmail.com>
2024-09-15 12:29:05 -0500

    🚀 PES-1085 Agregar campos faltantes en Cargas

--- main-app/directivo/areas-editar.php
+++ main-app/directivo/areas-editar.php
@@ -3,7 +3,9 @@
 <?php include("../compartido/historial-acciones-guardar.php");?>
 <?php include("../compartido/head.php");
 require_once(ROOT_PATH."/main-app/class/Areas.php");
+
 Utilidades::validarParametros($_GET);
+
 if(!Modulos::validarSubRol([$idPaginaInterna])){
 	echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
 	exit();
