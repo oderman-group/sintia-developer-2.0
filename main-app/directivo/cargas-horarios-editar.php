c14fc65072d9e689275bb42fa4513e968e33581a
Luis Guillermo <softcomputo77@gmail.com>
2024-09-26 10:10:34 -0500

    🚀 PES-1046 agragar validaciones en paginas faltantes en carga

--- main-app/directivo/cargas-horarios-editar.php
+++ main-app/directivo/cargas-horarios-editar.php
@@ -5,6 +5,8 @@ require_once(ROOT_PATH."/main-app/class/CargaAcademica.php");?>
 <?php include("../compartido/head.php");?>
 <?php 
 
+Utilidades::validarParametros($_GET,["id","idGH"]);
+
 if(!Modulos::validarSubRol([$idPaginaInterna])){
 	echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
 	exit();
--- main-app/directivo/cargas-indicadores-editar.php
+++ main-app/directivo/cargas-indicadores-editar.php
@@ -6,6 +6,8 @@
 <?php
 require_once(ROOT_PATH."/main-app/class/Indicadores.php");
 
+Utilidades::validarParametros($_GET,["carga","docente","periodo","idR"]);
+
 if(!Modulos::validarSubRol([$idPaginaInterna])){
 	echo '<script type="text/javascript">window.location.href="page-info.php?idmsg=301";</script>';
 	exit();
