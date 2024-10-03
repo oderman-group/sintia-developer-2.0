490ce289c18ff0467dfe08daa4ef9c622fb21779
Luis Guillermo <softcomputo77@gmail.com>
2024-10-03 17:06:44 -0500

    🚀 PES-1042 quitar limites en la consulta

--- main-app/directivo/cargas.php
+++ main-app/directivo/cargas.php
@@ -120,7 +120,7 @@ if($config['conf_doble_buscador'] == 1) {
 													<tbody id="cargas_result">
 													<?php
 													include("includes/consulta-paginacion-cargas.php");
-													$filtroLimite = 'LIMIT 9';
+													$filtroLimite = '';
 													$selectSql = ["car_id","car_periodo","car_curso","car_ih","car_permiso2",
 																	"car_indicador_automatico","car_maximos_indicadores",
 																	"car_docente","gra_tipo","am.mat_id",
