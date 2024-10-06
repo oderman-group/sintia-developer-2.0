12eabb42932e32500e389bde412fd0d92c4fbeee
El gran Sherman <jhooderman@gmail.com>
2024-10-06 18:52:28 -0500

    POC - Sintia SMS Twilio

---
+++ send-sms.php
@@ -0,0 +1,23 @@
+<?php
+
+// Update the path below to your autoload.php,
+// see https://getcomposer.org/doc/01-basic-usage.md
+require_once "vendor/twilio/sdk/src/Twilio/autoload.php";
+
+use Twilio\Rest\Client;
+
+// Find your Account SID and Auth Token at twilio.com/console
+// and set the environment variables. See http://twil.io/secure
+$sid = TWILIO_ACCOUNT_SID;
+$token = TWILIO_AUTH_TOKEN;
+$twilio = new Client($sid, $token);
+
+$message = $twilio->messages->create(
+    "+573006075800", // To
+    [
+        "body" => "Hola desde SINTIA, esto es un mensaje de prueba!",
+        "from" => TWILIO_FROM_PHONE_NUMBER,
+    ]
+);
+
+print $message->body;
