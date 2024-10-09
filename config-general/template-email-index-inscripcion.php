0a0ae9923eb843f467be7b8f2cb8ef12597e2899
El gran Sherman <jhooderman@gmail.com>
2024-10-09 03:54:31 -0500

    DEV - Mejorar email de inscripción

--- config-general/template-email-index-inscripcion.php
+++ config-general/template-email-index-inscripcion.php
@@ -20,13 +20,17 @@ $Plataforma = new Plataforma;
 			<div style="font-family:arial; background:#FAFAFA; width:600px; color:#000; text-align:justify; padding:15px;">
 				Cordial saludo <?=$data['usuario_nombre']?>, su solicitud de admisión para el aspirante <b><?=$data['solicitud_nombre']?></b>, fue realizada correctamente.<br>
 				A continuación le informaremos algunos datos importantes que debe tener presente al momento de continuar al siguiente paso, o cuando desee consultar el estado de su solicitud.<br>
-				<span style="color:navy; font-weight:bold;">
+
+				<p style="color:navy; font-weight:bold;">
 					Guarde estos datos en un lugar seguro porque los necesitará durante todo el proceso de admisión:
-				</span><br>  
-				<b>Número de solicitud:</b>          <pre><?=$data['solicitud_id'];?></pre>
-				<b>Número de documento del aspirante:</b> <pre><?=$data['solicitud_documento'];?></pre><br><br>
+				</p><br>
+
+				Número de solicitud:               <b><?=$data['solicitud_id'];?></b><br>
+				Número de documento del aspirante: <b><?=$data['solicitud_documento'];?></b><br><br>
+
 				Puede consultar el estado de su solicitud en el siguiente enlace:<br>
 				<a href="<?=REDIRECT_ROUTE;?>/admisiones/consultar-estado.php?idInst=<?=$_REQUEST['idInst']?>&solicitud=<?=base64_encode($data['solicitud_id']);?>&documento=<?=base64_encode($data['solicitud_documento']);?>">CONSULTAR ESTADO DE SOLICITUD</a>
+
 				<p style="text-align:center;">
 					Gracias por preferirnos, que tenga un feliz día.
 				</p>
