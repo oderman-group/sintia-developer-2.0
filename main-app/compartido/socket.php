2b73ae6601ba93c6583335dfb66a10df84f1b6bc
Luis Guillermo <softcomputo77@gmail.com>
2024-10-15 13:17:18 -0500

    🚀 PES-1112 obligar acolocar  la varable URLAPI en sensitive

--- main-app/compartido/socket.php
+++ main-app/compartido/socket.php
@@ -1,6 +1,6 @@
 <script src="https://cdn.socket.io/3.1.3/socket.io.min.js" integrity="sha384-cPwlPLvBTa3sKAgddT6krw0cJat7egBga3DJepJyrLl4Q9/5WLra3rrnMcyTyOnh" crossorigin="anonymous"></script>
 <script>
-	var socket = io('<?= empty(URL_API)?'wss://plataformasintia.com:3600':URL_API ?>', {
+	var socket = io('<?= URL_API ?>', {
 		transports: ['websocket', 'polling', 'flashsocket']
 	});
 	var chat_remite_usuario = '<?= $idSession ?>';
