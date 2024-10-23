bf7bcd8086a493bc6361060a6ae6d13364dbda9f
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-10-23 19:22:14 +0200

    PES-1223 - Se valida por periodo a trabajar la institucion

--- main-app/js/Comportamiento.js
+++ main-app/js/Comportamiento.js
@@ -6,48 +6,49 @@ function comportamientoPeriodo(datosPeriodo) {
     var periodo         = datosPeriodo.value;
     var idRegistro      = datosPeriodo.getAttribute("data-id-registro");
     var periodoActual   = datosPeriodo.getAttribute("data-periodo-actual");
+    var periodoTotal    = datosPeriodo.getAttribute("data-periodo-total");
 
-    var inputElement = document.getElementById(idInput);
-    
-    if(periodo <= 4){
-        fetch('../directivo/comportamiento-actualizar-periodo.php?periodo='+periodo+'&id='+idRegistro, {
+    var inputElement    = document.getElementById(idInput);
+
+    if (periodo <= periodoTotal) {
+        fetch('../directivo/comportamiento-actualizar-periodo.php?periodo=' + periodo + '&id=' + idRegistro, {
             method: 'GET'
         })
-        .then(response => response.text()) // Convertir la respuesta a texto
-        .then(data => {
+            .then(response => response.text()) // Convertir la respuesta a texto
+            .then(data => {
 
-            inputElement.dataset.periodoActual = periodo;
+                inputElement.dataset.periodoActual = periodo;
 
-            $.toast({
-    
-                heading: 'Proceso completado', 
-                text: 'El periodo del comportamiento se actualizo correctamente...', 
-                position: 'bottom-right',
-                showHideTransition: 'slide',
-                loaderBg:'#26c281', 
-                icon: 'success', 
-                hideAfter: 5000, 
-                stack: 6
-    
+                $.toast({
+
+                    heading: 'Proceso completado',
+                    text: 'El periodo del comportamiento se actualizo correctamente...',
+                    position: 'bottom-right',
+                    showHideTransition: 'slide',
+                    loaderBg: '#26c281',
+                    icon: 'success',
+                    hideAfter: 5000,
+                    stack: 6
+
+                });
+            })
+            .catch(error => {
+                // Manejar errores
+                console.error('Error:', error);
             });
-        })
-        .catch(error => {
-            // Manejar errores
-            console.error('Error:', error);
-        });
-    }else{
+    } else {
 
         inputElement.value = periodoActual;
 
         $.toast({
 
-            heading: 'Periodo Invalido', 
-            text: 'El periodo que ingreso no es valido...', 
+            heading: 'Periodo Invalido',
+            text: 'El periodo que ingreso no es valido...',
             position: 'bottom-right',
             showHideTransition: 'slide',
-            loaderBg:'#26c281', 
-            icon: 'warning', 
-            hideAfter: 5000, 
+            loaderBg: '#26c281',
+            icon: 'warning',
+            hideAfter: 5000,
             stack: 6
 
         });
