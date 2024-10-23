5006bc7e151e75f949bfe468909dfb88d73a3f34
ing-enuarlara7 <enuarlara@oderman-group.com>
2024-10-23 19:16:04 +0200

    PES-1223 - Se añade ROOT_PATH a direccionamiento

--- main-app/class/componentes/filter/filter-comportamiento.php
+++ main-app/class/componentes/filter/filter-comportamiento.php
@@ -1,5 +1,5 @@
 <?php
-require_once("../../class/Disciplina.php");
+require_once(ROOT_PATH . "/main-app/class/Disciplina.php");
 
 if (!empty($filtrosDecode['carga'])) {
     $filtro .= " AND dn_id_carga='" . $filtrosDecode['carga'] . "'";
