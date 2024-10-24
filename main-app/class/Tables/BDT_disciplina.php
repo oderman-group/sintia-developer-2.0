dea0e381743a42181d1f485498d43d14d486d1d4
ing-enuarlara <enuar2110@gmail.com>
2024-10-24 14:07:18 +0200

    PES-1223 - Se nombran tablas directamente en la consulta

--- main-app/class/Tables/BDT_disciplina.php
+++ main-app/class/Tables/BDT_disciplina.php
@@ -3,11 +3,5 @@ require_once 'BDT_tablas.php';
 
 class BDT_disciplina extends BDT_Tablas
 {
-    public static $tableName        = 'disiplina_nota';
-    public static $tableMatricula   = 'academico_matriculas';
-    public static $tableCargas      = 'academico_cargas';
-    public static $tableUsuarios    = 'usuarios';
-    public static $tableGrados      = 'academico_grados';
-    public static $tableGrupos      = 'academico_grupos';
-    public static $tableMaterias    = 'academico_materias';
+    public static $tableName    =   'disiplina_nota';
 }
