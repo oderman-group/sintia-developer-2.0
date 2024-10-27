223107666ef4d0729750c4d7bf06801c50b95025
ing-enuarlara <enuar2110@gmail.com>
2024-10-27 23:04:46 +0100

    PES-1223 - Se oculta parte de la descripcion cuando es muy largo el texto

--- main-app/js/Comportamiento.js
+++ main-app/js/Comportamiento.js
@@ -54,3 +54,26 @@ function comportamientoPeriodo(datosPeriodo) {
         });
     }
 }
+
+/**
+ * Alterna entre mostrar el texto truncado o completo de un elemento.
+ * 
+ * La función `toggleFullText` se activa cuando un usuario hace clic en un elemento
+ * con un título completo en su atributo `title`. Cambia entre mostrar una versión truncada
+ * y la versión completa del texto. Si el texto ya está expandido, lo trunca a 20 caracteres 
+ * y añade puntos suspensivos (`...`). De lo contrario, muestra el texto completo.
+ * 
+ * @param {HTMLElement} element - El elemento en el cual se alternará el texto.
+ */
+function toggleFullText(element) {
+    const fullText = element.getAttribute('title'); // Obtiene el texto completo del atributo 'title'.
+    const isExpanded = element.getAttribute('data-expanded') === 'true'; // Verifica si el texto ya está expandido.
+
+    if (isExpanded) {
+        element.innerHTML = fullText.substring(0, 20) + '...'; // Trunca el texto a 20 caracteres y añade '...'.
+        element.setAttribute('data-expanded', 'false'); // Marca el elemento como no expandido.
+    } else {
+        element.innerHTML = fullText; // Muestra el texto completo.
+        element.setAttribute('data-expanded', 'true'); // Marca el elemento como expandido.
+    }
+}
