d9f44c14418dfdb019372eae1ea8c6b244740120
Luis Guillermo <softcomputo77@gmail.com>
2024-11-05 13:29:46 -0500

    🚀 PES-1140 optimizar consulta

--- config-general/constantes.php
+++ config-general/constantes.php
@@ -130,11 +130,13 @@ define('ICUI', 'ICUI');
 define('OTRO', 'OTRO');
 
 /* ESTADOS DE MATRICULA */
+define('ELIMINADO', 0);
 define('MATRICULADO', 1);
 define('ASISTENTE', 2);
 define('CANCELADO', 3);
 define('NO_MATRICULADO', 4);
 define('EN_INSCRIPCION', 5);
+
 
 /* NIVELES EDUCATIVOS */
 define('PREESCOLAR', 1);
