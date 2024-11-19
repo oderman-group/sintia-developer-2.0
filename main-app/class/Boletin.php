33128c6d7c8b78113e262aa761d790993325edfc
Luis Guillermo <softcomputo77@gmail.com>
2024-11-18 19:55:02 -0500

    🚀 PES-1289 actualizar update por nota

--- main-app/class/Boletin.php
+++ main-app/class/Boletin.php
@@ -1170,14 +1170,15 @@ class Boletin {
         string  $idCarga,
         string  $idEstudiante,
         array   $update,
-        string  $yearBd = ""
+        string  $yearBd = "",
+        string  $filter = ""
     )
     {
         $year= !empty($yearBd) ? $yearBd : $_SESSION["bd"];
 
         [$updateSql, $updateValues] = BindSQL::prepararUpdateConArray($update);
 
-        $sql = "UPDATE ".BD_ACADEMICA.".academico_boletin SET {$updateSql} WHERE bol_estudiante=? AND bol_carga=? AND institucion=? AND year=?";
+        $sql = "UPDATE ".BD_ACADEMICA.".academico_boletin SET {$updateSql} WHERE bol_estudiante=? AND bol_carga=? AND institucion=? AND year=? $filter";
 
         $parametros = array_merge($updateValues, [$idEstudiante, $idCarga, $config['conf_id_institucion'], $year]);
 
