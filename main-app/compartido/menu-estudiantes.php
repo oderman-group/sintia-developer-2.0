65955718f862b3f38dd924d9520c7221fc247f27
El gran Sherman <jhooderman@gmail.com>
2024-12-01 11:11:20 -0500

    DEV - Utilizar metodo verificarModulosDeInstitucion en el menu estudiantil

--- main-app/compartido/menu-estudiantes.php
+++ main-app/compartido/menu-estudiantes.php
@@ -8,14 +8,14 @@
 
 	                        
 							
-							<?php if (array_key_exists(1, $arregloModulos)) { ?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_ACADEMICO)) { ?>
 							<li class="nav-item">
 	                            <a href="cargas.php" class="nav-link nav-toggle"> <i class="material-icons">class</i>
 	                                <span class="title"><?=$frases[73][$datosUsuarioActual['uss_idioma']];?></span> 
 	                            </a>
 	                        </li>
 							<?php } ?>
-							<?php if (array_key_exists(10, $arregloModulos) && $datosEstudianteActual['mat_tipo_matricula'] == GRADO_INDIVIDUAL) {?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_MEDIA_TECNICA) && $datosEstudianteActual['mat_tipo_matricula'] == GRADO_INDIVIDUAL) {?>
 							<li class="nav-item">
 	                            <a href="cargas-adicionales.php" class="nav-link nav-toggle"> <i class="fa-solid fa-sitemap"></i>
 	                                <span class="title"><?=$frases[429][$datosUsuarioActual['uss_idioma']];?></span> 
@@ -24,7 +24,7 @@
 							<?php }?>
 
 
-							<?php if (array_key_exists(Modulos::MODULO_ACADEMICO, $arregloModulos)) { ?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_ACADEMICO)) { ?>
 							<?php if((!empty($_COOKIE["cargaE"]) && !empty($_COOKIE["periodoE"])) || (!empty($_GET["carga"]) && !empty($_GET["periodo"]))){?>
 							<li class="nav-item">
 	                            <a href="#" class="nav-link nav-toggle"> <i class="material-icons">assignment_ind</i>
@@ -49,23 +49,23 @@
 
 									<?php }?>
 									
-									<?php if(!empty($arregloModulos) && array_key_exists(Modulos::MODULO_CLASES, $arregloModulos)){?>
+									<?php if(Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_CLASES)){?>
 										<li class="nav-item"><a href="ausencias.php" class="nav-link "> <span class="title"><?=$frases[7][$datosUsuarioActual['uss_idioma']];?></span></a></li>
 									<?php }?>
 									
-									<?php if(!empty($arregloModulos) && array_key_exists(Modulos::MODULO_CRONOGRAMA, $arregloModulos)){?>
+									<?php if(Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_CRONOGRAMA)){?>
 										<li class="nav-item"><a href="cronograma-calendario.php" class="nav-link "> <span class="title"><?=$frases[111][$datosUsuarioActual['uss_idioma']];?></span></a></li>
 									<?php }?>
 									
-									<?php if(!empty($arregloModulos) && array_key_exists(Modulos::MODULO_ACTIVIDAES, $arregloModulos)){?>
+									<?php if(Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_ACTIVIDAES)){?>
 										<li class="nav-item"><a href="actividades.php" class="nav-link "> <span class="title"><?=$frases[112][$datosUsuarioActual['uss_idioma']];?></span></a></li>
 									<?php }?>
 									
-									<?php if(!empty($arregloModulos) && array_key_exists(Modulos::MODULO_FOROS, $arregloModulos)){?>
+									<?php if(Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_FOROS)){?>
 										<li class="nav-item"><a href="foros.php" class="nav-link "> <span class="title"><?=$frases[113][$datosUsuarioActual['uss_idioma']];?></span></a></li>
 									<?php }?>
 									
-									<?php if(!empty($arregloModulos) && array_key_exists(Modulos::MODULO_EVALUACIONES, $arregloModulos)){?>
+									<?php if(Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_EVALUACIONES)){?>
 										<li class="nav-item"><a href="evaluaciones.php" class="nav-link "> <span class="title"><?=$frases[114][$datosUsuarioActual['uss_idioma']];?></span></a></li>
 									<?php }?>
 									
@@ -74,7 +74,7 @@
 							<?php }?>
 							<?php } ?>
 							
-							<?php if (array_key_exists(Modulos::MODULO_CARPETAS, $arregloModulos)) { ?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_CARPETAS)) { ?>
 								<li class="nav-item">
 									<a href="cargas-carpetas.php" class="nav-link nav-toggle"> <i class="fa fa-folder"></i>
 										<span class="title"><?=$frases[216][$datosUsuarioActual['uss_idioma']];?></span> 
@@ -83,7 +83,7 @@
 							<?php } ?>
 
 							
-							<?php if (array_key_exists(Modulos::MODULO_GENERAL, $arregloModulos)) { ?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_GENERAL)) { ?>
 								<li class="nav-item">
 									<a href="matricula.php" class="nav-link nav-toggle"> <i class="fa fa-pencil-square-o"></i>
 										<span class="title"><?=$frases[60][$datosUsuarioActual['uss_idioma']];?></span> 
@@ -91,7 +91,7 @@
 								</li>
 							<?php } ?>
 							
-							<?php if (array_key_exists(Modulos::MODULO_FINANCIERO, $arregloModulos)) { ?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_FINANCIERO)) { ?>
 								<li class="nav-item">
 									<a href="estado-de-cuenta.php" class="nav-link nav-toggle"> <i class="material-icons">attach_money</i>
 										<span class="title"><?=$frases[104][$datosUsuarioActual['uss_idioma']];?></span> 
@@ -99,7 +99,7 @@
 								</li>
 							<?php } ?>
 							
-							<?php if (array_key_exists(Modulos::MODULO_DISCIPLINARIO, $arregloModulos)) { ?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_DISCIPLINARIO)) { ?>
 								<li class="nav-item">
 									<a href="reportes-disciplinarios.php" class="nav-link nav-toggle"> <i class="material-icons">backspace</i>
 										<span class="title"><?=$frases[105][$datosUsuarioActual['uss_idioma']];?></span> 
@@ -107,7 +107,7 @@
 								</li>
 							<?php } ?>
 
-							<?php if(array_key_exists(Modulos::MODULO_DISCIPLINARIO, $arregloModulos) && $config['conf_ver_observador']==1){?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_DISCIPLINARIO) && $config['conf_ver_observador'] == 1) {?>
 								<li class="nav-item">
 									<a href="aspectos.php" class="nav-link nav-toggle"> <i class="material-icons">backspace</i>
 										<span class="title"><?=$frases[264][$datosUsuarioActual['uss_idioma']];?></span> 
@@ -115,7 +115,7 @@
 								</li>
 							<?php }?>
 							
-							<?php if (array_key_exists(Modulos::MODULO_ACADEMICO, $arregloModulos)) { ?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_ACADEMICO)) { ?>
 								<li class="nav-item">
 									<a href="estudiantes.php" class="nav-link nav-toggle"> <i class="material-icons">group</i>
 										<span class="title"><?=$frases[74][$datosUsuarioActual['uss_idioma']];?></span> 
@@ -123,7 +123,7 @@
 								</li>
 							<?php } ?>
 
-							<?php if (array_key_exists(20, $arregloModulos) && Modulos::validarModulosActivos($conexion, 20)) { ?>
+							<?php if (Modulos::verificarModulosDeInstitucion(null, Modulos::MODULO_MARKETPLACE)) { ?>
 								<li class="nav-item active" data-step="11" data-intro="<b><?=$frases[175][$datosUsuarioActual['uss_idioma']];?>:</b> Encuentra los mejores productos y servicios complementarios." data-position='left'>
 									<a href="marketplace.php" class="nav-link nav-toggle bg-warning text-dark"> <i class="fa fa-shopping-cart text-dark"></i>
 										<span class="title">Marketplace</span> 
