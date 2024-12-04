d148ed564ae6a6e015f05fe5781440ce7777ed6e
ing-enuarlara <enuar2110@gmail.com>
2024-12-04 12:23:29 +0100

    PES-1170 - Se quita el extend

--- main-app/class/App/Mensajes_Informativos/Error.php
+++ main-app/class/App/Mensajes_Informativos/Error.php
@@ -1,7 +1,5 @@
 <?php
-require_once ROOT_PATH.'/main-app/class/Tables/BDT_tablas.php';
-
-class Mensajes_Informativos_Error extends BDT_Tablas {
+class Mensajes_Informativos_Error {
 
     public const USUARIO_BLOQUEADO = 6;
 
