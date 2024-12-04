53f386d36de26ed9a00aa6d188e99ee1ab5201b8
ing-enuarlara <enuar2110@gmail.com>
2024-12-04 11:58:28 +0100

    PES-1170 - Se mueven archivos BDT y se modifican segun nueva extructura a trabajar

--- main-app/class/App/Administrativo/Usuario/Usuario_Bloqueado.php
+++ main-app/class/App/Administrativo/Usuario/Usuario_Bloqueado.php
@@ -1,12 +1,16 @@
 <?php
-require_once 'BDT_tablas.php';
+require_once ROOT_PATH.'/main-app/class/Tables/BDT_tablas.php';
 
-class BDT_usuariosBloqueados extends BDT_Tablas {
+class Administrativo_Usuario_Usuario_Bloqueado extends BDT_Tablas {
 
     public const USUARIO_INDIVIDUAL = "USUARIO_INDIVIDUAL";
     public const USUARIO_GRUPAL     = "USUARIO_GRUPAL";
     public const MATRICULA          = "MATRICULA";
 
+    public static $schema = BD_ADMIN;
+
     public static $tableName = 'usuarios_bloqueados';
+
+    public static $primaryKey = 'usblo_id';
 
 }
