28602ffdc04ff2dcfafae9caeab0dcf597a6baf8
ing-enuarlara <enuar2110@gmail.com>
2024-12-13 19:17:31 +0100

    PES-1171 - Se modifica constante de solicitudes pendientes v2

--- main-app/directivo/ajax-contar-solicitudes.php
+++ main-app/directivo/ajax-contar-solicitudes.php
@@ -3,7 +3,7 @@ include("session.php");
 require_once(ROOT_PATH."/main-app/class/App/Administrativo/Solicitud_Desbloqueo/General_Solicitud.php");
 
 $predicado = [
-	'soli_estado'  		=> Administrativo_Solicitud_Desbloqueo_General_Solicitud::PENDIENTE,
+	'soli_estado'  		=> Administrativo_Solicitud_Desbloqueo_General_Solicitud::SOLICITUD_PENDIENTE,
 	'soli_institucion' 	=> $_SESSION["idInstitucion"],
 	'soli_year'        	=> $_SESSION["bd"]
 ];
