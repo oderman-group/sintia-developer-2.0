21167d5a431e831767025819b1a2399a585d7410
ing-enuarlara <enuar2110@gmail.com>
2024-12-19 14:49:48 +0100

    PES-1171 - Se corrige tabulación

--- main-app/solicitud-desbloqueo-guardar.php
+++ main-app/solicitud-desbloqueo-guardar.php
@@ -8,19 +8,19 @@ require_once(ROOT_PATH."/main-app/compartido/socket.php");
 $datosMotivo = [
 	'soli_id_recurso'   => $_POST["usuario"],
 	'soli_remitente'    => $_POST["usuario"],
-	'soli_fecha'   		=> date('Y-m-d H:i:s'),
-	'soli_mensaje'   	=> $_POST["contenido"],
+	'soli_fecha'        => date('Y-m-d H:i:s'),
+	'soli_mensaje'      => $_POST["contenido"],
 	'soli_estado'       => 1,
-	'soli_tipo'			=> 1,
+	'soli_tipo'         => 1,
 	'soli_institucion'  => $_POST["inst"],
-	'soli_year'			=> date("Y")
+	'soli_year'         => date("Y")
 ];
-Administrativo_Solicitud_Desbloqueo_General_Solicitud::Insert($datosMotivo, BD_GENERAL);
+Administrativo_General_Solicitud::Insert($datosMotivo, BD_GENERAL);
 
 $predicado = [
-	'uss_id'  		=> $_POST['usuario'],
-	'institucion' 	=> $_POST['inst'],
-	'year'        	=> $datosMotivo["soli_year"]
+	'uss_id'        => $_POST['usuario'],
+	'institucion'   => $_POST['inst'],
+	'year'          => $datosMotivo["soli_year"]
 ];
 
 $campos = "TRIM(CONCAT(IFNULL(uss_nombre, ''), ' ', IFNULL(uss_nombre2, ''), ' ', IFNULL(uss_apellido1, ''), ' ', IFNULL(uss_apellido2, ''))) AS uss_nombre";
@@ -28,9 +28,9 @@ $consultaNombre = Administrativo_Usuario_Usuario::Select($predicado, $campos, BD
 $nombreUsuario = $consultaNombre->fetch(PDO::FETCH_ASSOC);
 
 $predicadoD = [
-	'uss_tipo'  	=> TIPO_DIRECTIVO,
-	'institucion' 	=> $_POST['inst'],
-	'year'        	=> $datosMotivo["soli_year"]
+	'uss_tipo'      => TIPO_DIRECTIVO,
+	'institucion'   => $_POST['inst'],
+	'year'          => $datosMotivo["soli_year"]
 ];
 
 $camposD = "uss_id";
@@ -38,13 +38,13 @@ $consultaDirectivos = Administrativo_Usuario_Usuario::Select($predicadoD, $campo
 while ($datosDirectivo = $consultaDirectivos->fetch(PDO::FETCH_ASSOC)) {
 	?>
 		<script>
-			var year			= '<?=($datosMotivo["soli_year"])?>';
-			var institucion		= <?=($_POST['inst'])?>;
-			var emisor			= '<?=($_POST['usuario'])?>';
-			var nombreEmisor	= '<?=($nombreUsuario['uss_nombre'])?>';
-			var asunto			= 'SOLICITUD DE DESBLOQUEO';
-			var contenido		= 'Ha recibido una nueva solicitud de desbloqueo para el usuario ' + nombreEmisor + '.';
-			var receptor		= '<?=($datosDirectivo['uss_id'])?>';
+			var year            = '<?=($datosMotivo["soli_year"])?>';
+			var institucion     = <?=($_POST['inst'])?>;
+			var emisor          = '<?=($_POST['usuario'])?>';
+			var nombreEmisor    = '<?=($nombreUsuario['uss_nombre'])?>';
+			var asunto          = 'SOLICITUD DE DESBLOQUEO';
+			var contenido       = 'Ha recibido una nueva solicitud de desbloqueo para el usuario ' + nombreEmisor + '.';
+			var receptor        = '<?=($datosDirectivo['uss_id'])?>';
 			socket.emit("enviar_mensaje_correo", {
 				year: year,
 				institucion: institucion,
@@ -60,10 +60,10 @@ while ($datosDirectivo = $consultaDirectivos->fetch(PDO::FETCH_ASSOC)) {
 
 ?>
 	<script>
-		var year		= '<?=($datosMotivo["soli_year"])?>';
-		var institucion	= <?=($_POST['inst'])?>;
-		var idRecurso	= '<?=($_POST['usuario'])?>';
-		var ENVIROMENT	= '<?=ENVIROMENT?>';
+		var year        = '<?=($datosMotivo["soli_year"])?>';
+		var institucion = <?=($_POST['inst'])?>;
+		var idRecurso   = '<?=($_POST['usuario'])?>';
+		var ENVIROMENT  = '<?=ENVIROMENT?>';
 		socket.emit("solicitud_desbloqueo", {
 			year: year,
 			institucion: institucion,
